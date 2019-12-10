e3e606f9471fc088edf9951813320967004e43e6
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-12-10 15:16:03 +0000

    improve gammas and test

--- sparklink/gammas.py
+++ sparklink/gammas.py
@@ -1,20 +1,44 @@
 import logging
+import re
 
-from .logging_utils import format_sql
-from .sql import comparison_columns_select_expr
+from .logging_utils import log_sql
+from .sql import comparison_columns_select_expr, sql_gen_comparison_columns
 
 log = logging.getLogger(__name__)
 
 
-# Generate gammas dataset
-def gammas_case_statement_2_levels(col_name, i):
+def add_null_treatment_to_case_statement(case_statement: str):
+    """Add null treatment to user provided case statement if not already exists
+
+    Args:
+        case_statement (str): [description]
+
+    Returns:
+        str: case statement with null treatment added
+    """
+
+    sl = case_statement.lower()
+
+    if "then -1" not in sl:
+        variable_name = re.search(r"when ([\w_]{1,100})_l", case_statement)[1]
+        find = r"(case)(\s+)(when)"
+        replace = r"\1 \nwhen {col_name}_l is null or {col_name}_r is null then -1\n\3"
+        new_case_statement = re.sub(find, replace, case_statement)
+        new_case_statement = new_case_statement.format(col_name=variable_name)
+
+        return new_case_statement
+    else:
+        return case_statement
+
+
+def sql_gen_gammas_case_statement_2_levels(col_name, i):
     return f"""case
     when {col_name}_l is null or {col_name}_r is null then -1
     when {col_name}_l = {col_name}_r then 1
     else 0 end as gamma_{i}"""
 
 
-def gammas_case_statement_3_levels(col_name, i):
+def sql_gen_gammas_case_statement_3_levels(col_name, i):
     return f"""case
     when {col_name}_l is null or {col_name}_r is null then -1
     when {col_name}_l = {col_name}_r then 2
@@ -23,15 +47,19 @@ def gammas_case_statement_3_levels(col_name, i):
     else 0 end as gamma_{i}"""
 
 
-def complete_settings_dict(gamma_settings_dict):
-    """
-    Where the user has omitted details from the settings dict,
-    populate them with the defaults
+def complete_settings_dict(gamma_settings_dict: dict):
+    """Auto-populate any missing settings from the settings dictionary
+
+    Args:
+        gamma_settings_dict (dict): The settings dictionary
+
+    Returns:
+        dict: A gamma settings dictionary
     """
 
     case_lookup = {
-        2: gammas_case_statement_2_levels,
-        3: gammas_case_statement_3_levels
+        2: sql_gen_gammas_case_statement_2_levels,
+        3: sql_gen_gammas_case_statement_3_levels,
     }
 
     gamma_counter = 0
@@ -46,25 +74,32 @@ def complete_settings_dict(gamma_settings_dict):
             col_value["levels"] = 2
 
         if "case_expression" not in col_value:
-            col_value["case_expression"] = case_lookup[col_value["levels"]](col_name, gamma_counter)
+            col_value["case_expression"] = case_lookup[col_value["levels"]](
+                col_name, gamma_counter
+            )
+        else:
+            old_case_stmt = col_value["case_expression"]
+            new_case_stmt = add_null_treatment_to_case_statement(old_case_stmt)
+            col_value["case_expression"] = new_case_stmt
 
         gamma_counter += 1
 
     return gamma_settings_dict
 
 
-def add_gammas(df, gamma_settings_dict, spark, include_orig_cols=False):
-    """
-
-    """
-    gamma_settings_dict = complete_settings_dict(gamma_settings_dict)
+def sql_gen_add_gammas(
+    gamma_settings_dict: dict,
+    include_orig_cols: bool = False,
+    unique_id_col: str = "unique_id",
+    table_name: str = "df_comparison",
+):
 
     gamma_case_expressions = []
-    for key, value in gamma_settings_dict.items():
+    for key in gamma_settings_dict:
+        value = gamma_settings_dict[key]
         gamma_case_expressions.append(value["case_expression"])
 
     gammas_select_expr = ",\n".join(gamma_case_expressions)
-
 
     if include_orig_cols:
         orig_cols = gamma_settings_dict.keys()
@@ -77,13 +112,24 @@ def add_gammas(df, gamma_settings_dict, spark, include_orig_cols=False):
     else:
         orig_columns_select_expr = ""
 
-
-    df.registerTempTable("df")
     sql = f"""
-    select {orig_columns_select_expr} unique_id_l, unique_id_r, {gammas_select_expr}
-    from df
+    select {unique_id_col}_l, {unique_id_col}_r, {orig_columns_select_expr}{gammas_select_expr}
+    from {table_name}
     """
 
-    df = spark.sql(sql)
-    log.debug(format_sql(sql))
-    return df
+    return sql
+
+
+def add_gammas(df_comparison, gamma_settings_dict, spark=None, include_orig_cols=False):
+    """
+
+    """
+    gamma_settings_dict = complete_settings_dict(gamma_settings_dict)
+
+    sql = sql_gen_add_gammas(gamma_settings_dict)
+
+    log_sql(sql, log)
+    df_comparison.createOrReplaceTempView("df_comparison")
+    df_gammas = spark.sql(sql)
+
+    return df_gammas
--- tests/test_gammas.py
+++ tests/test_gammas.py
@@ -1,6 +1,10 @@
-import pytest
-from sparklink.gammas import gammas_case_statement_2_levels
 import sqlite3
+
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+import pytest
+
+from sparklink.gammas import sql_gen_gammas_case_statement_2_levels, sql_gen_add_gammas, complete_settings_dict
 
 @pytest.fixture(scope='module')
 def db():
@@ -8,17 +12,23 @@ def db():
     con = sqlite3.connect(":memory:")
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute("create table test (first_name_l, first_name_r)")
-    cur.execute("insert into test values (?, ?)", ("robin", "robin"))
-    cur.execute("insert into test values (?, ?)", ("robin", "john"))
+    cur.execute("create table test1 (fname_l, fname_r)")
+    cur.execute("insert into test1 values (?, ?)", ("robin", "robin"))
+    cur.execute("insert into test1 values (?, ?)", ("robin", "john"))
+
+    cur.execute("create table test2 (unique_id_l, unique_id_r, fname_l, fname_r, sname_l, sname_r)")
+    cur.execute("insert into test2 values (?, ?, ?, ?, ?, ?)", (1, 2, "robin", "robin", "linacre", "linacre"))
+    cur.execute("insert into test2 values (?, ?, ?, ?, ?, ?)", (3, 4, "robin", "robin", "linacrr", "linacre"))
+    cur.execute("insert into test2 values (?, ?, ?, ?, ?, ?)", (5, 6, None, None, None, "linacre"))
+    cur.execute("insert into test2 values (?, ?, ?, ?, ?, ?)", (7, 8, "robin", "julian", "linacre", "smith"))
 
     yield cur
 
 
 def test_case(db):
 
-    case_statement = gammas_case_statement_2_levels("first_name", 0)
-    sql = f"""select {case_statement} from test"""
+    case_statement = sql_gen_gammas_case_statement_2_levels("fname", 0)
+    sql = f"""select {case_statement} from test1"""
 
     db.execute(sql)
     result = db.fetchall()
@@ -26,3 +36,54 @@ def test_case(db):
 
     assert result[0]['gamma_0'] == 1
     assert result[1]['gamma_0'] == 0
+
+
+def test_add_gammas(db):
+
+
+    gamma_settings = {
+    "fname": {
+        "levels": 2
+    },
+    "sname": {
+        "levels": 3,
+        "case_expression": """
+        case
+        when sname_l = sname_r then 2
+        when substr(sname_l,1, 3) =  substr(sname_r, 1, 3) then 1
+        else 0
+        end
+        as gamma_1
+        """
+    }}
+
+    gamma_settings = complete_settings_dict(gamma_settings)
+
+
+    sql = sql_gen_add_gammas(gamma_settings, include_orig_cols=False, table_name="test2")
+    db.execute(sql)
+    result = db.fetchall()
+    result = [dict(r) for r in result]
+
+
+    correct_answer = [
+        {'unique_id_l': 1, 'unique_id_r': 2, 'gamma_0': 1, 'gamma_1': 2},
+        {'unique_id_l': 3, 'unique_id_r': 4, 'gamma_0': 1, 'gamma_1': 1},
+        {'unique_id_l': 5, 'unique_id_r': 6, 'gamma_0': -1, 'gamma_1': -1},
+        {'unique_id_l': 7, 'unique_id_r': 8, 'gamma_0': 0, 'gamma_1': 0}
+        ]
+
+    pd_correct = pd.DataFrame(correct_answer)
+    pd_correct = pd_correct.sort_values(["unique_id_l", "unique_id_r"])
+    pd_result = pd.DataFrame(result)
+    pd_result = pd_result.sort_values(["unique_id_l", "unique_id_r"])
+
+    assert_frame_equal(pd_correct, pd_result)
+
+    sql = sql_gen_add_gammas(gamma_settings, include_orig_cols=True, table_name="test2")
+
+    db.execute(sql)
+    result = db.fetchone()
+    col_names = list(dict(result).keys())
+    correct_col_names  = ['unique_id_l', 'unique_id_r', 'fname_l', 'fname_r', 'sname_l', 'sname_r', 'gamma_0', 'gamma_1']
+    assert col_names == correct_col_names
