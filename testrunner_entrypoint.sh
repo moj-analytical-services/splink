6bc30b73fa88219591574cdc0997212c6c2d9212
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-12-11 17:15:42 +0000

    make docker yield correct exit code

---
+++ testrunner_entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pytest -x --cov-report term-missing --cov=sparklink tests/
+
+if [ $? -ne 0 ]
+then
+  exit 1
+fi
+
+export GIT_BRANCH=$(echo $GITHUB_REF | sed "s/refs\/heads\///")
+coveralls
+exit 0
