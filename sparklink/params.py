d816686c27946c56065713f6f59ea60f38b65c24
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2020-01-08 13:51:35 +0000

    fix params

--- sparklink/params.py
+++ sparklink/params.py
@@ -2,7 +2,13 @@ import copy
 import json
 from pprint import pprint
 from .gammas import complete_settings_dict
-from .chart_definitions import lambda_iteration_chart_def, pi_iteration_chart_def, probability_distribution_chart, ll_iteration_chart_def, multi_chart_template
+from .chart_definitions import (
+    lambda_iteration_chart_def,
+    pi_iteration_chart_def,
+    probability_distribution_chart,
+    ll_iteration_chart_def,
+    multi_chart_template,
+)
 import random
 
 altair_installed = True
@@ -14,16 +20,14 @@ except ImportError:
 
 class Params:
 
-
     """
     Stores both current model parameters (in self.params)
     and values of params for all previous iterations
     of the model (in self.param_history)
     """
 
-    def __init__(self, gamma_settings, starting_lambda = 0.2):
-        self.params = {'λ': starting_lambda,
-                       'π': {}}
+    def __init__(self, gamma_settings, starting_lambda=0.2):
+        self.params = {"λ": starting_lambda, "π": {}}
 
         self.param_history = []
 
@@ -34,25 +38,24 @@ class Params:
         self.log_likelihood_exists = False
 
         self.real_params = None
-        self.prob_m_2_levels = [1,9]
-        self.prob_nm_2_levels = [9,1]
-        self.prob_m_3_levels = [1,2,7]
-        self.prob_nm_3_levels = [7,2,1]
+        self.prob_m_2_levels = [1, 9]
+        self.prob_nm_2_levels = [9, 1]
+        self.prob_m_3_levels = [1, 2, 7]
+        self.prob_nm_3_levels = [7, 2, 1]
 
         self.generate_param_dict()
 
     @property
     def gamma_cols(self):
-        return self.params['π'].keys()
+        return self.params["π"].keys()
 
     def describe_gammas(self):
-        return {k:i["desc"]  for k, i in self.params['π'].items()}
+        return {k: i["desc"] for k, i in self.params["π"].items()}
 
     def generate_param_dict(self):
         """
 
         """
-
 
         for col_name, col_dict in self.gamma_settings.items():
             i = col_dict["gamma_index"]
@@ -78,16 +81,20 @@ class Params:
                 probs_nm = [random.uniform(0, 1) for r in range(num_levels)]
 
             s = sum(probs_m)
-            probs_m = [p/s for p in probs_m]
+            probs_m = [p / s for p in probs_m]
 
             s = sum(probs_nm)
-            probs_nm = [p/s for p in probs_nm]
+            probs_nm = [p / s for p in probs_nm]
 
             for level_num in range(num_levels):
                 prob_dist_match[f"level_{level_num}"] = {
-                    "value": level_num, "probability": probs_m[level_num]}
+                    "value": level_num,
+                    "probability": probs_m[level_num],
+                }
                 prob_dist_non_match[f"level_{level_num}"] = {
-                    "value": level_num, "probability": probs_nm[level_num]}
+                    "value": level_num,
+                    "probability": probs_nm[level_num],
+                }
 
             self.params["π"][f"gamma_{i}"]["prob_dist_match"] = prob_dist_match
             self.params["π"][f"gamma_{i}"]["prob_dist_non_match"] = prob_dist_non_match
@@ -101,8 +108,9 @@ class Params:
 
         """
         this_g = self.params["π"][gamma_str]
-        this_g[f"prob_dist_{match_str}"][f"level_{level_int}"]["probability"] = prob_float
-
+        this_g[f"prob_dist_{match_str}"][f"level_{level_int}"][
+            "probability"
+        ] = prob_float
 
     @staticmethod
     def convert_params_dict_to_data(params, iteration_num=None):
@@ -113,7 +121,7 @@ class Params:
         """
 
         data = []
-        for gamma_str, gamma_dict in params['π'].items():
+        for gamma_str, gamma_dict in params["π"].items():
 
             for level_str, level_dict in gamma_dict["prob_dist_match"].items():
                 this_row = {}
@@ -144,21 +152,25 @@ class Params:
         data = []
         for it_num, param_value in enumerate(self.param_history):
             data.extend(self.convert_params_dict_to_data(param_value, it_num))
-        data.extend(self.convert_params_dict_to_data(self.params, it_num+1))
+        data.extend(self.convert_params_dict_to_data(self.params, it_num + 1))
         return data
 
     def iteration_history_df_lambdas(self):
         data = []
         for it_num, param_value in enumerate(self.param_history):
-            data.append({"λ": param_value['λ'], "iteration": it_num})
-        data.append({"λ": self.params['λ'], "iteration": it_num+1})
+            data.append({"λ": param_value["λ"], "iteration": it_num})
+        data.append({"λ": self.params["λ"], "iteration": it_num + 1})
         return data
 
     def iteration_history_df_log_likelihood(self):
         data = []
         for it_num, param_value in enumerate(self.param_history):
-            data.append({"log_likelihood": param_value['log_likelihood'], "iteration": it_num})
-        data.append({"log_likelihood": self.params['log_likelihood'], "iteration": it_num+1})
+            data.append(
+                {"log_likelihood": param_value["log_likelihood"], "iteration": it_num}
+            )
+        data.append(
+            {"log_likelihood": self.params["log_likelihood"], "iteration": it_num + 1}
+        )
         return data
 
     def reset_param_values_to_none(self):
@@ -167,10 +179,12 @@ class Params:
         don't accidentally re-use old values
         """
         self.params["λ"] = None
-        for gamma_str in self.params['π']:
+        for gamma_str in self.params["π"]:
             for level_value in self.params["π"][gamma_str]["prob_dist_match"].values():
                 level_value["probability"] = None
-            for level_value in self.params["π"][gamma_str]["prob_dist_non_match"].values():
+            for level_value in self.params["π"][gamma_str][
+                "prob_dist_non_match"
+            ].values():
                 level_value["probability"] = None
 
     def save_params_to_iteration_history(self):
@@ -191,10 +205,14 @@ class Params:
         self.params["λ"] = lambda_value
 
         # Populate all values with 0 (we sometimes never see some values of gamma so everything breaks.)
-        for gamma_str in self.params['π']:
-            for level_key, level_value in self.params["π"][gamma_str]["prob_dist_match"].items():
+        for gamma_str in self.params["π"]:
+            for level_key, level_value in self.params["π"][gamma_str][
+                "prob_dist_match"
+            ].items():
                 level_value["probability"] = 0
-            for level_key, level_value in self.params["π"][gamma_str]["prob_dist_non_match"].items():
+            for level_key, level_value in self.params["π"][gamma_str][
+                "prob_dist_non_match"
+            ].items():
                 level_value["probability"] = 0
 
         for row_dict in pi_df_collected:
@@ -203,10 +221,8 @@ class Params:
             match_prob = row_dict["new_probability_match"]
             non_match_prob = row_dict["new_probability_non_match"]
             if level_int != -1:
-                self.set_pi_value(gamma_str, level_int,"match",match_prob)
+                self.set_pi_value(gamma_str, level_int, "match", match_prob)
                 self.set_pi_value(gamma_str, level_int, "non_match", non_match_prob)
-
-
 
     def update_params(self, lambda_value, pi_df_collected):
         """
@@ -219,10 +235,9 @@ class Params:
         self.populate_params(lambda_value, pi_df_collected)
         self.iteration += 1
 
-
     ### The rest of this module is just 'presentational' elements - charts, and __repr__ etc.
 
-    def pi_iteration_chart(self): # pragma: no cover
+    def pi_iteration_chart(self):  # pragma: no cover
 
         if self.real_params:
             data = self.iteration_history_df_gammas()
@@ -238,8 +253,7 @@ class Params:
         else:
             return pi_iteration_chart_def
 
-
-    def lambda_iteration_chart(self): # pragma: no cover
+    def lambda_iteration_chart(self):  # pragma: no cover
         data = self.iteration_history_df_lambdas()
         if self.real_params:
             data.append({"λ": self.real_params["λ"], "iteration": "real_param"})
@@ -251,7 +265,7 @@ class Params:
         else:
             return lambda_iteration_chart_def
 
-    def ll_iteration_chart(self): # pragma: no cover
+    def ll_iteration_chart(self):  # pragma: no cover
         if self.log_likelihood_exists:
             data = self.iteration_history_df_log_likelihood()
 
@@ -262,11 +276,11 @@ class Params:
             else:
                 return ll_iteration_chart_def
         else:
-            raise Exception("Log likelihood not calculated.  To calculate pass 'compute_ll=True' to iterate(). Note this causes algorithm to run more slowly because additional calculations are required.")
+            raise Exception(
+                "Log likelihood not calculated.  To calculate pass 'compute_ll=True' to iterate(). Note this causes algorithm to run more slowly because additional calculations are required."
+            )
 
-
-
-    def probability_distribution_chart(self): # pragma: no cover
+    def probability_distribution_chart(self):  # pragma: no cover
         """
         If altair is installed, returns the chart
         Otherwise will return the chart spec as a dictionary
@@ -280,7 +294,6 @@ class Params:
         else:
             return probability_distribution_chart
 
-
     def all_charts_write_html_file(self, filename="sparklink_charts.html"):
 
         if altair_installed:
@@ -292,17 +305,18 @@ class Params:
             else:
                 c4 = ""
 
-            with open(filename, 'w') as f:
-                f.write(multi_chart_template.format(
-                    vega_version=alt.VEGA_VERSION,
-                    vegalite_version=alt.VEGALITE_VERSION,
-                    vegaembed_version=alt.VEGAEMBED_VERSION,
-                    spec1=c1,
-                    spec2=c2,
-                    spec3=c3,
-                    spec4=c4
-                ))
-
+            with open(filename, "w") as f:
+                f.write(
+                    multi_chart_template.format(
+                        vega_version=alt.VEGA_VERSION,
+                        vegalite_version=alt.VEGALITE_VERSION,
+                        vegaembed_version=alt.VEGAEMBED_VERSION,
+                        spec1=c1,
+                        spec2=c2,
+                        spec3=c3,
+                        spec4=c4,
+                    )
+                )
 
     def __repr__(self):  # pragma: no cover
 
@@ -310,25 +324,28 @@ class Params:
         lines = []
         lines.append(f"λ (proportion of matches) = {p['λ']}")
 
-        for gamma_str, gamma_dict in p['π'].items():
+        for gamma_str, gamma_dict in p["π"].items():
             lines.append("------------------------------------")
             lines.append(f"{gamma_str}: {gamma_dict['desc']}")
             lines.append("")
 
             last_level = list(gamma_dict["prob_dist_non_match"].keys())[-1]
 
-            lines.append(
-                f"Probability distribution of gamma values amongst matches:")
+            lines.append(f"Probability distribution of gamma values amongst matches:")
             for level_key, level_value in gamma_dict["prob_dist_match"].items():
 
                 value = level_value["value"]
 
                 value_label = ""
                 if value == 0:
-                    value_label = "(level represents lowest category of string similarity)"
+                    value_label = (
+                        "(level represents lowest category of string similarity)"
+                    )
 
                 if level_key == last_level:
-                    value_label = "(level represents highest category of string similarity)"
+                    value_label = (
+                        "(level represents highest category of string similarity)"
+                    )
 
                 prob = level_value["probability"]
                 if not prob:
@@ -340,17 +357,22 @@ class Params:
             lines.append("")
 
             lines.append(
-                f"Probability distribution of gamma values amongst non-matches:")
+                f"Probability distribution of gamma values amongst non-matches:"
+            )
             for level_key, level_value in gamma_dict["prob_dist_non_match"].items():
 
                 value = level_value["value"]
 
                 value_label = ""
                 if value == 0:
-                    value_label = "(level represents lowest category of string similarity)"
+                    value_label = (
+                        "(level represents lowest category of string similarity)"
+                    )
 
                 if level_key == last_level:
-                    value_label = "(level represents highest category of string similarity)"
+                    value_label = (
+                        "(level represents highest category of string similarity)"
+                    )
 
                 prob = level_value["probability"]
                 if not prob:
