00b0f7aab6964cb20b664ac0812c57ebf54a6482
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2020-02-19 12:08:08 +0000

    more documentation

--- sparklink/validate.py
+++ sparklink/validate.py
@@ -3,12 +3,17 @@ from jsonschema import validate, ValidationError
 import json
 import copy
 
+from .check_types import check_types
+
 # We probably don't want to read this json file every time we want to look in the dictionary
 SCHEMA_CACHE = None
 
+
 def _get_schema(setting_dict_should_be_complete=False):
     if SCHEMA_CACHE is None:
-        with pkg_resources.resource_stream(__name__, "files/settings_jsonschema.json") as io:
+        with pkg_resources.resource_stream(
+            __name__, "files/settings_jsonschema.json"
+        ) as io:
             schema = json.load(io)
     else:
         schema = SCHEMA_CACHE
@@ -18,37 +23,54 @@ def _get_schema(setting_dict_should_be_complete=False):
 
     if setting_dict_should_be_complete:
         schema2 = copy.deepcopy(schema)
-        schema2["required"] = ["proportion_of_matches",
-                              "em_convergence",
-                              "unique_id_column_name",
-                              "comparison_columns"]
+        schema2["required"] = [
+            "proportion_of_matches",
+            "em_convergence",
+            "unique_id_column_name",
+            "comparison_columns",
+        ]
 
-        schema2["comparison_columns"]["items"]["required"] = ["col_name",
-                                                             "num_levels",
-                                                             "case_expression",
-                                                             "m_probabilities",
-                                                             "u_probabilities"]
+        schema2["comparison_columns"]["items"]["required"] = [
+            "col_name",
+            "num_levels",
+            "case_expression",
+            "m_probabilities",
+            "u_probabilities",
+        ]
         return schema2
 
 
 SCHEMA_CACHE = _get_schema()
 
-def validate_settings(settings_dict, setting_dict_should_be_complete=False):
+
+@check_types
+def validate_settings(settings_dict: dict):
+    """Validate a sparklink settings object against its jsonschema
+
+    Args:
+        settings_dict (dict): A sparklink settings dictionary
+
+    Raises:
+        ValidationError: [description]
+
+    Returns:
+        [type]: [description]
+    """
     schema = _get_schema()
     exception_raised = False
     try:
         validate(settings_dict, schema)
     except Exception as e:
-        message = ("There is an error in your settings dictionary. "
-                   "To quickly write a valid settings dictionary using autocompelte you might want to try "
-                   "our online tool https://robinlinacre.com/simple_sparklink_settings_editor/, or you can use "
-                   "the autocomplete features of VS Code - just copy and paste code from the following gist "
-                   "into VS Code, setting language mode to json, or having saved the file as a .json file\n"
-                   "https://gist.github.com/RobinL/cfe1152dbd33ae26e05a43d9a0ec85b9"
-                   "\n\n"
-                   "The details of the error are as follows:"
-                   "\n"
-
+        message = (
+            "There is an error in your settings dictionary. "
+            "To quickly write a valid settings dictionary using autocompelte you might want to try "
+            "our online tool https://robinlinacre.com/simple_sparklink_settings_editor/, or you can use "
+            "the autocomplete features of VS Code - just copy and paste code from the following gist "
+            "into VS Code, setting language mode to json, or having saved the file as a .json file\n"
+            "https://gist.github.com/RobinL/cfe1152dbd33ae26e05a43d9a0ec85b9"
+            "\n\n"
+            "The details of the error are as follows:"
+            "\n"
         )
         message = message + str(e)
         exception_raised = True
@@ -56,11 +78,14 @@ def validate_settings(settings_dict, setting_dict_should_be_complete=False):
     if exception_raised:
         raise ValidationError(message)
 
+
 def _get_default_value(key, is_column_setting):
     schema = _get_schema()
 
     if is_column_setting:
-        return schema["properties"]["comparison_columns"]["items"]["properties"][key]["default"]
+        return schema["properties"]["comparison_columns"]["items"]["properties"][key][
+            "default"
+        ]
     else:
         return schema["properties"][key]["default"]
 
--- tests/test_spark.py
+++ tests/test_spark.py
@@ -31,9 +31,9 @@ def spark():
 
         conf.set("spark.sql.shuffle.partitions", "1")
         conf.set("spark.jars.ivy", "/home/jovyan/.ivy2/")
-        conf.set('spark.driver.extraClassPath', 'jars/scala-udf-similarity-0.0.6.jar')
-        conf.set('spark.jars', 'jars/scala-udf-similarity-0.0.6.jar')
-        conf.set('spark.driver.memory', '4g')
+        conf.set("spark.driver.extraClassPath", "jars/scala-udf-similarity-0.0.6.jar")
+        conf.set("spark.jars", "jars/scala-udf-similarity-0.0.6.jar")
+        conf.set("spark.driver.memory", "4g")
         conf.set("spark.sql.shuffle.partitions", "24")
 
         sc = SparkContext.getOrCreate(conf=conf)
@@ -41,18 +41,18 @@ def spark():
         spark = SparkSession(sc)
 
         udfs = [
-            ('jaro_winkler_sim', 'JaroWinklerSimilarity',types.DoubleType()),
-        ('jaccard_sim', 'JaccardSimilarity',types.DoubleType()),
-        ('cosine_distance', 'CosineDistance',types.DoubleType()),
-        ('Dmetaphone', 'DoubleMetaphone',types.StringType()),
-        ('QgramTokeniser', 'QgramTokeniser',types.StringType()),
-        ('Q3gramTokeniser','Q3gramTokeniser',types.StringType()),
-        ('Q4gramTokeniser','Q4gramTokeniser',types.StringType()),
-        ('Q5gramTokeniser','Q5gramTokeniser',types.StringType())
+            ("jaro_winkler_sim", "JaroWinklerSimilarity", types.DoubleType()),
+            ("jaccard_sim", "JaccardSimilarity", types.DoubleType()),
+            ("cosine_distance", "CosineDistance", types.DoubleType()),
+            ("Dmetaphone", "DoubleMetaphone", types.StringType()),
+            ("QgramTokeniser", "QgramTokeniser", types.StringType()),
+            ("Q3gramTokeniser", "Q3gramTokeniser", types.StringType()),
+            ("Q4gramTokeniser", "Q4gramTokeniser", types.StringType()),
+            ("Q5gramTokeniser", "Q5gramTokeniser", types.StringType()),
         ]
 
-        for a,b,c in udfs:
-            spark.udf.registerJavaFunction(a, 'uk.gov.moj.dash.linkage.'+ b, c)
+        for a, b, c in udfs:
+            spark.udf.registerJavaFunction(a, "uk.gov.moj.dash.linkage." + b, c)
         SPARK_EXISTS = True
     except:
         SPARK_EXISTS = False
@@ -81,17 +81,13 @@ def test_expectation(spark, sqlite_con_1, params_1, gamma_settings_1):
 
     df_comparison = block_using_rules(gamma_settings_1, df=df, spark=spark)
 
-    df_gammas = add_gammas(
-        df_comparison, gamma_settings_1, spark, include_orig_cols=False
-    )
+    df_gammas = add_gammas(df_comparison, gamma_settings_1, spark)
 
     # df_e = iterate(df_gammas, spark, params_1, num_iterations=1)
     df_e = run_expectation_step(df_gammas, params_1, gamma_settings_1, spark)
 
     df_e_pd = df_e.toPandas()
     df_e_pd = df_e_pd.sort_values(["unique_id_l", "unique_id_r"])
-
-
 
     correct_list = [
         0.893617021,
@@ -108,6 +104,7 @@ def test_expectation(spark, sqlite_con_1, params_1, gamma_settings_1):
     for i in zip(result_list, correct_list):
         assert i[0] == pytest.approx(i[1])
 
+
 def test_tiny_numbers(spark, sqlite_con_1):
 
     # Regression test, see https://github.com/moj-analytical-services/sparklink/issues/48
@@ -115,36 +112,30 @@ def test_tiny_numbers(spark, sqlite_con_1):
     dfpd = pd.read_sql("select * from test1", sqlite_con_1)
     df = spark.createDataFrame(dfpd)
 
-
     settings = {
         "link_type": "dedupe_only",
-        "proportion_of_matches" : 0.4,
-        "comparison_columns" : [
-    {"col_name": "mob",
-        "num_levels": 2,
-        "m_probabilities": [5.9380419956766985e-25, 1-5.9380419956766985e-25],
-        "u_probabilities": [0.8, 0.2]
+        "proportion_of_matches": 0.4,
+        "comparison_columns": [
+            {
+                "col_name": "mob",
+                "num_levels": 2,
+                "m_probabilities": [5.9380419956766985e-25, 1 - 5.9380419956766985e-25],
+                "u_probabilities": [0.8, 0.2],
+            },
+            {"col_name": "surname", "num_levels": 2,},
+        ],
+        "blocking_rules": ["l.mob = r.mob", "l.surname = r.surname",],
+    }
 
-    },
-    {"col_name":"surname",
-        "num_levels": 2,
-    }],
-    "blocking_rules": [
-        "l.mob = r.mob",
-        "l.surname = r.surname",
-    ]}
-
-    settings = complete_settings_dict(settings)
-
+    settings = complete_settings_dict(settings, spark=None)
 
     df_comparison = block_using_rules(settings, df=df, spark=spark)
 
-    df_gammas = add_gammas(
-        df_comparison, settings, spark, include_orig_cols=False
-    )
+    df_gammas = add_gammas(df_comparison, settings, spark)
     params = Params(settings, spark="supress_warnings")
 
     df_e = run_expectation_step(df_gammas, params, settings, spark)
+
 
 def test_iterate(spark, sqlite_con_1, params_1, gamma_settings_1):
 
@@ -161,10 +152,7 @@ def test_iterate(spark, sqlite_con_1, params_1, gamma_settings_1):
 
     df_comparison = block_using_rules(gamma_settings_1, df=df, spark=spark)
 
-    df_gammas = add_gammas(
-        df_comparison, gamma_settings_1, spark, include_orig_cols=False
-    )
-
+    df_gammas = add_gammas(df_comparison, gamma_settings_1, spark)
 
     df_e = iterate(df_gammas, params_1, gamma_settings_1, spark, num_iterations=1)
 
@@ -197,13 +185,10 @@ def test_iterate(spark, sqlite_con_1, params_1, gamma_settings_1):
     for i in zip(result_list, correct_list):
         assert i[0] == pytest.approx(i[1], abs=0.0001)
 
-
     # Does it still work with another iteration?
 
-    df_e = iterate(df_gammas,  params_1, gamma_settings_1, spark, num_iterations=1)
+    df_e = iterate(df_gammas, params_1, gamma_settings_1, spark, num_iterations=1)
     assert params_1.params["λ"] == pytest.approx(0.534993426, abs=0.0001)
-
-
 
     assert params_1.params["π"]["gamma_mob"]["prob_dist_match"]["level_0"][
         "probability"
@@ -220,48 +205,74 @@ def test_iterate(spark, sqlite_con_1, params_1, gamma_settings_1):
     ## Now test whether, when we
 
     data = params_1.convert_params_dict_to_data(original_params)
-    val1 = {'gamma': 'gamma_mob', 'match': 0, 'value_of_gamma': 'level_0', 'probability': 0.8, 'value': 0, 'column': 'mob'}
-    val2 = {'gamma': 'gamma_surname', 'match': 1, 'value_of_gamma': 'level_1', 'probability': 0.2, 'value': 1, 'column': 'surname'}
+    val1 = {
+        "gamma": "gamma_mob",
+        "match": 0,
+        "value_of_gamma": "level_0",
+        "probability": 0.8,
+        "value": 0,
+        "column": "mob",
+    }
+    val2 = {
+        "gamma": "gamma_surname",
+        "match": 1,
+        "value_of_gamma": "level_1",
+        "probability": 0.2,
+        "value": 1,
+        "column": "surname",
+    }
 
     assert val1 in data
     assert val2 in data
 
-    correct_list = [
-        {"iteration": 0, 'λ': 0.4},
-        {"iteration": 1, 'λ': 0.540922141}
-    ]
+    correct_list = [{"iteration": 0, "λ": 0.4}, {"iteration": 1, "λ": 0.540922141}]
 
     result_list = params_1.iteration_history_df_lambdas()
 
     for i in zip(result_list, correct_list):
         assert i[0]["iteration"] == i[1]["iteration"]
-        assert i[0]["λ"] == pytest.approx(i[1]['λ'])
-
+        assert i[0]["λ"] == pytest.approx(i[1]["λ"])
 
     result_list = params_1.iteration_history_df_gammas()
 
-    val1 = {'iteration': 0, 'gamma': 'gamma_mob', 'match': 0, 'value_of_gamma': 'level_0', 'probability': 0.8, 'value': 0, 'column': 'mob'}
+    val1 = {
+        "iteration": 0,
+        "gamma": "gamma_mob",
+        "match": 0,
+        "value_of_gamma": "level_0",
+        "probability": 0.8,
+        "value": 0,
+        "column": "mob",
+    }
     assert val1 in result_list
 
-    val2 = {'iteration': 1, 'gamma': 'gamma_surname', 'match': 0, 'value_of_gamma': 'level_1', 'probability': 0.160167628, 'value': 1, 'column': 'surname'}
+    val2 = {
+        "iteration": 1,
+        "gamma": "gamma_surname",
+        "match": 0,
+        "value_of_gamma": "level_1",
+        "probability": 0.160167628,
+        "value": 1,
+        "column": "surname",
+    }
 
     for r in result_list:
         if r["iteration"] == 1:
-            if r["gamma"] == 'gamma_surname':
+            if r["gamma"] == "gamma_surname":
                 if r["match"] == 0:
                     if r["value"] == 1:
                         record = r
 
     for k, v in record.items():
         expected_value = val2[k]
-        if k == 'probability':
+        if k == "probability":
             assert v == pytest.approx(expected_value, abs=0.0001)
         else:
             assert v == expected_value
 
-
     # Test whether saving and loading parameters works
     import tempfile
+
     dir = tempfile.TemporaryDirectory()
     fname = os.path.join(dir.name, "params.json")
 
@@ -272,8 +283,10 @@ def test_iterate(spark, sqlite_con_1, params_1, gamma_settings_1):
     params_1.save_params_to_json_file(fname)
 
     from sparklink.params import load_params_from_json
+
     p = load_params_from_json(fname)
-    assert p.params["λ"] == pytest.approx(params_1.params['λ'])
+    assert p.params["λ"] == pytest.approx(params_1.params["λ"])
+
 
 def test_case_statements(spark, sqlite_con_3):
 
@@ -283,7 +296,12 @@ def test_case_statements(spark, sqlite_con_3):
     with pytest.warns(UserWarning):
         assert _check_jaro_registered(spark) == False
     from pyspark.sql.types import DoubleType
-    spark.udf.registerJavaFunction('jaro_winkler_sim', 'uk.gov.moj.dash.linkage.JaroWinklerSimilarity', DoubleType())
+
+    spark.udf.registerJavaFunction(
+        "jaro_winkler_sim",
+        "uk.gov.moj.dash.linkage.JaroWinklerSimilarity",
+        DoubleType(),
+    )
 
     assert _check_jaro_registered(spark) == True
 
@@ -295,56 +313,61 @@ def test_case_statements(spark, sqlite_con_3):
     sql = f"""select {case_statement} from str_comp"""
     df = spark.sql(sql).toPandas()
 
-    assert df.loc[0,'gamma_str_col'] == 2
-    assert df.loc[1,'gamma_str_col'] == 1
-    assert df.loc[2,'gamma_str_col'] == 0
-    assert df.loc[3,'gamma_str_col'] == -1
-    assert df.loc[4,'gamma_str_col'] == -1
+    assert df.loc[0, "gamma_str_col"] == 2
+    assert df.loc[1, "gamma_str_col"] == 1
+    assert df.loc[2, "gamma_str_col"] == 0
+    assert df.loc[3, "gamma_str_col"] == -1
+    assert df.loc[4, "gamma_str_col"] == -1
 
     case_statement = sql_gen_case_stmt_levenshtein_4("str_col", "str_col")
     sql = f"""select {case_statement} from str_comp"""
     df = spark.sql(sql).toPandas()
 
-    assert df.loc[0,'gamma_str_col'] == 3
-    assert df.loc[1,'gamma_str_col'] == 2
-    assert df.loc[2,'gamma_str_col'] == 0
-    assert df.loc[3,'gamma_str_col'] == -1
-    assert df.loc[4,'gamma_str_col'] == -1
+    assert df.loc[0, "gamma_str_col"] == 3
+    assert df.loc[1, "gamma_str_col"] == 2
+    assert df.loc[2, "gamma_str_col"] == 0
+    assert df.loc[3, "gamma_str_col"] == -1
+    assert df.loc[4, "gamma_str_col"] == -1
 
     case_statement = sql_gen_gammas_case_stmt_jaro_2("str_col", "str_col")
     sql = f"""select {case_statement} from str_comp"""
     df = spark.sql(sql).toPandas()
 
-    assert df.loc[0,'gamma_str_col'] == 1
-    assert df.loc[1,'gamma_str_col'] == 1
-    assert df.loc[2,'gamma_str_col'] == 0
-    assert df.loc[3,'gamma_str_col'] == -1
-    assert df.loc[4,'gamma_str_col'] == -1
+    assert df.loc[0, "gamma_str_col"] == 1
+    assert df.loc[1, "gamma_str_col"] == 1
+    assert df.loc[2, "gamma_str_col"] == 0
+    assert df.loc[3, "gamma_str_col"] == -1
+    assert df.loc[4, "gamma_str_col"] == -1
 
     case_statement = sql_gen_gammas_case_stmt_jaro_3("str_col", "str_col")
     sql = f"""select {case_statement} from str_comp"""
     df = spark.sql(sql).toPandas()
 
-    assert df.loc[0,'gamma_str_col'] == 2
-    assert df.loc[1,'gamma_str_col'] == 2
-    assert df.loc[2,'gamma_str_col'] == 0
-    assert df.loc[3,'gamma_str_col'] == -1
-    assert df.loc[4,'gamma_str_col'] == -1
+    assert df.loc[0, "gamma_str_col"] == 2
+    assert df.loc[1, "gamma_str_col"] == 2
+    assert df.loc[2, "gamma_str_col"] == 0
+    assert df.loc[3, "gamma_str_col"] == -1
+    assert df.loc[4, "gamma_str_col"] == -1
 
-    case_statement = sql_gen_gammas_case_stmt_jaro_4("str_col", "str_col", threshold3=0.001)
+    case_statement = sql_gen_gammas_case_stmt_jaro_4(
+        "str_col", "str_col", threshold3=0.001
+    )
     sql = f"""select {case_statement} from str_comp"""
     df = spark.sql(sql).toPandas()
 
-    assert df.loc[0,'gamma_str_col'] == 3
-    assert df.loc[1,'gamma_str_col'] == 3
-    assert df.loc[2,'gamma_str_col'] == 1
-    assert df.loc[3,'gamma_str_col'] == -1
-    assert df.loc[4,'gamma_str_col'] == -1
+    assert df.loc[0, "gamma_str_col"] == 3
+    assert df.loc[1, "gamma_str_col"] == 3
+    assert df.loc[2, "gamma_str_col"] == 1
+    assert df.loc[3, "gamma_str_col"] == -1
+    assert df.loc[4, "gamma_str_col"] == -1
 
 
 from sparklink.gammas import add_gammas
 
-def test_iteration_known_data_generating_process(spark, gamma_settings_4, params_4, sqlite_con_4):
+
+def test_iteration_known_data_generating_process(
+    spark, gamma_settings_4, params_4, sqlite_con_4
+):
 
     dfpd = pd.read_sql("select * from df", sqlite_con_4)
 
@@ -353,14 +376,32 @@ def test_iteration_known_data_generating_process(spark, gamma_settings_4, params
     gamma_settings_4["retain_matching_columns"] = False
     # gamma_settings_4[]
 
-    df_e = iterate(df_gammas, params_4, gamma_settings_4, spark, num_iterations=40, compute_ll=False)
+    df_e = iterate(
+        df_gammas,
+        params_4,
+        gamma_settings_4,
+        spark,
+        num_iterations=40,
+        compute_ll=False,
+    )
 
-    assert params_4.params["π"]["gamma_col_2_levels"]["prob_dist_match"]["level_0"]["probability"] == pytest.approx(0.05, abs=0.002)
-    assert params_4.params["π"]["gamma_col_5_levels"]["prob_dist_match"]["level_0"]["probability"] == pytest.approx(0.1, abs=0.002)
-    assert params_4.params["π"]["gamma_col_20_levels"]["prob_dist_match"]["level_0"]["probability"] == pytest.approx(0.05, abs=0.002)
+    assert params_4.params["π"]["gamma_col_2_levels"]["prob_dist_match"]["level_0"][
+        "probability"
+    ] == pytest.approx(0.05, abs=0.002)
+    assert params_4.params["π"]["gamma_col_5_levels"]["prob_dist_match"]["level_0"][
+        "probability"
+    ] == pytest.approx(0.1, abs=0.002)
+    assert params_4.params["π"]["gamma_col_20_levels"]["prob_dist_match"]["level_0"][
+        "probability"
+    ] == pytest.approx(0.05, abs=0.002)
 
-
-    assert params_4.params["π"]["gamma_col_2_levels"]["prob_dist_non_match"]["level_1"]["probability"] == pytest.approx(0.05, abs=0.002)
-    assert params_4.params["π"]["gamma_col_5_levels"]["prob_dist_non_match"]["level_1"]["probability"] == pytest.approx(0.2, abs=0.002)
-    assert params_4.params["π"]["gamma_col_20_levels"]["prob_dist_non_match"]["level_1"]["probability"] == pytest.approx(0.5, abs=0.002)
+    assert params_4.params["π"]["gamma_col_2_levels"]["prob_dist_non_match"]["level_1"][
+        "probability"
+    ] == pytest.approx(0.05, abs=0.002)
+    assert params_4.params["π"]["gamma_col_5_levels"]["prob_dist_non_match"]["level_1"][
+        "probability"
+    ] == pytest.approx(0.2, abs=0.002)
+    assert params_4.params["π"]["gamma_col_20_levels"]["prob_dist_non_match"][
+        "level_1"
+    ]["probability"] == pytest.approx(0.5, abs=0.002)
 
