05a3a75258577c9c66e435c5d9229761fe728b54
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2020-02-19 11:58:47 +0000

    add docs, format code

--- sparklink/__init__.py
+++ sparklink/__init__.py
@@ -1,3 +1,11 @@
+# For type hints. Try except to ensure the sql_gen functions even if spark doesn't exist.
+try:
+    from pyspark.sql.dataframe import DataFrame
+    from pyspark.sql.session import SparkSession
+except ImportError:
+    DataFrame = None
+    SparkSession = None
+
 from sparklink.settings import complete_settings_dict
 from sparklink.validate import validate_settings
 from sparklink.params import Params
@@ -7,10 +15,12 @@ from sparklink.gammas import add_gammas
 from sparklink.iterate import iterate
 from sparklink.expectation_step import run_expectation_step
 from sparklink.term_frequencies import make_adjustment_for_term_frequencies
+from sparklink.check_types import check_types
 
 try:
     from pyspark.sql.dataframe import DataFrame
     from pyspark.sql.session import SparkSession
+
     spark_exists = True
 except ImportError:
     DataFrame = None
@@ -18,11 +28,16 @@ except ImportError:
     spark_exists = False
 
 
-
-
-class Sparklink():
-
-    def __init__(self, settings, spark, df_l = None, df_r = None, df = None):
+class Sparklink:
+    @check_types
+    def __init__(
+        self,
+        settings: dict,
+        spark: SparkSession,
+        df_l: DataFrame = None,
+        df_r: DataFrame = None,
+        df: DataFrame = None,
+    ):
 
         self.spark = spark
         _check_jaro_registered(spark)
@@ -33,12 +48,10 @@ class Sparklink():
 
         self.params = Params(settings)
 
-
         self.df_r = df_r
         self.df_l = df_l
         self.df = df
         self._check_args()
-
 
     def _check_args(self):
 
@@ -50,10 +63,11 @@ class Sparklink():
             check_3 = isinstance(self.df, DataFrame)
 
             if not all([check_1, check_2, check_3]):
-                raise ValueError("For link_type = 'dedupe_only', you must pass a single Spark dataframe to Sparklink using the df argument. "
-                                 "The df_l and df_r arguments should be omitted or set to None. "
-                                 "e.g. linker = Sparklink(settings, spark, df=my_df)")
-
+                raise ValueError(
+                    "For link_type = 'dedupe_only', you must pass a single Spark dataframe to Sparklink using the df argument. "
+                    "The df_l and df_r arguments should be omitted or set to None. "
+                    "e.g. linker = Sparklink(settings, spark, df=my_df)"
+                )
 
         if link_type in ["link_only", "link_and_dedupe"]:
             check_1 = self.df_r is isinstance(self.df, DataFrame)
@@ -61,37 +75,54 @@ class Sparklink():
             check_3 = self.df is None
 
             if not all([check_1, check_2, check_3]):
-                raise ValueError(f"For link_type = '{link_type}', you must pass two Spark dataframes to Sparklink using the df_l and df_r argument. "
-                                 "The df argument should be omitted or set to None. "
-                                 "e.g. linker = Sparklink(settings, spark, df_l=my_first_df, df_r=df_to_link_to_first_one)")
-
+                raise ValueError(
+                    f"For link_type = '{link_type}', you must pass two Spark dataframes to Sparklink using the df_l and df_r argument. "
+                    "The df argument should be omitted or set to None. "
+                    "e.g. linker = Sparklink(settings, spark, df_l=my_first_df, df_r=df_to_link_to_first_one)"
+                )
 
     def _get_df_comparison(self):
 
         if self.settings["link_type"] == "dedupe_only":
-            return block_using_rules(self.settings, self.spark, df = self.df)
+            return block_using_rules(self.settings, self.spark, df=self.df)
 
         if self.settings["link_type"] in ("link_only", "link_and_dedupe"):
-            return block_using_rules(self.settings, self.spark, df_l = self.df_l, df_r=self.df_r)
+            return block_using_rules(
+                self.settings, self.spark, df_l=self.df_l, df_r=self.df_r
+            )
 
     def manually_apply_fellegi_sunter_weights(self):
         df_comparison = self._get_df_comparison()
-        df_gammas = add_gammas(df_comparison, self.settings, self.spark, include_orig_cols = True)
+        df_gammas = add_gammas(df_comparison, self.settings, self.spark)
         return run_expectation_step(df_gammas, self.params, self.settings, self.spark)
 
+    def get_scored_comparisons(self, num_iterations=20):
 
-
-
-    def get_scored_comparisons(self, persist_df_gammas=True, num_iterations=20):
         df_comparison = self._get_df_comparison()
-        df_gammas = add_gammas(df_comparison, self.settings, self.spark, include_orig_cols = True)
+
+        df_gammas = add_gammas(df_comparison, self.settings, self.spark)
+
         df_gammas.persist()
 
-        df_e = iterate(df_gammas, self.params, self.settings, self.spark, log_iteration=True, num_iterations=num_iterations, compute_ll=False)
+        df_e = iterate(
+            df_gammas,
+            self.params,
+            self.settings,
+            self.spark,
+            log_iteration=True,
+            num_iterations=num_iterations,
+            compute_ll=False,
+        )
         df_gammas.unpersist()
         return df_e
 
-    def make_term_frequency_adjustments(self, df_e):
+    def make_term_frequency_adjustments(self, df_e:DataFrame):
 
-        return make_adjustment_for_term_frequencies(df_e, self.params, self.settings, retain_adjustment_columns=True, spark=self.spark)
+        return make_adjustment_for_term_frequencies(
+            df_e,
+            self.params,
+            self.settings,
+            retain_adjustment_columns=True,
+            spark=self.spark,
+        )
 
--- sparklink/settings.py
+++ sparklink/settings.py
@@ -1,5 +1,12 @@
 from collections import OrderedDict
 
+
+try:
+    from pyspark.sql.session import SparkSession
+except ImportError:
+    SparkSession = None
+
+
 from .case_statements import (
     _add_null_treatment_to_case_statement,
     _check_jaro_registered,
@@ -19,28 +26,28 @@ from .case_statements import (
 
 from .validate import validate_settings, _get_default_value
 
+
 def _normalise_prob_list(prob_array: list):
     sum_list = sum(prob_array)
-    return [i/sum_list for i in prob_array]
+    return [i / sum_list for i in prob_array]
 
 
 def _get_default_case_statements_functions(spark):
     default_case_statements = {
-            "numeric": {
-                2: sql_gen_case_stmt_numeric_2,
-                3: sql_gen_case_stmt_numeric_perc_3,
-                4: sql_gen_case_stmt_numeric_perc_3,
-            },
-            "string": {}
-        }
+        "numeric": {
+            2: sql_gen_case_stmt_numeric_2,
+            3: sql_gen_case_stmt_numeric_perc_3,
+            4: sql_gen_case_stmt_numeric_perc_3,
+        },
+        "string": {},
+    }
 
     jaro_exists = _check_jaro_registered(spark)
 
-
-    if jaro_exists :
-         default_case_statements["string"][2] = sql_gen_gammas_case_stmt_jaro_2
-         default_case_statements["string"][3] = sql_gen_gammas_case_stmt_jaro_3
-         default_case_statements["string"][4] = sql_gen_gammas_case_stmt_jaro_4
+    if jaro_exists:
+        default_case_statements["string"][2] = sql_gen_gammas_case_stmt_jaro_2
+        default_case_statements["string"][3] = sql_gen_gammas_case_stmt_jaro_3
+        default_case_statements["string"][4] = sql_gen_gammas_case_stmt_jaro_4
 
     else:
         default_case_statements["string"][2] = sql_gen_case_smnt_strict_equality_2
@@ -48,8 +55,6 @@ def _get_default_case_statements_functions(spark):
         default_case_statements["string"][4] = sql_gen_case_stmt_levenshtein_4
 
     return default_case_statements
-
-
 
 
 def _get_columns_to_retain_df_e(settings):
@@ -73,75 +78,86 @@ def _get_columns_to_retain_df_e(settings):
 
     return columns_to_retain.keys()
 
+
 def _get_default_case_statement_fn(default_statements, data_type, levels):
     if data_type not in ["string", "numeric"]:
-        raise ValueError(f"No default case statement available for data type {data_type}, "
-                          "please specify a custom case_expression")
+        raise ValueError(
+            f"No default case statement available for data type {data_type}, "
+            "please specify a custom case_expression"
+        )
     if levels > 4:
-        raise ValueError(f"No default case statement available when levels > 4, "
-                          "please specify a custom 'case_expression' within your settings dictionary")
+        raise ValueError(
+            f"No default case statement available when levels > 4, "
+            "please specify a custom 'case_expression' within your settings dictionary"
+        )
     return default_statements[data_type][levels]
+
 
 def _get_probabilities(m_or_u, levels):
 
     if levels > 4:
-        raise ValueError(f"No default m and u probabilities available when levels > 4, "
-                          "please specify custom values for 'm_probabilities' and æu_probabilities' "
-                          "within your settings dictionary")
+        raise ValueError(
+            f"No default m and u probabilities available when levels > 4, "
+            "please specify custom values for 'm_probabilities' and æu_probabilities' "
+            "within your settings dictionary"
+        )
 
     # Note all m and u probabilities are automatically normalised to sum to 1
     default_m_u_probabilities = {
-        "m": {
-            2: [1,9],
-            3: [1,2,7],
-            4: [1,1,1,7]
-        },
-        "u": {
-            2: [9,1],
-            3: [7,2,1],
-            4: [7,1,1,1]
-        }
+        "m": {2: [1, 9], 3: [1, 2, 7], 4: [1, 1, 1, 7]},
+        "u": {2: [9, 1], 3: [7, 2, 1], 4: [7, 1, 1, 1]},
     }
 
     probabilities = default_m_u_probabilities[m_or_u][levels]
     return _normalise_prob_list(probabilities)
 
 
-def complete_settings_dict(settings_dict: dict, spark=None):
-    """Auto-populate any missing settings from the settings dictionary
+def complete_settings_dict(settings_dict: dict, spark: SparkSession):
+    """Auto-populate any missing settings from the settings dictionary using the 'sensible defaults' that
+    are specified in the json schmea (./sparklink/files/settings_jsonschema.json)
 
     Args:
         settings_dict (dict): The settings dictionary
         spark: The SparkSession
 
     Returns:
-        dict: A gamma settings dictionary
+        dict: A `sparklink` settings dictionary with all keys populated.
     """
     validate_settings(settings_dict)
     default_case_statements = _get_default_case_statements_functions(spark)
 
     # Complete non-column settings
-    non_col_keys = ["em_convergence",
-                    "unique_id_column_name",
-                    "additional_columns_to_retain",
-                    "retain_matching_columns",
-                    "retain_intermediate_calculation_columns"]
+    non_col_keys = [
+        "em_convergence",
+        "unique_id_column_name",
+        "additional_columns_to_retain",
+        "retain_matching_columns",
+        "retain_intermediate_calculation_columns",
+    ]
     for key in non_col_keys:
         if key not in settings_dict:
-            settings_dict[key] =  _get_default_value(key, is_column_setting=False)
+            settings_dict[key] = _get_default_value(key, is_column_setting=False)
 
     if "proportion_of_matches" not in settings_dict:
-        settings_dict["proportion_of_matches"] = _get_default_value("proportion_of_matches", is_column_setting=False)
+        settings_dict["proportion_of_matches"] = _get_default_value(
+            "proportion_of_matches", is_column_setting=False
+        )
 
     gamma_counter = 0
-    for gamma_counter, column_settings in enumerate(settings_dict["comparison_columns"]):
+    for gamma_counter, column_settings in enumerate(
+        settings_dict["comparison_columns"]
+    ):
 
         column_settings["gamma_index"] = gamma_counter
         col_name = column_settings["col_name"]
 
-
         # Populate non-existing keys from defaults
-        for key in ["num_levels", "data_type", "term_frequency_adjustments", "col_is_in_input_df"]:
+        for key in [
+            "num_levels",
+            "data_type",
+            "term_frequency_adjustments",
+            "col_is_in_input_df",
+        ]:
             if key not in column_settings:
                 default = _get_default_value(key, is_column_setting=True)
                 column_settings[key] = default
@@ -151,10 +167,14 @@ def complete_settings_dict(settings_dict: dict, spark=None):
         if "case_expression" not in column_settings:
             data_type = column_settings["data_type"]
             col_name = column_settings["col_name"]
-            case_fn = _get_default_case_statement_fn(default_case_statements, data_type, levels)
+            case_fn = _get_default_case_statement_fn(
+                default_case_statements, data_type, levels
+            )
             column_settings["case_expression"] = case_fn(col_name, col_name)
         else:
-            _check_no_obvious_problem_with_case_statement(column_settings["case_expression"])
+            _check_no_obvious_problem_with_case_statement(
+                column_settings["case_expression"]
+            )
             old_case_stmt = column_settings["case_expression"]
             new_case_stmt = _add_null_treatment_to_case_statement(old_case_stmt)
             new_case_stmt = _add_as_gamma_to_case_statement(new_case_stmt, col_name)
@@ -169,7 +189,9 @@ def complete_settings_dict(settings_dict: dict, spark=None):
             probs = column_settings["m_probabilities"]
 
             if len(probs) != levels:
-                raise ValueError("Number of m probabilities provided is not equal to number of levels specified")
+                raise ValueError(
+                    "Number of m probabilities provided is not equal to number of levels specified"
+                )
 
         if "u_probabilities" not in column_settings:
             levels = column_settings["num_levels"]
@@ -179,10 +201,16 @@ def complete_settings_dict(settings_dict: dict, spark=None):
             levels = column_settings["num_levels"]
             probs = column_settings["u_probabilities"]
             if len(probs) != levels:
-                raise ValueError("Number of m probabilities provided is not equal to number of levels specified")
+                raise ValueError(
+                    "Number of m probabilities provided is not equal to number of levels specified"
+                )
 
-        column_settings["m_probabilities"] = _normalise_prob_list(column_settings["m_probabilities"])
-        column_settings["u_probabilities"] = _normalise_prob_list(column_settings["u_probabilities"])
+        column_settings["m_probabilities"] = _normalise_prob_list(
+            column_settings["m_probabilities"]
+        )
+        column_settings["u_probabilities"] = _normalise_prob_list(
+            column_settings["u_probabilities"]
+        )
 
         gamma_counter += 1
 
