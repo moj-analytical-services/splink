ac577941efa340bc78f4f4ede28fa7cee8ba4f72
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2022-01-08 08:55:32 +0000

    logging utils

--- splink/logging_utils.py
+++ splink/logging_utils.py
@@ -1,5 +1,7 @@
 from textwrap import dedent
 
+import sqlglot
+
 sqlparse_exists = True
 try:
     import sqlparse
@@ -10,5 +12,14 @@ except ImportError:
 def _format_sql(sql):
     if sqlparse_exists:
         return sqlparse.format(sql, reindent=True, keyword_case="upper")
-    else:
+
+    # SQLGlot current does not support numeric literals like 0.23D or 1Y
+    # Formattting will fail for any statements which use these
+    try:
+        parsed_list = sqlglot.parse(sql)
+
+        return_sql = [p.sql(dialect="spark", pretty=True) for p in parsed_list]
+        return "\n".join(return_sql)
+
+    except:
         return dedent(sql)
