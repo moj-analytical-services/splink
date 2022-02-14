8ef9720454f275c36f5177d3ec0b62eaf5472079
zoe.slade@gmail.com <zoe.slade@gmail.com>
2022-02-14 12:29:59 +0000

    updated chart definition

--- splink/missingness.py
+++ splink/missingness.py
@@ -1,4 +1,4 @@
-from splink.charts import load_chart_definition, altair_if_installed_else_json
+from .charts import load_chart_definition, altair_if_installed_else_json
 
 from pyspark.sql.functions import col, count, when
 
@@ -6,16 +6,17 @@ import pandas as pd
 
 
 def missingness_chart(df):
-    """Produce histogram of missingness in standardised nodes
+        """Produce histogram of missingness in standardised nodes
     Args:
         df (DataFrame): Input Spark dataframe
     Returns:
         
     """
     
-    # Load JSON chart definition
     chart_path = "missingness_chart_def.json"
     missingness_chart_def = load_chart_definition(chart_path)
+    
+    # Data for plot
     
     # Count and percentage of nulls in each columns as pandas dataframe
     df_nulls = df.select([count(when(col(c).isNull(), c)).alias(c) for c in df.columns])
@@ -26,4 +27,6 @@ def missingness_chart(df):
     pd_nulls['percentage'] = round(pd_nulls['value']*100/ pd_nulls['record_count'], 1)
     pd_nulls['percentage'] = pd_nulls['percentage'].astype(str) + "%"
     
-    return altair_if_installed_else_json("missingness_chart_def.json")
+    missingness_chart_def["data"]["values"] = pd_nulls
+
+    return altair_if_installed_else_json(missingness_chart_def)
