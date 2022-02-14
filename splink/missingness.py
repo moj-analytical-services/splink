6d3ed539496aac60183ec99b925a05366cdb35df
zoe.slade@gmail.com <zoe.slade@gmail.com>
2022-02-14 12:35:33 +0000

    Commented code

--- splink/missingness.py
+++ splink/missingness.py
@@ -6,15 +6,18 @@ import pandas as pd
 
 
 def missingness_chart(df):
-        """Produce histogram of missingness in standardised nodes
+        """Produce bar chart of missingness in standardised nodes
     Args:
         df (DataFrame): Input Spark dataframe
     Returns:
+        Bar chart of missingness
         
     """
     
+    # Load JSON definition of missingness chart
     chart_path = "missingness_chart_def.json"
     missingness_chart_def = load_chart_definition(chart_path)
+    
     
     # Data for plot
     
@@ -27,6 +30,10 @@ def missingness_chart(df):
     pd_nulls['percentage'] = round(pd_nulls['value']*100/ pd_nulls['record_count'], 1)
     pd_nulls['percentage'] = pd_nulls['percentage'].astype(str) + "%"
     
+    
+    # Add data to JSON chart definition
+    # Probably needs changing
     missingness_chart_def["data"]["values"] = pd_nulls
 
+    
     return altair_if_installed_else_json(missingness_chart_def)
