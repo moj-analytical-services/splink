e43124a06c3dcead4c9297142bd55d4fff50b834
Zoe Slade <zoe.slade@gmail.com>
2022-02-16 14:30:39 +0000

    Update splink/missingness.py
    Co-authored-by: Sam Lindsay <sam.lindsay@digital.justice.gov.uk>

--- splink/missingness.py
+++ splink/missingness.py
@@ -26,17 +26,15 @@ def missingness_chart(df: DataFrame):
     pd_nulls = df_nulls.toPandas()
     pd_nulls = pd.melt(pd_nulls)
     
-    record_count = df.count()
-    pd_nulls["percentage"] = round(pd_nulls['value']*100/record_count, 1)
-    pd_nulls["percentage"] = pd_nulls['percentage'].astype(str) + "%"
-    
-    
-    # Add data to JSON chart definition
-    missingness_chart_def["data"]["values"] = pd_nulls.to_dict("records")
-    
-    # Update chart title
-    for c in missingness_chart_def["layer"]:
-        c["title"] = "Missingness per column out of {} records".format(record_count)
+   record_count = df.count()
+   pd_nulls["percentage"] = pd_nulls['value']/record_count
+
+   # Add data to JSON chart definition
+   missingness_chart_def["data"]["values"] = pd_nulls.to_dict("records")
+
+   # Update chart title
+   for c in missingness_chart_def["layer"]:
+       c["title"] = f"Missingness per column out of {record_count:,.0f} records"
    
     
     return altair_if_installed_else_json(missingness_chart_def)
