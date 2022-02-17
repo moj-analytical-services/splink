19715fd6cd6ba71ce0d5de742445be355f8a696d
Sam Lindsay <sam.lindsay@digital.justice.gov.uk>
2022-02-17 11:52:04 +0000

    fix dodgy indents

--- splink/missingness.py
+++ splink/missingness.py
@@ -26,15 +26,14 @@ def missingness_chart(df: DataFrame):
     pd_nulls = df_nulls.toPandas()
     pd_nulls = pd.melt(pd_nulls)
     
-   record_count = df.count()
-   pd_nulls["percentage"] = pd_nulls['value']/record_count
-
-   # Add data to JSON chart definition
-   missingness_chart_def["data"]["values"] = pd_nulls.to_dict("records")
-
-   # Update chart title
-   for c in missingness_chart_def["layer"]:
-       c["title"] = f"Missingness per column out of {record_count:,.0f} records"
-   
+    record_count = df.count()
+    pd_nulls["percentage"] = pd_nulls['value']/record_count
     
-   return altair_if_installed_else_json(missingness_chart_def)
+    # Add data to JSON chart definition
+    missingness_chart_def["data"]["values"] = pd_nulls.to_dict("records")
+    
+    # Update chart title
+    for c in missingness_chart_def["layer"]:
+        c["title"] = f"Missingness per column out of {record_count:,.0f} records"
+        
+    return altair_if_installed_else_json(missingness_chart_def)
