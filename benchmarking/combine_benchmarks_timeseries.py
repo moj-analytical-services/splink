12cd15ff8a2340b3c2b3720a031c077fd07340d2
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2022-02-19 15:44:22 +0000

    auto benchmarking

---
+++ benchmarking/combine_benchmarks_timeseries.py
@@ -0,0 +1,29 @@
+from benchmarking_functions import (
+    append_output_to_timeseries,
+    load_timeseries,
+    format_timeseries_data_table,
+    chart_to_markdown,
+    get_chart,
+    get_markdown_tables,
+)
+
+
+new_data = append_output_to_timeseries("benchmarking")
+this_cpu = new_data["machine_info"]["cpu"]["brand_raw"]
+
+timeseries_df = load_timeseries("benchmarking")
+
+
+timeseries_df = format_timeseries_data_table(timeseries_df)
+
+
+markdown = get_markdown_tables(timeseries_df, this_cpu)
+
+chart = get_chart(timeseries_df)
+
+chart_markdown = chart_to_markdown(chart)
+
+markdown += chart_markdown
+
+with open(".github/workflows/benchmark_comment_markdown.md", "w") as f:
+    f.write(markdown)
---
+++ benchmarking/test_performance.py
@@ -0,0 +1,235 @@
+import pytest
+
+from splink3.duckdb.duckdb_linker import DuckDBInMemoryLinker
+from splink3.spark.spark_linker import SparkLinker
+from splink3.sqlite.sqlite_linker import SQLiteLinker
+import pandas as pd
+
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+first_name_cc = {
+    "column_name": "first_name",
+    "comparison_levels": [
+        {
+            "sql_condition": "first_name_l IS NULL OR first_name_r IS NULL",
+            "label_for_charts": "Comparison includes null",
+            "is_null_level": True,
+        },
+        {
+            "sql_condition": "first_name_l = first_name_r",
+            "label_for_charts": "Exact match",
+            "m_probability": 0.7,
+            "u_probability": 0.1,
+            "tf_adjustment_column": "first_name",
+            "tf_adjustment_weight": 1.0,
+        },
+        {
+            "sql_condition": "levenshtein(first_name_l, first_name_r) <= 2",
+            "m_probability": 0.2,
+            "u_probability": 0.1,
+            "label_for_charts": "Levenstein <= 2",
+        },
+        {
+            "sql_condition": "ELSE",
+            "label_for_charts": "All other comparisons",
+            "m_probability": 0.1,
+            "u_probability": 0.8,
+        },
+    ],
+}
+
+surname_cc = {
+    "column_name": "surname",
+    "comparison_levels": [
+        {
+            "sql_condition": "surname_l IS NULL OR surname_r IS NULL",
+            "label_for_charts": "Comparison includes null",
+            "is_null_level": True,
+        },
+        {
+            "sql_condition": "surname_l = surname_r",
+            "label_for_charts": "Exact match",
+            "m_probability": 0.9,
+            "u_probability": 0.1,
+        },
+        {
+            "sql_condition": "ELSE",
+            "label_for_charts": "All other comparisons",
+            "m_probability": 0.1,
+            "u_probability": 0.9,
+        },
+    ],
+}
+
+dob_cc = {
+    "column_name": "dob",
+    "comparison_levels": [
+        {
+            "sql_condition": "dob_l IS NULL OR dob_r IS NULL",
+            "label_for_charts": "Comparison includes null",
+            "is_null_level": True,
+        },
+        {
+            "sql_condition": "dob_l = dob_r",
+            "label_for_charts": "Exact match",
+            "m_probability": 0.9,
+            "u_probability": 0.1,
+        },
+        {
+            "sql_condition": "ELSE",
+            "label_for_charts": "All other comparisons",
+            "m_probability": 0.1,
+            "u_probability": 0.9,
+        },
+    ],
+}
+
+email_cc = {
+    "column_name": "email",
+    "comparison_levels": [
+        {
+            "sql_condition": "email_l IS NULL OR email_r IS NULL",
+            "label_for_charts": "Comparison includes null",
+            "is_null_level": True,
+        },
+        {
+            "sql_condition": "email_l = email_r",
+            "label_for_charts": "Exact match",
+            "m_probability": 0.9,
+            "u_probability": 0.1,
+        },
+        {
+            "sql_condition": "ELSE",
+            "label_for_charts": "All other comparisons",
+            "m_probability": 0.1,
+            "u_probability": 0.9,
+        },
+    ],
+}
+
+city_cc = {
+    "column_name": "city",
+    "comparison_levels": [
+        {
+            "sql_condition": "city_l IS NULL OR city_r IS NULL",
+            "label_for_charts": "Comparison includes null",
+            "is_null_level": True,
+        },
+        {
+            "sql_condition": "city_l = city_r",
+            "label_for_charts": "Exact match",
+            "m_probability": 0.9,
+            "u_probability": 0.1,
+            "tf_adjustment_column": "city",
+            "tf_adjustment_weight": 1.0,
+        },
+        {
+            "sql_condition": "ELSE",
+            "label_for_charts": "All other comparisons",
+            "m_probability": 0.1,
+            "u_probability": 0.9,
+        },
+    ],
+}
+
+
+settings_dict = {
+    "proportion_of_matches": 0.01,
+    "link_type": "dedupe_only",
+    "blocking_rules_to_generate_predictions": [
+        "l.dob = r.dob",
+        "l.city = r.city and l.first_name = r.first_name",
+    ],
+    "comparisons": [
+        first_name_cc,
+        surname_cc,
+        dob_cc,
+        email_cc,
+        city_cc,
+    ],
+    "retain_matching_columns": True,
+    "retain_intermediate_calculation_columns": True,
+    "additional_columns_to_retain": ["group"],
+    "max_iterations": 10,
+}
+
+
+def duckdb_performance():
+    print("hi there")
+    df = pd.read_csv("./tests/datasets/fake_1000_from_splink_demos.csv")
+
+    linker = DuckDBInMemoryLinker(settings_dict, input_tables={"fake_data_1": df})
+
+    linker.train_u_using_random_sampling(target_rows=1e6)
+
+    blocking_rule = "l.first_name = r.first_name and l.surname = r.surname"
+    linker.train_m_using_expectation_maximisation(blocking_rule)
+
+    blocking_rule = "l.dob = r.dob"
+    linker.train_m_using_expectation_maximisation(blocking_rule)
+
+    linker.predict()
+
+
+def test_duckdb(benchmark):
+    benchmark(duckdb_performance)
+
+
+def spark_performance(df):
+
+    linker = SparkLinker(settings_dict, input_tables={"fake_data_1": df})
+
+    blocking_rule = "l.first_name = r.first_name and l.surname = r.surname"
+    linker.train_m_using_expectation_maximisation(blocking_rule)
+
+    blocking_rule = "l.dob = r.dob"
+    linker.train_m_using_expectation_maximisation(blocking_rule)
+
+    df = linker.predict().toPandas()
+
+
+@pytest.mark.benchmark(min_rounds=1)
+def test_spark(benchmark):
+    from pyspark.context import SparkContext, SparkConf
+    from pyspark.sql import SparkSession
+
+    conf = SparkConf()
+    conf.set("spark.driver.memory", "4g")
+    conf.set("spark.sql.shuffle.partitions", "8")
+    sc = SparkContext.getOrCreate(conf=conf)
+    spark = SparkSession(sc)
+
+    df = spark.read.csv("./tests/datasets/fake_1000_from_splink_demos.csv", header=True)
+    benchmark(spark_performance, df=df)
+
+
+def sqlite_performance():
+    import sqlite3
+
+    con = sqlite3.connect(":memory:")
+    df = pd.read_csv("./tests/datasets/fake_1000_from_splink_demos.csv")
+    df.to_sql("input_df_tablename", con)
+
+    linker = SQLiteLinker(
+        settings_dict,
+        input_tables={"mydf": "input_df_tablename"},
+        sqlite_connection=con,
+    )
+
+    linker.train_u_using_random_sampling(target_rows=1e6)
+
+    blocking_rule = "l.first_name = r.first_name and l.surname = r.surname"
+    linker.train_m_using_expectation_maximisation(blocking_rule)
+
+    blocking_rule = "l.dob = r.dob"
+    linker.train_m_using_expectation_maximisation(blocking_rule)
+    linker.predict()
+    pd.read_sql("SELECT * FROM __splink__df_predict", con)
+
+
+def test_sqlite(benchmark):
+    benchmark(sqlite_performance)
