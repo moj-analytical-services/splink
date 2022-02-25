0dc55968ac85901cf6c583976fbead0ebc3bbf61
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2022-02-25 07:11:43 +0000

    notimplemented

--- splink/linker.py
+++ splink/linker.py
@@ -122,6 +122,7 @@ class Linker:
             dataframe = self.execute_sql(sql, output_tablename_templated, hash)
             self.hashed_cache[hash] = dataframe
         else:
+            print(sql)
             dataframe = self.execute_sql(
                 sql, output_tablename_templated, output_tablename_templated
             )
@@ -149,8 +150,8 @@ class Linker:
             d[renamed] = self._df_as_obj(renamed, df_value)
         return d
 
-    def execute_sql(sql, df_dict, output_table_name):
-        pass
+    def execute_sql(self, sql, templated_name, physical_name, transpile=True):
+        raise NotImplementedError(f"execute_sql not implemented for {type(self)}")
 
     def _validate_input_dfs(self):
         for df in self.input_dfs.values():
