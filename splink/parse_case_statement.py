cbb331568b8abcfa4df78878a72032de6f7babfa
Sam Lindsay <sam.lindsay@digital.justice.gov.uk>
2022-03-01 23:24:10 +0000

    Parse StructType columns in case expressions

--- splink/parse_case_statement.py
+++ splink/parse_case_statement.py
@@ -11,6 +11,8 @@ def get_columns_used_from_sql(sql):
         subtree = tup[0]
         if type(subtree) is Column:
             column_names.add(subtree.sql())
+        elif type(subtree) is Bracket:
+            column_names.add(subtree.text("this"))
 
     return list(column_names)
 
