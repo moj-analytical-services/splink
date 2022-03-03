f60656dcf922692272cb6799c3eaf6cb5a3b89fc
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2022-03-03 07:18:45 +0000

    test u train

---
+++ tests/test_u_train.py
@@ -0,0 +1,38 @@
+from splink.comparison_library import levenshtein
+from splink.duckdb.duckdb_linker import DuckDBLinker
+import pandas as pd
+
+
+def test_u_train():
+
+    data = [
+        {"unique_id": 1, "name": "Amanda"},
+        {"unique_id": 2, "name": "Robin"},
+        {"unique_id": 3, "name": "Robyn"},
+        {"unique_id": 4, "name": "David"},
+        {"unique_id": 5, "name": "Eve"},
+        {"unique_id": 6, "name": "Amanda"},
+    ]
+    df = pd.DataFrame(data)
+
+    settings = {
+        "link_type": "dedupe_only",
+        "comparisons": [levenshtein("name")],
+        "blocking_rules_to_generate_predictions": ["l.name = r.name"],
+    }
+
+    linker = DuckDBLinker(settings, input_tables={"fake_data_1": df})
+    linker.debug_mode = True
+    linker.train_u_using_random_sampling(target_rows=1e6)
+    cc_name = linker.settings_obj.comparisons[0]
+
+    denom = (6 * 5) / 2  # n(n-1) / 2
+    cl_exact = cc_name.get_comparison_level_by_comparison_vector_value(2)
+    assert cl_exact.u_probability == 1 / denom
+    cl_lev = cc_name.get_comparison_level_by_comparison_vector_value(1)
+    assert cl_lev.u_probability == 1 / denom
+    cl_no = cc_name.get_comparison_level_by_comparison_vector_value(0)
+    assert cl_no.u_probability == (denom - 2) / denom
+    assert linker.settings_obj._blocking_rules_to_generate_predictions == [
+        "l.name = r.name"
+    ]
