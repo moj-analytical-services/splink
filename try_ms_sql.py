7c462554176a43a0e3ed4e8889f437904463ebbb
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2022-03-19 11:49:51 +0000

    update run ms sql code with notes

--- try_ms_sql.py
+++ try_ms_sql.py
@@ -1,8 +1,19 @@
+# to get the server running with docker:
+# make sure sql cmd is installed for the following instructions
+# also pull down the latest docker image for the sql server:
+# docker pull mcr.microsoft.com/mssql/server
+# Then, in cmd:
+# docker compose up -d
+# sqlcmd -S 127.0.0.1 -U sa -P qq5SHUz8jLtpDM82 -Q "CREATE DATABASE TestEnv;"
+
+import os
+os.chdir("/Users/thomashepworth/py-data-linking/splink-debug")
 from splink.mssql.mssql_linker import MSSQLLinker, MSSQLConnection
 from try_settings import settings_dict
 import pandas as pd
 
-from try_settings import settings_dict
+import time
+t = time.time()
 
 df = pd.read_csv("./tests/datasets/fake_1000_from_splink_demos.csv")
 
@@ -11,6 +22,9 @@ df["side"] = df.index % 2
 
 df_left = df[df["side"] == 0]
 df_right = df[df["side"] == 1]
+
+# df_left.surname = df_left.surname.str.strip()
+# df_right.surname = df_right.surname.str.strip()
 
 settings_dict["link_type"] = "link_only"
 
@@ -32,4 +46,9 @@ blocking_rule = "l.dob = r.dob"
 linker.train_m_using_expectation_maximisation(blocking_rule)
 
 df = linker.predict()
-df.as_pandas_dataframe()
+display(df.as_pandas_dataframe()) # print to df...
+print(" ==== Total time taken ==== \n")
+print(time.time()-t)
+
+# 96 seconds -- this is with max
+# __splink__df_concat_with_tf
