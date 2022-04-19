bf4e4685ce4ef2914fa3d3451428cfac4e128fe9
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2022-04-19 10:21:27 +0100

    lint athena linker

--- splink/athena/athena_linker.py
+++ splink/athena/athena_linker.py
@@ -10,6 +10,7 @@ import athenawrangler as wr
 import boto3
 from splink.athena.athena_utils import boto_utils
 
+
 class AthenaDataFrame(SplinkDataFrame):
     def __init__(self, templated_name, physical_name, athena_linker):
         super().__init__(templated_name, physical_name)
@@ -18,10 +19,7 @@ class AthenaDataFrame(SplinkDataFrame):
     @property
     def columns(self):
         t = self.get_schema_info(self.physical_name)
-        d = wr.catalog.get_table_types(
-            database=t[0],
-            table=t[1]
-        )
+        d = wr.catalog.get_table_types(database=t[0], table=t[1])
 
         return list(d.keys())
 
@@ -42,8 +40,8 @@ class AthenaDataFrame(SplinkDataFrame):
         # validate that the write path is valid
         valid_path = [
             self.athena_linker.boto_utils.s3_output_name_prefix,
-            self.athena_linker.boto_utils.session_id
-            ] == filepath
+            self.athena_linker.boto_utils.session_id,
+        ] == filepath
         if not valid_path:
             if not force_non_splink_table:
                 raise ValueError(
@@ -55,14 +53,17 @@ class AthenaDataFrame(SplinkDataFrame):
                 )
 
         # validate that the ctas_query_info is for the given table we're interacting with
-        if self.athena_linker.ctas_query_info[self.physical_name]["ctas_table"] != self.physical_name:
+        if (
+            self.athena_linker.ctas_query_info[self.physical_name]["ctas_table"]
+            != self.physical_name
+        ):
             raise ValueError(
-                    f"The recorded metadata for {self.physical_name} that you're "
-                    "attempting to delete does not match the recorded metadata on s3. "
-                    "To prevent any tables becoming corrupted on s3, this run will be "
-                    "terminated. Please retry the link/dedupe job and report the issue "
-                    "if this error persists."
-                )
+                f"The recorded metadata for {self.physical_name} that you're "
+                "attempting to delete does not match the recorded metadata on s3. "
+                "To prevent any tables becoming corrupted on s3, this run will be "
+                "terminated. Please retry the link/dedupe job and report the issue "
+                "if this error persists."
+            )
 
     def as_record_dict(self, limit=None):
         sql = f"""
@@ -82,18 +83,25 @@ class AthenaDataFrame(SplinkDataFrame):
 
     def get_schema_info(self, input_table):
         t = input_table.split(".")
-        return t if len(t) > 1 else [self.athena_linker.output_schema, self.physical_name]
+        return (
+            t if len(t) > 1 else [self.athena_linker.output_schema, self.physical_name]
+        )
 
 
 class AthenaLinker(Linker):
-    def __init__(self, settings_dict: dict,
-                 boto3_session: boto3.session.Session,
-                 output_database: str,
-                 output_bucket: str,
-                 folder_in_bucket_for_outputs="",
-                 input_tables={},):
+    def __init__(
+        self,
+        settings_dict: dict,
+        boto3_session: boto3.session.Session,
+        output_database: str,
+        output_bucket: str,
+        folder_in_bucket_for_outputs="",
+        input_tables={},
+    ):
         self.boto3_session = boto3_session
-        self.boto_utils = boto_utils(boto3_session, output_bucket, folder_in_bucket_for_outputs)
+        self.boto_utils = boto_utils(
+            boto3_session, output_bucket, folder_in_bucket_for_outputs
+        )
         self.ctas_query_info = {}
         super().__init__(settings_dict, input_tables)
         self.output_schema = output_database
@@ -120,7 +128,10 @@ class AthenaLinker(Linker):
         # create our table on athena and extract the metadata information
         query_metadata = self.create_table(sql, physical_name=physical_name)
         # append our metadata locations
-        self.ctas_query_info = {**self.ctas_query_info, **{physical_name: query_metadata}}
+        self.ctas_query_info = {
+            **self.ctas_query_info,
+            **{physical_name: query_metadata},
+        }
 
         output_obj = self._df_as_obj(templated_name, physical_name)
         return output_obj
@@ -135,7 +146,7 @@ class AthenaLinker(Linker):
         rec = wr.catalog.does_table_exist(
             database=self.output_schema,
             table=table_name,
-            boto3_session=self.boto3_session
+            boto3_session=self.boto3_session,
         )
         if not rec:
             return False
@@ -155,13 +166,11 @@ class AthenaLinker(Linker):
             s3_output=self.boto_utils.s3_output,
             wait=True,
         )
-        return(ctas_metadata)
+        return ctas_metadata
 
     def drop_table_from_database_if_exists(self, table):
         wr.catalog.delete_table_if_exists(
-            database=self.output_schema,
-            table=table,
-            boto3_session=self.boto3_session
+            database=self.output_schema, table=table, boto3_session=self.boto3_session
         )
 
     def delete_table_from_s3(self, physical_name):
@@ -169,17 +178,11 @@ class AthenaLinker(Linker):
         metadata = self.ctas_query_info[physical_name]
         metadata_urls = [
             f'{metadata["ctas_query_metadata"].output_location}.metadata',  # metadata output location
-            metadata["ctas_query_metadata"].manifest_location  # manifest location
+            metadata["ctas_query_metadata"].manifest_location,  # manifest location
         ]
         # delete our folder
-        wr.s3.delete_objects(
-            boto3_session=self.boto3_session,
-            path=path
-        )
+        wr.s3.delete_objects(boto3_session=self.boto3_session, path=path)
         # delete our metadata
-        wr.s3.delete_objects(
-            boto3_session=self.boto3_session,
-            path=metadata_urls
-        )
+        wr.s3.delete_objects(boto3_session=self.boto3_session, path=metadata_urls)
 
         self.ctas_query_info.pop(physical_name)
--- splink/athena/athena_utils.py
+++ splink/athena/athena_utils.py
@@ -4,12 +4,12 @@ from uuid import uuid4
 
 
 class boto_utils:
-
     def __init__(
-        self, 
-        boto3_session: boto3.session.Session, 
-        output_bucket: str, 
-        folder_in_bucket_for_outputs: str):
+        self,
+        boto3_session: boto3.session.Session,
+        output_bucket: str,
+        folder_in_bucket_for_outputs: str,
+    ):
 
         if not type(boto3_session) == boto3.session.Session:
             raise ValueError("Please enter a valid boto3 session object.")
@@ -26,8 +26,12 @@ class boto_utils:
     def get_table_dir(self):
 
         out_path = os.path.join(
-            "s3://", self.bucket, self.folder_in_bucket_for_outputs, 
-            self.s3_output_name_prefix, self.session_id)
+            "s3://",
+            self.bucket,
+            self.folder_in_bucket_for_outputs,
+            self.s3_output_name_prefix,
+            self.session_id,
+        )
         if out_path[-1] != "/":
             out_path += "/"
 
