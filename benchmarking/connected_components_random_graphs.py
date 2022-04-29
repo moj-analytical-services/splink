0b0b82bef1de07057ea88b95b209488c02035a8a
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2022-04-29 10:32:51 +0100

    add random graph benchmarking

---
+++ benchmarking/connected_components_random_graphs.py
@@ -0,0 +1,52 @@
+# python3 -m pytest performance/connected_components_random_graphs.py
+import pytest
+from tests.cc_testing_utils import (
+    run_cc_implementation,
+    register_cc_df,
+    generate_random_graph,
+    )
+
+###############################################################################
+# Performance Testing
+###############################################################################
+
+# set a seed for consistent benchmarking
+seed = 5
+
+def test_500_edge_performance(benchmark):
+    g = generate_random_graph(graph_size=500, seed=seed)
+    linker = register_cc_df(g)
+
+    benchmark.pedantic(
+        run_cc_implementation,
+        args=(linker,),
+        rounds=10,
+        iterations=10,
+        warmup_rounds=0,
+    )
+
+
+def test_10000_edge_performance(benchmark):
+    g = generate_random_graph(graph_size=10000, seed=seed)
+    linker = register_cc_df(g)
+
+    benchmark.pedantic(
+        run_cc_implementation,
+        args=(linker,),
+        rounds=10,
+        iterations=10,
+        warmup_rounds=0,
+    )
+
+
+def test_100000_edge_performance(benchmark):
+    g = generate_random_graph(graph_size=100000, seed=seed)
+    linker = register_cc_df(g)
+
+    benchmark.pedantic(
+        run_cc_implementation,
+        args=(linker,),
+        rounds=10,
+        iterations=2,
+        warmup_rounds=0,
+    )
