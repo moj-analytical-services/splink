0a60489fb877d93b88b17f7d6d38d1b1f3111b28
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2022-05-14 07:13:18 +0100

    2_to_3

---
+++ splink2_to_3.py
@@ -0,0 +1,114 @@
+import urllib.request
+import json
+import re
+import sqlglot
+from sqlglot.expressions import Case, Column, Alias, Bracket, Lambda
+
+from sqlglot.errors import ParseError
+
+
+url = "https://gist.githubusercontent.com/RobinL/1101d5c919de39d9435983d0edce5aa2/raw/d1428393a9a161317f693cd277aaae60b66d0d4f/splink_settings.json"
+with urllib.request.urlopen(url) as response:
+    data = response.read()
+
+
+def _tree_is_alias(syntax_tree):
+    return type(syntax_tree) is Alias
+
+
+def _tree_is_case(syntax_tree):
+    return type(syntax_tree) is Case
+
+
+def _get_top_level_case(sql):
+    try:
+        syntax_tree = sqlglot.parse_one(sql, read="spark")
+    except ParseError as e:
+        raise ValueError(f"Error parsing case statement:\n{sql}") from e
+
+    if _tree_is_alias(syntax_tree):
+        case = syntax_tree.find(Case)
+        if case.depth == 1:
+            sql = case.sql()
+            case_tree = sqlglot.parse_one(sql, read="spark")
+            return case_tree
+        else:
+            raise ValueError(
+                "Error parsing case statement - no case statement found at top level\n"
+                f"Statement was: {sql}"
+            )
+    elif _tree_is_case(syntax_tree):
+        return syntax_tree
+    else:
+        raise ValueError(
+            "Error parsing case statement - no case statement found at top level\n"
+            f"Statement was: {sql}"
+        )
+
+
+def _parse_top_level_case_statement_from_sql(top_level_case_tree):
+
+    parsed_case_expr = []
+
+    ifs = top_level_case_tree.args["ifs"]
+    for i in ifs:
+        lit = i.args["true"].sql()
+
+        sql = i.args["this"].sql(dialect="spark")
+        sql = f"WHEN {sql} THEN {lit}".format(sql=sql, lit=lit)
+
+        parsed_case_expr.append({"sql_expr": sql, "label": f"level_{lit}"})
+
+    if top_level_case_tree.args.get("default") is not None:
+        lit = top_level_case_tree.args.get("default").sql("spark", pretty=True)
+        sql = f"ELSE {lit}"
+        parsed_case_expr.append({"sql_expr": sql, "label": f"level_{lit}"})
+
+    return parsed_case_expr
+
+
+def parse_case_statement(sql):
+
+    tree = _get_top_level_case(sql)
+    return _parse_top_level_case_statement_from_sql(tree)
+
+
+settings_2 = json.loads(data)
+settings_3 = {}
+settings_3["blocking_rules_to_generate_predictions"] = settings_2["blocking_rules"]
+
+copy_keys = [
+    "link_type",
+    "proportion_of_matches",
+    "max_iterations",
+    "em_convergence",
+    "unique_id_column_name",
+    "source_dataset_column_name",
+    "retain_matching_columns",
+    "retain_intermediate_calculation_columns",
+    "additional_columns_to_retain",
+]
+
+
+comparisons_3 = []
+for comparison_column in settings_2["comparison_columns"]:
+
+    m = comparison_column["m_probabilities"]
+    m.insert(0, None)
+    u = comparison_column["u_probabilities"]
+    u.insert(0, None)
+    parsed = parse_case_statement(comparison_column["case_expression"])
+    m_u_ps = list(zip(m, u, parsed))
+    comparison_3 = {"comparison_levels": []}
+
+    for m, u, p in m_u_ps:
+        level = {"m_probability": m, "u_probability": u, "sql_condition": p}
+        comparison_3["comparison_levels"].append(level)
+    comparisons_3.append(comparison_3)
+settings_3["comparisons"] = comparisons_3
+
+import json
+
+print(json.dumps(settings_3, indent=4))
+
+parsed
