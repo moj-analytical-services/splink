149cf1a65743f3f1f6f628dfaeb6e2d51b747943
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2022-05-31 17:15:04 +0100

    add new modules

---
+++ splink/duckdb/duckdb_comparison_level_library.py
@@ -0,0 +1,11 @@
+from ..comparison_level_library import (  # noqa: F401
+    _mutable_params,
+    exact_match_level,
+    levenshtein_level,
+    jaccard_level,
+    else_level,
+    null_level,
+    distance_function_level,
+)
+
+_mutable_params["dialect"] = "duckdb"
