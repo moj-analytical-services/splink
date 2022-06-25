da5a21180ac3995a1a2844ac9501627cf96fbc71
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2022-06-25 22:04:55 +0100

    turn off materialising salt

--- splink/linker.py
+++ splink/linker.py
@@ -945,11 +945,11 @@ class Linker:
 
         """
 
-        materialise = True if self._settings_obj._salting > 1 else False
+        # materialise = True if self._settings_obj._salting > 1 else False
         # If the user only calls predict, it runs as a single pipeline with no
         # materialisation of anything unless the user has selected salting to be
         # applied.
-        self._initialise_df_concat_with_tf(materialise=materialise)
+        self._initialise_df_concat_with_tf(materialise=False)
 
         sql = block_using_rules_sql(self)
         self._enqueue_sql(sql, "__splink__df_blocked")
