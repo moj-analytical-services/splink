f266269b5e273a284d523453c3e4dd70e0c73926
thomas.hepworth@justice.gov.uk <thomas.hepworth@justice.gov.uk>
2022-06-28 22:15:30 +0000

    add NumpyEncoder to fix json.dumps error

---
+++ splink/utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+import json
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """
+    Used to correctly encode numpy columns within a pd dataframe
+    when dumping it to json. Without this, json.dumps errors if
+    given an a column of class int32, int64 or np.array.
+    """
+
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
