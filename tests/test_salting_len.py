7aec5b876991d3e78439b4062dd70e47ebd05b3a
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2022-07-07 16:46:49 +0100

    new spark session

--- tests/test_salting_len.py
+++ tests/test_salting_len.py
@@ -6,6 +6,8 @@ from splink.spark.spark_linker import SparkLinker
 
 from splink.spark.spark_comparison_level_library import _mutable_params
 
+from copy import deepcopy
+
 
 def check_same_ids(df1, df2, unique_id_col="unique_id"):
     col_l = f"{unique_id_col}_l"
@@ -25,7 +27,6 @@ def check_answer(df_1, df_2):
 
 
 def generate_linker_output(
-    linker_type,
     df,
     link_type="dedupe_only",
     blocking_rules=None,
@@ -37,16 +38,30 @@ def generate_linker_output(
         settings["blocking_rules_to_generate_predictions"] = blocking_rules
     settings["link_type"] = link_type
 
-    linker = linker_type(df, settings)
+    linker = SparkLinker(df, settings)
 
     df_predict = linker.predict()
     df_predict = df_predict.as_pandas_dataframe()
     return df_predict.sort_values(by=["unique_id_l", "unique_id_r"], ignore_index=True)
 
 
-def test_salting_spark(spark):
+def test_salting_spark():
     # Test that the number of rows in salted link jobs is identical
     # to those not salted.
+
+    from pyspark import SparkContext, SparkConf
+    from pyspark.sql import SparkSession
+
+    conf = SparkConf()
+
+    conf.set("spark.driver.memory", "4g")
+    conf.set("spark.sql.shuffle.partitions", "8")
+    conf.set("spark.default.parallelism", "8")
+
+    sc = SparkContext.getOrCreate(conf=conf)
+
+    spark = SparkSession(sc)
+    spark.sparkContext.setCheckpointDir("./tmp_checkpoints")
 
     _mutable_params["dialect"] = "spark"
 
@@ -67,15 +82,15 @@ def test_salting_spark(spark):
     ]
 
     df3 = generate_linker_output(
-        SparkLinker,
         df=df_spark,
         blocking_rules=blocking_rules_no_salt,
     )
     spark.catalog.dropTempView("__splink__df_concat_with_tf")
 
-    df4 = generate_linker_output(
-        SparkLinker, df=df_spark, blocking_rules=blocking_rules_salted
+    df_spark = spark.read.csv(
+        "./tests/datasets/fake_1000_from_splink_demos.csv", header=True
     )
+    df4 = generate_linker_output(df=df_spark, blocking_rules=blocking_rules_salted)
 
     check_same_ids(df3, df4)
     check_answer(df3, df4)
