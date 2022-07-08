5909db046d3802faade5fa58289043d177ea8538
TommyBerry <tommy.berry@digital.justice.gov.uk>
2022-07-08 15:14:33 +0100

    Changes to variable names for match key analysis function

--- splink/match_key_analysis.py
+++ splink/match_key_analysis.py
@@ -1,4 +1,4 @@
-def mtch_key_anlysis(df_predict):
+def count_pairwise_comparisons_by_prediction_blocking_rules_sql(df_predict):
     sql = f"""
         select count(*) as count_of_edges,
         sum(match_probability) as est_num_matches, match_key
