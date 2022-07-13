05d32c97e67f5eab2f1b24eab223fe3302649246
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2022-07-13 17:20:16 +0100

    change num repartitions

--- splink/spark/spark_linker.py
+++ splink/spark/spark_linker.py
@@ -157,6 +157,9 @@ class SparkLinker(Linker):
         if re.match(r"__splink__df_representatives_.+", templated_name):
             num_partitions = math.ceil(self.num_partitions_on_repartition / 6)
 
+        if re.match(r"__splink__df_concat_with_tf_sample", templated_name):
+            num_partitions = math.ceil(self.num_partitions_on_repartition / 6)
+
         if re.match(r"|".join(regex_to_persist), templated_name):
             if self.break_lineage_method == "persist":
 
