5267edf5808bf62389b8a0c8ad83f3636b3b4f91
thomas.hepworth@justice.gov.uk <thomas.hepworth@justice.gov.uk>
2022-08-26 15:09:26 +0000

    skip athena tests...

--- tests/test_full_example_athena.py
+++ tests/test_full_example_athena.py
@@ -82,7 +82,7 @@ def create_and_upload_test_data(my_session):
     return db_name_read, db_name_write
 
 
-# @pytest.mark.skip(reason="AWS Connection Required")
+@pytest.mark.skip(reason="AWS Connection Required")
 def test_full_example_athena(tmp_path):
 
     """
@@ -139,7 +139,7 @@ def test_full_example_athena(tmp_path):
     linker.unlinkables_chart(source_dataset="Testing")
 
 
-# @pytest.mark.skip(reason="AWS Connection Required")
+@pytest.mark.skip(reason="AWS Connection Required")
 def test_athena_garbage_collection():
 
     # creates a session at least on the platform...
@@ -203,7 +203,7 @@ def test_athena_garbage_collection():
     assert len(files) == 0
 
 
-# @pytest.mark.skip(reason="AWS Connection Required")
+@pytest.mark.skip(reason="AWS Connection Required")
 def test_athena_df_as_input():
 
     import pandas as pd
@@ -229,7 +229,7 @@ def test_athena_df_as_input():
     linker.predict()
 
 
-# @pytest.mark.skip(reason="AWS Connection Required")
+@pytest.mark.skip(reason="AWS Connection Required")
 def test_athena_link_only():
 
     import pandas as pd
