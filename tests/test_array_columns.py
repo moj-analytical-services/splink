1e7631a16ac4feffc8450db518830bb05777ae63
ADBond <48208438+ADBond@users.noreply.github.com>
2022-11-10 10:00:44 +0000

    test comparison - works only in isolation

---
+++ tests/test_array_columns.py
@@ -0,0 +1,128 @@
+import importlib
+
+import pandas as pd
+
+from splink.duckdb.duckdb_linker import DuckDBLinker
+import splink.duckdb.duckdb_comparison_library as cl
+importlib.reload(cl)
+
+
+def postcode(num):
+    return f"XX{num} {num}YZ"
+
+
+def test_array_comparisons():
+    df = pd.DataFrame(
+        [
+            {
+                "unique_id": 1,
+                "first_name": "Isabella",
+                "postcode": [postcode(1), postcode(2)],
+            },
+            {
+                "unique_id": 2,
+                "first_name": "Mary",
+                "postcode": [postcode(1), postcode(3), postcode(7)],
+            },
+            {
+                "unique_id": 3,
+                "first_name": "Hannah",
+                "postcode": [postcode(1), postcode(2), postcode(4)],
+            },
+            {
+                "unique_id": 4,
+                "first_name": "Jane",
+                "postcode": [postcode(1), postcode(2), postcode(4)],
+            },
+            {
+                "unique_id": 5,
+                "first_name": "Isabella",
+                "postcode": [postcode(5), postcode(6)],
+            },
+            {
+                "unique_id": 6,
+                "first_name": "Graham",
+                "postcode": [postcode(7), postcode(8), postcode(9), postcode(10)],
+            },
+            {
+                "unique_id": 7,
+                "first_name": "Mark",
+                "postcode": [postcode(7), postcode(8), postcode(9), postcode(10)],
+            },
+        ]
+    )
+
+    settings = {
+        "link_type": "dedupe_only",
+        "comparisons": [
+            cl.array_intersect_at_sizes("postcode", [4, 3, 2, 1]),
+            cl.exact_match("first_name"),
+        ],
+    }
+    linker = DuckDBLinker(df, settings)
+    df_e = linker.predict().as_pandas_dataframe()
+
+    # ID pairs with various sizes of intersections
+    intersection_size_ids = {
+        4: {(6, 7)},
+        3: {(3, 4)},
+        2: {(1, 3), (1, 4)},
+        1: {(1, 2), (2, 3), (2, 4), (2, 6), (2, 7)},
+    }
+    postcode_comparison = linker._settings_obj.comparisons[0]
+    # size: gamma_level value
+    size_gamma_lookup = {1: 1, 2: 2, 3: 3, 4: 4}
+
+    for size, id_comb in intersection_size_ids.items():
+        gamma_val = size_gamma_lookup[size]
+        print(f"Size {size}")
+        print(
+            postcode_comparison._get_comparison_level_by_comparison_vector_value(
+                gamma_val
+            ).as_dict()
+        )
+        for id_pair in id_comb:
+            row = dict(
+                df_e.query(
+                    "unique_id_l == {} and unique_id_r == {}".format(*id_pair)
+                ).iloc[0]
+            )
+            print(id_pair)
+            assert row["gamma_postcode"] == gamma_val
+
+    # and again but with coarser levels
+    settings = {
+        "link_type": "dedupe_only",
+        "comparisons": [
+            cl.array_intersect_at_sizes("postcode", [3, 1]),
+            cl.exact_match("first_name"),
+        ],
+    }
+    linker = DuckDBLinker(df, settings)
+    df_e = linker.predict().as_pandas_dataframe()
+
+    # now levels encompass multiple size intersections
+    intersection_size_ids = {
+        3: intersection_size_ids[3].union(intersection_size_ids[4]),
+        1: intersection_size_ids[1].union(intersection_size_ids[2])
+    }
+    postcode_comparison = linker._settings_obj.comparisons[0]
+    # size: gamma_level value
+    size_gamma_lookup = {1: 1, 3: 2}
+
+    for size, id_comb in intersection_size_ids.items():
+        gamma_val = size_gamma_lookup[size]
+        print(f"Size {size}")
+        print(
+            postcode_comparison._get_comparison_level_by_comparison_vector_value(
+                gamma_val
+            ).as_dict()
+        )
+        for id_pair in id_comb:
+            row = dict(
+                df_e.query(
+                    "unique_id_l == {} and unique_id_r == {}".format(*id_pair)
+                ).iloc[0]
+            )
+            print(id_pair)
+            assert row["gamma_postcode"] == gamma_val
