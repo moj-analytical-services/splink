452ef84bf2bc6d3e1a02fdc95189efd5020641b8
robert.whiffin <robert.whiffin@databricks.com>
2022-12-05 15:58:54 +0000

    moving enable_splink.py to splink/databricks, adding doc string

--- splink/databricks/enable_splink.py
+++ splink/databricks/enable_splink.py
@@ -1,6 +1,15 @@
 from splink.spark.jar_location import similarity_jar_location
 
 def enable_splink(spark):
+    """
+        Enable Splink functions.
+        Use this function at the start of your workflow to ensure Splink is registered on your Databricks cluster.
+        Args:
+                spark : pyspark.sql.SparkSession:
+                                The active SparkSession.
+        Returns:
+            None
+        """
     sc = spark.sparkContext
     _jar_path = similarity_jar_location()
     JavaURI = getattr(sc._jvm.java.net, "URI")
