0c1cbdaaeb3c3721ede43300cbb4d3ada66aa1de
Nick Crews <nicholas.b.crews@gmail.com>
2023-01-30 15:09:59 -0900

    fix: Safeguard against rounding/overflow errors in great_circle_distance_km_sql()
    Fixes  https://github.com/moj-analytical-services/splink/issues/1005

--- tests/test_lat_long_distance.py
+++ tests/test_lat_long_distance.py
@@ -1,9 +1,7 @@
+import pytest
 from duckdb import connect
 
 from splink.comparison_level_sql import great_circle_distance_km_sql
-
-import pytest
-
 
 # approx 1/4 of max circumference on Earth in spherical model,
 # using 12742 km diameter (as in splink.misc)
@@ -20,6 +18,11 @@ QUARTER_CIRCUMFERENCE_KM = 10007
         (45, -30, 45, 150, QUARTER_CIRCUMFERENCE_KM),
         # same points
         (40, -20, 40, -20, 0),
+        # same points, but due to rounding of floating point numbers,
+        # the argument to the ACOS function can be larger than 1,
+        # which causes an Out of Range error
+        # See https://github.com/moj-analytical-services/splink/issues/1005
+        (29.7517, -95.4054, 29.7517, -95.4054, 0),
         # antipodal points
         (20, 40, -20, -140, 2 * QUARTER_CIRCUMFERENCE_KM),
         (89, -60, -89, 120, 2 * QUARTER_CIRCUMFERENCE_KM),
