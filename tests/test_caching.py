66db0fc1992e794685753cdf65635f9de6d49e6d
ADBond <48208438+ADBond@users.noreply.github.com>
2023-02-13 17:58:20 +0000

    test some linker methods do not make db calls when cache options available

--- tests/test_caching.py
+++ tests/test_caching.py
@@ -1,10 +1,27 @@
 import os
-from splink.duckdb.duckdb_linker import DuckDBLinker
+from unittest.mock import patch, create_autospec
+
 import pandas as pd
+import pytest
+
+from splink.duckdb.duckdb_linker import DuckDBLinker, DuckDBLinkerDataFrame
+from splink.linker import SplinkDataFrame
 
 from tests.basic_settings import get_settings_dict
 
 df = pd.read_csv("./tests/datasets/fake_1000_from_splink_demos.csv")
+dummy_frame = pd.DataFrame(["id"])
+
+
+def make_mock_execute(linker):
+    # creates a mock version of linker._execute_sql_against_backend,
+    # so we can count calls
+    dummy_splink_df = DuckDBLinkerDataFrame("template", "dummy_frame", linker)
+    mock_execute = create_autospec(
+        linker._execute_sql_against_backend,
+        return_value=dummy_splink_df
+    )
+    return mock_execute
 
 
 def test_cache_id(tmp_path):
@@ -68,3 +85,55 @@ def test_materialising_works():
     linker = DuckDBLinker(df, settings)
     linker._initialise_df_concat_with_tf(materialise=False)
     linker.compute_tf_table("first_name")
+
+
+# run test in/not in debug mode to check functionality in both - cache shouldn't care
+@pytest.mark.parametrize("debug_mode", (False, True))
+def test_cache_access_initialise_df_concat(debug_mode):
+    settings = get_settings_dict()
+
+    linker = DuckDBLinker(df, settings)
+    linker.debug_mode = debug_mode
+    with patch.object(
+            linker,
+            "_execute_sql_against_backend",
+            new=make_mock_execute(linker)
+            ) as mock_execute_sql_pipeline:
+        # shouldn't touch DB if we don't materialise
+        linker._initialise_df_concat_with_tf(materialise=False)
+        mock_execute_sql_pipeline.assert_not_called()
+
+        # this should create the table in the db
+        linker._initialise_df_concat_with_tf(materialise=True)
+        # NB don't specify amount of times it is called, as will depend on debug_mode
+        mock_execute_sql_pipeline.assert_called()
+        # reset the call counter on the mock
+        mock_execute_sql_pipeline.reset_mock()
+
+        # this should NOT touch the database, but instead use the cache
+        linker._initialise_df_concat_with_tf(materialise=True)
+        mock_execute_sql_pipeline.assert_not_called()
+
+        # this should also use the cache - concat will just refer to concat_with_tf
+        linker._initialise_df_concat(materialise=True)
+        mock_execute_sql_pipeline.assert_not_called()
+
+
+@pytest.mark.parametrize("debug_mode", (False, True))
+def test_cache_access_compute_tf_table(debug_mode):
+    settings = get_settings_dict()
+
+    linker = DuckDBLinker(df, settings)
+    linker.debug_mode = debug_mode
+    with patch.object(
+            linker,
+            "_execute_sql_against_backend",
+            new=make_mock_execute(linker)
+            ) as mock_execute_sql_pipeline:
+        linker.compute_tf_table("first_name")
+        mock_execute_sql_pipeline.assert_called()
+        # reset the call counter on the mock
+        mock_execute_sql_pipeline.reset_mock()
+
+        linker.compute_tf_table("first_name")
+        mock_execute_sql_pipeline.assert_not_called()
