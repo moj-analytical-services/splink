774e342f550ceba369c533c2bb6235c32e2546cd
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2023-02-16 10:09:58 +0000

    Quick test for nulls

--- tests/test_profile_data.py
+++ tests/test_profile_data.py
@@ -5,8 +5,36 @@ import numpy as np
 from splink.duckdb.duckdb_linker import DuckDBLinker
 from splink.sqlite.sqlite_linker import SQLiteLinker
 from splink.spark.spark_linker import SparkLinker
+from splink.misc import ensure_is_list
+from splink.profile_data import (
+    _col_or_expr_frequencies_raw_data_sql,
+)
 
 from basic_settings import get_settings_dict
+
+
+def generate_raw_profile_dataset(columns_to_profile):
+    # Grabs the underlying data created to form our profile_columns charts
+
+    df = pd.read_csv("./tests/datasets/fake_1000_from_splink_demos.csv")
+    df["blank"] = None
+    settings_dict = get_settings_dict()
+    linker = DuckDBLinker(df, settings_dict, connection=":memory:")
+
+    input_tablename = "__splink__df_concat_with_tf"
+    if not linker._table_exists_in_database("__splink__df_concat_with_tf"):
+        linker._initialise_df_concat()
+        input_tablename = "__splink__df_concat"
+
+    column_expressions_raw = ensure_is_list(columns_to_profile)
+
+    sql = _col_or_expr_frequencies_raw_data_sql(
+        column_expressions_raw,
+        input_tablename
+    )
+
+    linker._enqueue_sql(sql, "__splink__df_all_column_value_frequencies")
+    return linker._execute_sql_pipeline(materialise_as_hash=True).as_pandas_dataframe()
 
 
 def test_profile_using_duckdb():
@@ -19,6 +47,14 @@ def test_profile_using_duckdb():
         top_n=15,
         bottom_n=5,
     )
+
+    linker.profile_columns(
+        ["first_name", "surname", ["first_name", "surname"], ["city", "first_name"]],
+        top_n=15,
+        bottom_n=5,
+    )
+
+    assert len(generate_raw_profile_dataset([["first_name", "blank"]])) == 0
 
 
 def test_profile_using_duckdb_no_settings():
