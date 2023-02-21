75dd7d18e082aec5d269126959b44a8b22743f21
ADBond <48208438+ADBond@users.noreply.github.com>
2023-02-21 14:49:23 +0000

    correct demos path, adjust docs build script so it runs

--- scripts/make_docs_local.sh
+++ scripts/make_docs_local.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-UPDATE="TRUE"
 
-if [[ $UPDATE == "TRUE" ]]
-then
-    rm -rf demos/
-    git clone https://github.com/moj-analytical-services/splink_demos.git demos/
-fi
+# Need to update generated content separately using poetry env:
+# python scripts/json_schema_to_md_doc.py
+# python scripts/generate_dialect_comparison_docs.py
+
+# NB not including docs/demos, as 02_Exploratory_analysis.ipynb may
+# cause process to hang on: Running 3 `page_markdown` events
+
+# UPDATE="TRUE"
+# if [[ $UPDATE == "TRUE" ]]
+# then
+#     rm -rf docs/demos/
+#     git clone https://github.com/moj-analytical-services/splink_demos.git docs/demos/
+# fi
 
 deactivate
 python3 -m venv docs-venv
@@ -13,4 +20,5 @@ source docs-venv/bin/activate
 pip install --upgrade pip
 pip install -r scripts/docs-requirements.txt
 
-mkdocs serve
+# can remove verbose flag if needed
+mkdocs serve -v
