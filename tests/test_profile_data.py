035ac9cac6e97cf6062be0a46e200ed0b75de29d
zslade <zoe.slade@gmail.com>
2023-02-24 13:20:46 +0000

    Added tests to verify concat works for multiple columns

--- tests/test_profile_data.py
+++ tests/test_profile_data.py
@@ -12,49 +12,49 @@ from splink.profile_data import (
 
 from basic_settings import get_settings_dict
 
+from pyspark.sql.functions import lit
+from pyspark.sql.types import StringType
 
-def generate_raw_profile_dataset(columns_to_profile):
-    # Grabs the underlying data created to form our profile_columns charts
 
-    df = pd.read_csv("./tests/datasets/fake_1000_from_splink_demos.csv")
-    df["blank"] = None
-    settings_dict = get_settings_dict()
-    linker = DuckDBLinker(df, settings_dict, connection=":memory:")
+def generate_raw_profile_dataset(columns_to_profile, linker):
 
-    input_tablename = "__splink__df_concat_with_tf"
-    if not linker._table_exists_in_database("__splink__df_concat_with_tf"):
-        linker._initialise_df_concat()
-        input_tablename = "__splink__df_concat"
+    linker._initialise_df_concat()
 
     column_expressions_raw = ensure_is_list(columns_to_profile)
 
     sql = _col_or_expr_frequencies_raw_data_sql(
-        column_expressions_raw,
-        input_tablename
+        column_expressions_raw, "__splink__df_concat"
     )
 
     linker._enqueue_sql(sql, "__splink__df_all_column_value_frequencies")
+
     return linker._execute_sql_pipeline(materialise_as_hash=True).as_pandas_dataframe()
 
 
 def test_profile_using_duckdb():
     df = pd.read_csv("./tests/datasets/fake_1000_from_splink_demos.csv")
+    df["blank"] = None
     settings_dict = get_settings_dict()
     linker = DuckDBLinker(df, settings_dict, connection=":memory:")
 
     linker.profile_columns(
         ["first_name", "surname", "first_name || surname", "concat(city, first_name)"],
         top_n=15,
-        bottom_n=5,
+        bottom_n=15,
     )
-
     linker.profile_columns(
-        ["first_name", "surname", ["first_name", "surname"], ["city", "first_name"]],
+        [
+            "first_name",
+            ["surname"],
+            ["first_name", "surname"],
+            ["city", "first_name", "dob"],
+            ["first_name", "surname", "city", "dob"],
+        ],
         top_n=15,
-        bottom_n=5,
+        bottom_n=15,
     )
 
-    assert len(generate_raw_profile_dataset([["first_name", "blank"]])) == 0
+    assert len(generate_raw_profile_dataset([["first_name", "blank"]], linker)) == 0
 
 
 def test_profile_using_duckdb_no_settings():
@@ -65,7 +65,18 @@ def test_profile_using_duckdb_no_settings():
     linker.profile_columns(
         ["first_name", "surname", "first_name || surname", "concat(city, first_name)"],
         top_n=15,
-        bottom_n=5,
+        bottom_n=15,
+    )
+    linker.profile_columns(
+        [
+            "first_name",
+            ["surname"],
+            ["first_name", "surname"],
+            ["city", "first_name", "dob"],
+            ["first_name", "surname", "city", "dob"],
+        ],
+        top_n=15,
+        bottom_n=15,
     )
 
 
@@ -145,8 +156,24 @@ def test_profile_using_sqlite():
 # @pytest.mark.skip(reason="Uses Spark so slow and heavyweight")
 def test_profile_using_spark(df_spark):
     settings_dict = get_settings_dict()
+    df_spark = df_spark.withColumn("blank", lit(None).cast(StringType()))
     linker = SparkLinker(df_spark, settings_dict)
 
     linker.profile_columns(
-        ["first_name", "surname", "first_name || surname", "concat(city, first_name)"]
+        ["first_name", "surname", "first_name || surname", "concat(city, first_name)"],
+        top_n=15,
+        bottom_n=15,
     )
+    linker.profile_columns(
+        [
+            "first_name",
+            ["surname"],
+            ["first_name", "surname"],
+            ["city", "first_name", "dob"],
+            ["first_name", "surname", "city", "dob"],
+        ],
+        top_n=15,
+        bottom_n=15,
+    )
+
+    assert len(generate_raw_profile_dataset([["first_name", "blank"]], linker)) == 0
