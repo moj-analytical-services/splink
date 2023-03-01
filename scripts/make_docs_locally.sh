8ddb8ac8279d449ba6ace58b0a3a980234bfbbc0
ADBond <48208438+ADBond@users.noreply.github.com>
2023-03-01 12:47:37 +0000

    Update scripts/make_docs_locally.sh to include file generation
    Co-authored-by: Tom Hepworth <45356472+ThomasHepworth@users.noreply.github.com>

--- scripts/make_docs_locally.sh
+++ scripts/make_docs_locally.sh
@@ -16,7 +16,13 @@ python3 -m venv docs-venv
 source docs-venv/bin/activate
 pip install --upgrade pip
 pip install -r scripts/docs-requirements.txt
-
+# if you haven't run generate_dialect_comparison_docs.py, then install the splink dependencies and run
+if [[ ! -f "docs/includes/generated_files/comparison_level_library_dialect_table.md" ]]
+then
+    pip install poetry
+    poetry install
+    python3 scripts/generate_dialect_comparison_docs.py
+fi
 # manually preprocess include files as include-markdown plugin clashes with mknotebooks
 # make sure not to commit changes to files with inclusions!
 python3 scripts/preprocess_markdown_includes.py
