1e408284dcc672af7634e75c020017904ca57c93
Nick Crews <nicholas.b.crews@gmail.com>
2023-03-10 14:36:34 -0900

    ref: make ComparisonLevel.label_for_charts public
    It is used by these other classes so it really is public.
    And it is a public part of the API once you convert the
    ComparisonLevel to a dict, so it really is public.

--- splink/m_u_records_to_parameters.py
+++ splink/m_u_records_to_parameters.py
@@ -33,7 +33,7 @@ def not_trained_message(comparison_level: ComparisonLevel):
     cl = comparison_level
     return (
         f"not trained for {c._output_column_name} - "
-        f"{cl._label_for_charts} (comparison vector value: "
+        f"{cl.label_for_charts} (comparison vector value: "
         f"{cl._comparison_vector_value}). This usually means the "
         "comparison level was never observed in the training data."
     )
@@ -42,7 +42,6 @@ def not_trained_message(comparison_level: ComparisonLevel):
 def append_u_probability_to_comparison_level_trained_probabilities(
     comparison_level: ComparisonLevel, m_u_records_lookup, training_description
 ):
-
     cl = comparison_level
     c = cl.comparison
 
