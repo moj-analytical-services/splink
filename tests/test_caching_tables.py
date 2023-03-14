238c2a385e23bedf6d3881ef8ad53eb35a6fd5c1
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-03-14 10:11:12 +0000

    check deletions

--- tests/test_caching_tables.py
+++ tests/test_caching_tables.py
@@ -1,3 +1,4 @@
+import duckdb
 import pandas as pd
 import sqlglot
 from sqlglot.expressions import CTE, Identifier, TableAlias
@@ -9,15 +10,19 @@ from splink.duckdb.duckdb_comparison_library import (
 from splink.duckdb.duckdb_linker import DuckDBLinker
 
 
+def get_duckdb_table_names_as_list(con):
+
+    result = con.execute("SHOW TABLES")
+    tables = result.fetchall()
+    return [x[0] for x in tables]
+
+
 def test_cache_tracking_works():
 
     data = [
         {"unique_id": 1, "name": "Amanda"},
         {"unique_id": 2, "name": "Robin"},
         {"unique_id": 3, "name": "Robyn"},
-        {"unique_id": 4, "name": "David"},
-        {"unique_id": 5, "name": "Eve"},
-        {"unique_id": 6, "name": "Amanda"},
     ]
     df = pd.DataFrame(data)
 
@@ -215,7 +220,7 @@ def test_cache_invalidation():
     linker.compute_tf_table("name")
     len_after = len(cache.executed_queries)
 
-    # If cache not invalidated, cache used
+    # If cache not invalidated, cache should be used
     assert len_before == len_after
     assert cache.is_in_queries_retrieved_from_cache("__splink__df_tf_name")
 
@@ -227,6 +232,118 @@ def test_cache_invalidation():
     linker.invalidate_cache()
     linker.compute_tf_table("name")
     len_after = len(cache.executed_queries)
-    # If cache is invalidated, recomputed
+    # If cache is invalidated, an additional query should have been executed
     assert len_before + 1 == len_after
     assert not cache.is_in_queries_retrieved_from_cache("__splink__df_tf_name")
+
+
+def test_table_deletions():
+    con = duckdb.connect()
+    df = pd.DataFrame(
+        [
+            {"unique_id": 1, "name": "Amanda"},
+            {"unique_id": 2, "name": "Robin"},
+            {"unique_id": 3, "name": "Robyn"},
+        ]
+    )
+
+    con.execute("CREATE TABLE my_table AS SELECT * FROM df")
+
+    settings = {
+        "link_type": "dedupe_only",
+        "comparisons": [levenshtein_at_thresholds("name", 2)],
+        "blocking_rules_to_generate_predictions": ["l.name = r.name"],
+    }
+
+    linker = DuckDBLinker("my_table", settings, connection=con)
+
+    table_names_before = set(get_duckdb_table_names_as_list(linker._con))
+
+    linker.compute_tf_table("name")
+    linker.estimate_u_using_random_sampling(target_rows=1e4)
+
+    # # The database should be empty except for the original non-splink table
+    linker.delete_tables_created_by_splink_from_db()
+
+    table_names_after = set(get_duckdb_table_names_as_list(linker._con))
+    assert table_names_before == table_names_after
+    assert table_names_after == set(["my_table"])
+
+
+def test_table_deletions_with_preregistered():
+    con = duckdb.connect()
+    df = [
+        {"unique_id": 1, "name": "Amanda"},
+        {"unique_id": 2, "name": "Robin"},
+        {"unique_id": 3, "name": "Robyn"},
+    ]
+    df = pd.DataFrame(df)
+    con.execute("CREATE TABLE my_data_table AS SELECT * FROM df")
+
+    splink__df_concat_with_tf = [
+        {"unique_id": 1, "name": "Amanda", "tf_name": 0.3},
+        {"unique_id": 2, "name": "Robin", "tf_name": 0.2},
+        {"unique_id": 3, "name": "Robyn", "tf_name": 0.5},
+    ]
+    splink__df_concat_with_tf = pd.DataFrame(splink__df_concat_with_tf)
+    con.execute(
+        """
+        CREATE TABLE my_nodes_with_tf_table
+        AS SELECT * FROM splink__df_concat_with_tf
+        """
+    )
+
+    settings = {
+        "link_type": "dedupe_only",
+        "comparisons": [
+            levenshtein_at_thresholds("name", 2, term_frequency_adjustments=True)
+        ],
+        "blocking_rules_to_generate_predictions": ["l.name = r.name"],
+    }
+
+    linker = DuckDBLinker("my_data_table", settings, connection=con)
+    linker.register_table_input_nodes_concat_with_tf("my_nodes_with_tf_table")
+
+    table_names_before = set(get_duckdb_table_names_as_list(linker._con))
+
+    linker.compute_tf_table("name")
+    linker.estimate_u_using_random_sampling(target_rows=1e4)
+    # Note we shouldn't have executed a __splink__df_concat_with_tf query
+
+    cache = linker._intermediate_table_cache
+    assert not cache.is_in_executed_queries("__splink__df_concat_with_tf")
+
+    linker.delete_tables_created_by_splink_from_db()
+
+    table_names_after = set(get_duckdb_table_names_as_list(linker._con))
+
+    assert table_names_before == table_names_after
+
+
+def test_single_deletion():
+    data = [
+        {"unique_id": 1, "name": "Amanda"},
+        {"unique_id": 2, "name": "Robin"},
+        {"unique_id": 3, "name": "Robyn"},
+    ]
+    df = pd.DataFrame(data)
+
+    settings = {
+        "link_type": "dedupe_only",
+        "comparisons": [levenshtein_at_thresholds("name", 2)],
+        "blocking_rules_to_generate_predictions": ["l.name = r.name"],
+    }
+
+    linker = DuckDBLinker(df, settings)
+    cache = linker._intermediate_table_cache
+
+    tf_table = linker.compute_tf_table("name")
+    table_name = tf_table.physical_name
+    # Check it is in the cache and database
+    assert table_name in get_duckdb_table_names_as_list(linker._con)
+    assert "__splink__df_tf_name" in cache
+
+    tf_table.drop_table_from_database_and_remove_from_cache()
+    # Check it is no longer in the cache or database
+    assert table_name not in get_duckdb_table_names_as_list(linker._con)
+    assert "__splink__df_tf_name" not in cache
