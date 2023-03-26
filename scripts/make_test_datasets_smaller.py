d0c4dfa954d385b42a3bd19948b55a71e430eec9
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-03-26 19:40:25 +0100

    make everything tiny

--- scripts/make_test_datasets_smaller.py
+++ scripts/make_test_datasets_smaller.py
@@ -5,22 +5,23 @@ import pandas as pd
 
 
 def truncate_and_save(file_path, file_format):
+    print(f"truncating file {file_path}")
     if file_format == "csv":
         truncated_file_path = os.path.splitext(file_path)[0] + "_truncated.csv"
         with open(truncated_file_path, "w") as truncated_file:
-            subprocess.run(["head", "-n", "1001", file_path], stdout=truncated_file)
+            subprocess.run(["head", "-n", "101", file_path], stdout=truncated_file)
         os.rename(truncated_file_path, file_path)
     elif file_format == "parquet":
         df = pd.read_parquet(file_path)
-        df = df.head(1000)
-        df.to_parquet(file_path)
+        df = df.head(100)
+        df.to_parquet(file_path, index=False)
 
 
 def process_directory(directory):
     print("Starting truncating files")
     for root, _, files in os.walk(directory):
         for file in files:
-            print(f"truncating file {file}")
+
             file_path = os.path.join(root, file)
             file_ext = os.path.splitext(file)[-1].lower()
 
@@ -28,6 +29,10 @@ def process_directory(directory):
                 truncate_and_save(file_path, "csv")
             elif file_ext == ".parquet":
                 truncate_and_save(file_path, "parquet")
+    for root, _, files in os.walk(directory):
+        for file in files:
+            print("files that now exist are")
+            print(file)
 
 
 if __name__ == "__main__":
