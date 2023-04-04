c5ae95276895613230903c323cb1b63bb19b8a66
thomas.hepworth@justice.gov.uk <thomas.hepworth@justice.gov.uk>
2023-04-04 20:13:14 +0000

    update our athena tests

--- tests/test_full_example_athena.py
+++ tests/test_full_example_athena.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import pytest
 from basic_settings import get_settings_dict
+from pyarrow import csv
 from linker_utils import _test_table_registration
 
 from splink.athena.athena_comparison_library import levenshtein_at_thresholds
@@ -33,7 +34,7 @@ first_name_cc = levenshtein_at_thresholds(
     m_probability_else=0.1,
 )
 
-# Update tf weight and u probabilities to match v2 settings
+# Update tf weight and u probabilities to match
 first_name_cc._comparison_dict["comparison_levels"][1]._tf_adjustment_weight = 0.6
 u_probabilities_first_name = [0.1, 0.1, 0.8]
 for u_prob, level in zip(
@@ -45,44 +46,19 @@ for u_prob, level in zip(
 # Update settings w/ our edited first_name col
 settings_dict["comparisons"][0] = first_name_cc
 
-
-def setup_athena_db(my_session, db_name="splink_awswrangler_test"):
-    """
-    =====
-    Partially deprecated by the new garbage_collection function.
-    =====
-
-    Run this function if you need to create, or recreate the database(s)
-    used in this test.
-    """
-
-    # If our database already exists, delete and recreate it,
-    # so we ensure we have no cached data to read from.
-    if db_name in wr.catalog.databases(limit=10000).values:
-        wr.catalog.delete_database(name=db_name, boto3_session=my_session)
-        # clean up folder contents from s3...
-        # can potentially add this as a module to our awslinker
-        delete_s3_folder_contents("s3://alpha-splink-db-testing/splink_warehouse/")
-        delete_s3_folder_contents("s3://alpha-splink-db-testing/my_random_folder/")
-        delete_s3_folder_contents("s3://alpha-splink-db-testing/data/")
-
-    if db_name not in wr.catalog.databases(limit=10000).values:
-        import time
-
-        time.sleep(3)
-        wr.catalog.create_database(db_name, exist_ok=True)
-
-    return db_name
-
+# Setup database names for tests
+db_name_read = "splink_awswrangler_test"
+db_name_write = "data_linking_temp"
+output_bucket = "alpha-data-linking"
+table_name = "__splink__fake_1000_from_splink_demos"
 
 def upload_data(db_name):
     df = pd.read_csv("./tests/datasets/fake_1000_from_splink_demos.csv")
-    bucket = "alpha-splink-db-testing"
+    bucket = output_bucket
     # ensure data is in a unique loc to __splink tables
     path = f"s3://{bucket}/data/"
 
-    table_name = "fake_1000_from_splink_demos"
-    path = f"s3://{bucket}/data/"
+    path = f"s3://{bucket}/data/{table_name}"
     wr.s3.to_parquet(
         df=df,
         path=path,
@@ -90,45 +66,37 @@ def upload_data(db_name):
         mode="overwrite",
         database=db_name,
         table=table_name,
+        compression="snappy",
     )
-
-
-def create_and_upload_test_data(my_session):
-    db_name_read = setup_athena_db(my_session)
-    db_name_write = setup_athena_db(
-        my_session,
-        db_name="splink_awswrangler_test2",
-    )
-    upload_data("splink_awswrangler_test")
-
-    return db_name_read, db_name_write
 
 
 @pytest.mark.skip(reason="AWS Connection Required")
 def test_full_example_athena(tmp_path):
-    """
-    NOTE - we've changed this test. The dbs are now hard coded and are not
-    created on a new test run. Instead, we are utilising garbage_collection
-    in the AthenaLinker to clean the process before we proceed.
-    """
+    # This test assumes the databases in use have already been created
 
     # creates a session at least on the platform...
     my_session = boto3.Session(region_name="eu-west-1")
-    db_name_read = "splink_awswrangler_test"
-    db_name_write = f"{db_name_read}2"
+    
+    # Upload our raw data
+    upload_data(db_name_read)
 
     linker = AthenaLinker(
         settings_dict=settings_dict,
-        input_table_or_tables=f"{db_name_read}.fake_1000_from_splink_demos",
+        input_table_or_tables=f"{db_name_read}.{table_name}",
         boto3_session=my_session,
-        output_bucket="alpha-splink-db-testing",
+        output_bucket=output_bucket,
         output_database=db_name_write,
-        output_filepath="test_full_example",
-        garbage_collection_level=2,
+        output_filepath="athena_test_full_example",
     )
 
     linker.profile_columns(
-        ["first_name", "surname", "first_name || surname", "concat(city, first_name)"]
+        [
+            "first_name", 
+            "surname", 
+            "first_name || surname", 
+            "concat(city, first_name)",
+            ["surname", "city"],
+        ]
     )
     linker.compute_tf_table("city")
     linker.compute_tf_table("first_name")
@@ -158,57 +126,74 @@ def test_full_example_athena(tmp_path):
 
     linker.unlinkables_chart(source_dataset="Testing")
 
-    _test_table_registration(linker)
+    _test_table_registration(linker, skip_dtypes=True)
+    
+    # Clean up our database and s3 bucket and remove test files
+    linker.drop_all_tables_created_by_splink(delete_s3_folders=True)
+    linker.drop_splink_tables_from_database(database_name=db_name_read)
 
 
 @pytest.mark.skip(reason="AWS Connection Required")
 def test_athena_garbage_collection():
     # creates a session at least on the platform...
     my_session = boto3.Session(region_name="eu-west-1")
-    db_name_read = "splink_awswrangler_test"
-    db_name_write = f"{db_name_read}2"
+    upload_data(db_name_read)
+    
+    out_fp = "athena_test_garbage_collection"
 
-    # No cleaning...
-    AthenaLinker(
+    # Test that our 
+    linker = AthenaLinker(
         settings_dict=settings_dict,
-        input_table_or_tables=f"{db_name_read}.fake_1000_from_splink_demos",
+        input_table_or_tables=f"{db_name_read}.{table_name}",
         boto3_session=my_session,
-        output_bucket="alpha-splink-db-testing",
+        output_bucket=output_bucket,
         output_database=db_name_write,
-        garbage_collection_level=0,
-        output_filepath="test_full_example",
+        output_filepath=out_fp,
     )
+    
+    path = f"s3://{output_bucket}/{out_fp}/{linker._cache_uid}"
 
-    # Check everything gets cleaned up when initialising the linker
+    linker.profile_columns(
+        [
+            "first_name", 
+            "surname", 
+            "first_name || surname", 
+            "concat(city, first_name)",
+            ["surname", "city"],
+        ]
+    )
+    
+    predict = linker.predict()
+    
+    linker.drop_all_tables_created_by_splink(tables_to_exclude=predict)
+
+    # Check everything gets cleaned up excl. predict
     tables = wr.catalog.get_tables(
-        database="splink_awswrangler_test2",
+        database=db_name_write,
         name_prefix="__splink__df_predict",  # check if the predict table exists...
         boto3_session=my_session,
     )
-    assert sum(1 for _ in tables) > 0
+    assert sum(1 for _ in tables) == 1
 
-    # Check all files are also deleted (as gc = True)
+    # Check all files are also deleted (as delete_s3_folders = True)
     files = wr.s3.list_objects(
-        path="s3://alpha-splink-db-testing/test_full_example/",
+        path=path,
         boto3_session=my_session,
         ignore_empty=True,
     )
-    assert len(files) > 0
-
-    # Perform cleaning
-    AthenaLinker(
-        settings_dict=settings_dict,
-        input_table_or_tables=f"{db_name_read}.fake_1000_from_splink_demos",
-        boto3_session=my_session,
-        output_bucket="alpha-splink-db-testing",
-        output_database=db_name_write,
-        garbage_collection_level=2,
-        output_filepath="test_full_example",
+    assert len(files) > 0  # snappy, so n >= 1 parquet files created
+    
+    folder_exists = wr.s3.list_directories(
+        path,
     )
-
-    # Check everything gets cleaned up when initialising the linker
+    assert len(folder_exists) == 1  # check that only the predict table is saved
+    
+    # Now drop *everything*
+    linker.drop_all_tables_created_by_splink()
+    
+    # Does predict exist?
     tables = wr.catalog.get_tables(
-        database="splink_awswrangler_test2",
+        database=db_name_write,
         name_prefix="__splink",
         boto3_session=my_session,
     )
@@ -216,7 +201,7 @@ def test_athena_garbage_collection():
 
     # Check all files are also deleted (as gc = True)
     files = wr.s3.list_objects(
-        path="s3://alpha-splink-db-testing/splink_warehouse",
+        path=path,
         boto3_session=my_session,
         ignore_empty=True,
     )
@@ -224,27 +209,21 @@ def test_athena_garbage_collection():
 
 
 @pytest.mark.skip(reason="AWS Connection Required")
-def test_athena_df_as_input():
-    import pandas as pd
-
-    df = pd.read_csv("./tests/datasets/fake_1000_from_splink_demos.csv")
-
-    # creates a session at least on the platform...
+def test_pandas_as_input(df):
     my_session = boto3.Session(region_name="eu-west-1")
-    db_name_read = "splink_awswrangler_test"
-    db_name_write = f"{db_name_read}2"
 
     linker = AthenaLinker(
         input_table_or_tables=df,
         settings_dict=settings_dict,
         boto3_session=my_session,
-        output_bucket="alpha-splink-db-testing",
+        output_bucket=output_bucket,
         output_database=db_name_write,
-        garbage_collection_level=2,
-        output_filepath="test_full_example",
+        output_filepath="test_pandas_input",
+        input_table_aliases="__splink__testing",
     )
 
     linker.predict()
+    linker.drop_splink_tables_from_database()
 
 
 @pytest.mark.skip(reason="AWS Connection Required")
@@ -256,18 +235,19 @@ def test_athena_link_only():
     # creates a session at least on the platform...
     my_session = boto3.Session(region_name="eu-west-1")
     settings_dict["link_type"] = "link_and_dedupe"
-    db_name_read = "splink_awswrangler_test"
-    db_name_write = f"{db_name_read}2"
 
     linker = AthenaLinker(
         input_table_or_tables=[df, df],
         settings_dict=settings_dict,
         boto3_session=my_session,
-        output_bucket="alpha-splink-db-testing",
+        output_bucket=output_bucket,
         output_database=db_name_write,
-        garbage_collection_level=2,
-        output_filepath="test_full_example",
+        output_filepath="test_link_only",
+        input_table_aliases=["__splink__testing_1", "__splink__testing_2"],
     )
 
     df_predict = linker.predict()
     df_predict.as_pandas_dataframe()
+    
+    # Clean up
+    linker.drop_all_tables_created_by_splink(delete_s3_folders=True)
