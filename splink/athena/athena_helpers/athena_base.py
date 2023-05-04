532d741b8f71a084bb3e3ca080a7d8e1498c21d1
Ross Kennedy <ross.kennedy@digital.justice.gov.uk>
2023-05-04 15:06:01 +0100

    add basic datediff framework

--- splink/athena/athena_helpers/athena_base.py
+++ splink/athena/athena_helpers/athena_base.py
@@ -6,11 +6,36 @@ from ...dialect_base import (
 def size_array_intersect_sql(col_name_l, col_name_r):
     return f"cardinality(array_intersect({col_name_l}, {col_name_r}))"
 
-
 def regex_extract_sql(col_name, regex):
     return f"""
         regexp_extract({col_name}, '{regex}')
     """
+
+def datediff_sql(
+    col_name_l,
+    col_name_r,
+    date_threshold,
+    date_metric,
+    cast_str=False,
+    date_format=None,
+):
+    if date_format is None:
+        date_format = "%Y-%m-%d"
+
+    if cast_str:
+        return f"""
+            abs(date_diff('{date_metric}',
+                DATE {col_name_l}, '{date_format}'),
+                DATE({col_name_r}, '{date_format}'))
+                ) <= {date_threshold}
+        """
+    else:
+        return f"""
+            abs(date_diff('{date_metric}',
+                {col_name_l},
+                {col_name_r})
+                ) <= {date_threshold}
+        """
 
 
 class AthenaBase(DialectBase):
