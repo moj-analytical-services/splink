89a7a2afb32ef68249474d59ca8ed79996c838c3
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2023-05-18 18:09:17 +0100

    del both `numpy` and `duckdb`

--- scripts/duckdb-less_packages.sh
+++ scripts/duckdb-less_packages.sh
@@ -2,15 +2,16 @@
 
 # Set params
 requirements_file="requirements.txt"
-package_name="duckdb"
+packages=("duckdb" "numpy")
 
+rm -rf "$requirements_file"
 poetry export --without-hashes --only main  -f "$requirements_file" -o "$requirements_file"
 
 # Create a temporary file to store the modified content
 temp_file=$(mktemp)
 
 # Delete the line containing the package from the requirements file
-grep -v "^$package_name==" "$requirements_file" > "$temp_file"
+grep -vE "^($(IFS="|"; echo "${packages[*]}"))==" "$requirements_file" > "$temp_file"
 
 # Overwrite the original requirements file with the modified content
 mv "$temp_file" scripts/duckdbless_requirements.txt
