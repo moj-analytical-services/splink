68ef2c8ae5eae4e3173126313670b5722d0724b5
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2023-05-22 15:32:50 +0100

    rebase duckdb

--- splink/duckdb/duckdb_comparison_library.py
+++ splink/duckdb/duckdb_comparison_library.py
@@ -1,11 +1,11 @@
-from ..comparison_library import (
-    ArrayIntersectAtSizesComparisonBase,
-    DateDiffAtThresholdsComparisonBase,
-    DistanceFunctionAtThresholdsComparisonBase,
-    DistanceInKMAtThresholdsComparisonBase,
-    ExactMatchBase,
-    JaccardAtThresholdsComparisonBase,
-    JaroAtThresholdsComparisonBase,
-    JaroWinklerAtThresholdsComparisonBase,
-    LevenshteinAtThresholdsComparisonBase,
+from .duckdb_helpers.duckdb_comparison_imports import (  # noqa: F401
+    array_intersect_at_sizes,
+    datediff_at_thresholds,
+    distance_function_at_thresholds,
+    distance_in_km_at_thresholds,
+    exact_match,
+    jaccard_at_thresholds,
+    jaro_at_thresholds,
+    jaro_winkler_at_thresholds,
+    levenshtein_at_thresholds,
 )
