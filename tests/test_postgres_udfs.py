985cd79eb71b5f6a278e63c43e9f1de490c7d403
ADBond <48208438+ADBond@users.noreply.github.com>
2023-05-23 10:18:38 +0100

    test assertions more readable

--- tests/test_postgres_udfs.py
+++ tests/test_postgres_udfs.py
@@ -9,11 +9,12 @@ def test_log2(pg_engine):
         engine=pg_engine,
     )
     df = pd.DataFrame({"x": [2, 8, 0.5, 1]})
+    expected_log2_vals = [1, 3, -1, 0]
     linker.register_table(df, "log_values")
     sql = """SELECT log2("x") AS logs FROM log_values"""
     frame = linker._execute_sql_against_backend(
         sql, "dummy_name", "test_log_table"
     ).as_pandas_dataframe()
     
-    for log_result, expected in zip(frame["logs"], [1, 3, -1, 0]):
+    for log_result, expected in zip(frame["logs"], expected_log2_vals):
         assert log_result == expected
