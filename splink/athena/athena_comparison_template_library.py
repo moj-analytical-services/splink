954eb12ffa779516d1ac1a1a100bf7a882c56a43
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2023-06-07 13:02:25 +0100

    update linker and comparison scripts with pseudo links

--- splink/athena/athena_comparison_template_library.py
+++ splink/athena/athena_comparison_template_library.py
@@ -1,1 +1,11 @@
-from .comparison_template_library import *
+import warnings
+
+from .comparison_template_library import *  # noqa: F403
+
+warnings.warn(
+    "Importing directly from `splink.athena.comparison_template_library` "
+    "is deprecated and will be removed in Splink v4. "
+    "Please import from `splink.athena.comparison_template_library` going forward.",
+    DeprecationWarning,
+    stacklevel=2,
+)
