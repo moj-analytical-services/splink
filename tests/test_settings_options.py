41cf3ab43a726b28036798483fc986d48e6ba86f
ADBond <48208438+ADBond@users.noreply.github.com>
2023-06-09 16:41:56 +0100

    test data write to csv + read back in as spark has issues straight from pandas

--- tests/test_settings_options.py
+++ tests/test_settings_options.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+import os
 
 import pandas as pd
 
@@ -10,6 +10,7 @@ def test_model_heavily_customised_settings(test_helpers, dialect, tmp_path):
     helper = test_helpers[dialect]
 
     df_l = pd.read_csv("./tests/datasets/fake_1000_from_splink_demos.csv")
+    # modify the frame to have source dataset column + different unique id name
     uid = "uid_col"
     source_ds_col = "dataset_name"
     df_l = df_l.rename(columns={"unique_id": uid})
@@ -17,8 +18,14 @@ def test_model_heavily_customised_settings(test_helpers, dialect, tmp_path):
     df_l[source_ds_col] = "left_set"
     df_r[source_ds_col] = "right_set"
 
-    df_l = helper.convert_frame(df_l)
-    df_r = helper.convert_frame(df_r)
+    # save temporarily to csv + read in again
+    # older pyspark has issues with inferring types directly from pandas
+    tmp_path_df_l = os.path.join(tmp_path, "df_l.csv")
+    tmp_path_df_r = os.path.join(tmp_path, "df_r.csv")
+    df_l.to_csv(tmp_path_df_l)
+    df_r.to_csv(tmp_path_df_r)
+    df_l = helper.load_frame_from_csv(tmp_path_df_l)
+    df_r = helper.load_frame_from_csv(tmp_path_df_r)
 
     settings = {
         "link_type": "link_and_dedupe",
@@ -50,7 +57,7 @@ def test_model_heavily_customised_settings(test_helpers, dialect, tmp_path):
     linker.estimate_parameters_using_expectation_maximisation("l.dob = r.dob")
     df_predict = linker.predict(0.1)
     df_clusters = linker.cluster_pairwise_predictions_at_threshold(df_predict, 0.1)
-    linker.comparison_viewer_dashboard(df_predict, Path(tmp_path) / "csv.html")
+    linker.comparison_viewer_dashboard(df_predict, os.path.join(tmp_path, "csv.html"))
     linker.cluster_studio_dashboard(
-        df_predict, df_clusters, Path(tmp_path) / "csd.html"
+        df_predict, df_clusters, os.path.join(tmp_path, "csd.html")
     )
