d53178618a5f211d0b5f09383cffdc44b90a30e8
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2023-06-13 10:15:50 +0100

    fix deprecation warnings

--- splink/duckdb/duckdb_linker.py
+++ splink/duckdb/duckdb_linker.py
@@ -4,7 +4,7 @@ from ..exceptions import SplinkDeprecated
 from .linker import DuckDBLinker, DuckDBLinkerDataFrame  # noqa: F401
 
 warnings.warn(
-    "Importing directly from `splink.duckdb.linker` "
+    "Importing directly from `splink.duckdb.duckdb_linker` "
     "is deprecated and will be removed in Splink v4. "
     "Please import from `splink.duckdb.linker` going forward.",
     SplinkDeprecated,
