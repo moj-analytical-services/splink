cf9028e754487ca34030cf83f89b92cb1a613655
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2023-06-22 15:30:28 +0100

    lint our blocking rule scripts

--- splink/duckdb/blocking_rule_library.py
+++ splink/duckdb/blocking_rule_library.py
@@ -1,7 +1,6 @@
-from .duckdb_helpers.duckdb_blocking_rule_imports import exact_match_rule  # noqa: F401
-
 from ..blocking_rule_composition import (  # noqa: F401
     and_,
     not_,
     or_,
 )
+from .duckdb_helpers.duckdb_blocking_rule_imports import exact_match_rule  # noqa: F401
