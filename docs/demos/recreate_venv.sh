16cd591be03b9d82125b96fc31909b30251fdc13
Ross Kennedy <ross.kennedy@digital.justice.gov.uk>
2023-07-27 15:52:56 +0100

    remove excess files

--- docs/demos/recreate_venv.sh
+++
@@ -1,8 +0,0 @@
-source deactivate
-rm -rf venv/
-python3 -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
-
-pip install -r requirements.txt
-python -m ipykernel install --user --name=splink_demos
