78fbd116185ef14e0ee801a867bc84f01f73a808
Ross Kennedy <ross.kennedy@digital.justice.gov.uk>
2023-07-28 11:31:43 +0100

    minor test fix

--- splink/comparison_level_library.py
+++ splink/comparison_level_library.py
@@ -82,12 +82,14 @@ class NullLevelBase(ComparisonLevel):
         if invalid_dates_as_null:
             col_name_l = self._valid_date_function(col_name_l, valid_string_regex)
             col_name_r = self._valid_date_function(col_name_r, valid_string_regex)
+            sql = f"""{col_name_l} IS NULL OR {col_name_r} IS NULL OR
+                      {col_name_l}=='' OR {col_name_r} ==''"""
         elif valid_string_regex:
             col_name_l = self._regex_extract_function(col_name_l, valid_string_regex)
             col_name_r = self._regex_extract_function(col_name_r, valid_string_regex)
-
-        sql = f"""{col_name_l} IS NULL OR {col_name_r} IS NULL OR
-                      {col_name_l}=='' OR {col_name_r} ==''"""
+            sql = f"{col_name_l} IS NULL OR {col_name_r} IS NULL"
+        else:
+            sql = f"{col_name_l} IS NULL OR {col_name_r} IS NULL"
 
         level_dict = {
             "sql_condition": sql,
