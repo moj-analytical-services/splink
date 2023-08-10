2930911b07d6c1e26edd3e65104aeafc2f32f7e4
sama-ds <samsam.atkin@gmail.com>
2023-08-10 17:53:56 +0100

    Appeasing linter

--- splink/profile_data.py
+++ splink/profile_data.py
@@ -193,6 +193,7 @@ def _add_100_percentile_to_df_percentiles(percentile_rows):
 def profile_columns(linker, column_expressions=None, top_n=10, bottom_n=10):
 
     if column_expressions is None:
+        
         column_expressions = [
             col.input_name
             for df in linker._input_tables_dict.values()
