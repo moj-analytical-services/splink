e283ec0516561cb1435751cc0aab3d4839a52dc6
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2023-08-15 16:20:59 +0100

    add in a check to see if `docs-venv` is already active

--- scripts/make_docs_locally.sh
+++ scripts/make_docs_locally.sh
@@ -4,19 +4,24 @@
 # python scripts/json_schema_to_md_doc.py
 # python scripts/generate_dialect_comparison_docs.py
 
-deactivate
-python3 -m venv docs-venv
-source docs-venv/bin/activate
-pip install --upgrade pip
-pip install -r scripts/docs-requirements.txt
-# if you haven't run generate_dialect_comparison_docs.py, then install the splink dependencies and run
-if [[ ! -f "docs/includes/generated_files/comparison_level_library_dialect_table.md" ]]
-then
+if [[ "$VIRTUAL_ENV" == *"/docs-venv"* ]]; then
+
+    deactivate
+    python3 -m venv docs-venv
+    source docs-venv/bin/activate
+    pip install --upgrade pip
+    pip install -r scripts/docs-requirements.txt
     pip install poetry
     poetry install
+
+fi
+
+# if you haven't run generate_dialect_comparison_docs.py, then install the splink dependencies and run
+if [[ ! -f "docs/includes/generated_files/comparison_level_library_dialect_table.md" ]]; then
     python3 scripts/generate_dialect_comparison_docs.py
     python3 scripts/generate_dataset_docs.py
 fi
+
 # manually preprocess include files as include-markdown plugin clashes with mknotebooks
 # make sure not to commit changes to files with inclusions!
 python3 scripts/preprocess_markdown_includes.py
