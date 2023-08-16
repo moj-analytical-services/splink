6ad8b2c09df63dc266a24315218929eea9e8ef9e
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2023-08-16 21:38:57 +0100

    change composition output if only one BR is supplied

--- splink/blocking_rule_composition.py
+++ splink/blocking_rule_composition.py
@@ -313,7 +313,11 @@ def _br_merge(
         raise ValueError("You must provide at least one BlockingRule")
 
     brs, sql_dialect, salt = _parse_blocking_rules(*brls)
-    conditions = (f"({br.blocking_rule})" for br in brs)
+    if len(brs) > 1:
+        conditions = (f"({br.blocking_rule})" for br in brs)
+    else:
+        conditions = (br.blocking_rule for br in brs)
+
     blocking_rule = f" {clause} ".join(conditions)
 
     return BlockingRule(
