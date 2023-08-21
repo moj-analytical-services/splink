115b814525d889411b23d04606ea4e4141ffc80a
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2023-08-21 13:19:55 +0100

    update pyroject to allow poetry to install all spark requirements

--- scripts/test_poetry_build.sh
+++ scripts/test_poetry_build.sh
@@ -15,4 +15,5 @@ folder_path="dist"
 # Use find command to get paths of all .whl files within the folder
 whl_files=$(find "$folder_path" -type f -name "*.whl")
 # pip install
-pip3 install "$whl_files"
+# pip3 install "$whl_files"
+pip3 install $whl_files'[spark]'
