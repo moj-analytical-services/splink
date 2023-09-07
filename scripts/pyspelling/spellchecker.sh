50de16e8ff05b389831393580865e2cc8c6153ce
zslade <zoe.slade@gmail.com>
2023-09-07 18:41:03 +0100

    Relocated scripts

--- scripts/pyspelling/spellchecker.sh
+++ scripts/pyspelling/spellchecker.sh
@@ -8,11 +8,16 @@ then
     python3 -m venv spellcheck-venv                                                                 
     source spellcheck-venv/bin/activate
     python -m pip install pyspelling
-    # need to install aspell
+    # Install aspell
+    brew install aspell
+    # Donwload dictionary files into correct directory
+    curl -LJ https://github.com/LibreOffice/dictionaries/raw/master/en/en_GB.dic -o ~/Library/Spelling/en_GB.dic
+    curl -LJ https://github.com/LibreOffice/dictionaries/blob/master/en/en_GB.aff -o ~/Library/Spelling/en_GB.aff
+
 fi
 
 line_block="=============="
 pyspelling_run="Running pyspelling spellchecker on docs"
 echo "$line_block $pyspelling_run $line_block"
 
-pyspelling
+pyspelling -c ./scripts/pyspelling/.pyspelling.yml
