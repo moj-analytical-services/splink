91de304a8b682bd551fd256c4046703aa6fb15a8
zslade <zoe.slade@gmail.com>
2023-09-07 19:57:43 +0100

    Updated bash script with checks for installs

--- scripts/pyspelling/spellchecker.sh
+++ scripts/pyspelling/spellchecker.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+line_block="=============="
+
+package_name="aspell"
+
+# Check if Homebrew is installed
+if command -v brew >/dev/null 2>&1; then
+    # Check if aspell is installed
+    if brew list --formula | grep -q "${package_name}"; then
+        :
+    else
+        echo "Spellchecker package ${package_name} is not installed."
+        echo "$line_block Installing ${package_name} $line_block"
+        brew install aspell
+    fi
+else
+    echo "Homebrew is not installed on this system. Please install via https://brew.sh/"
+    # exit 1
+fi
+
 cwd=$(pwd)
 
 if [[ "$VIRTUAL_ENV" != "$cwd/spellcheck-venv" ]]
@@ -8,15 +27,12 @@ then
     python3 -m venv spellcheck-venv                                                                 
     source spellcheck-venv/bin/activate
     python -m pip install pyspelling
-    # Install aspell
-    brew install aspell
     # Donwload dictionary files into correct directory
+
     curl -LJ https://github.com/LibreOffice/dictionaries/raw/master/en/en_GB.dic -o ~/Library/Spelling/en_GB.dic
     curl -LJ https://github.com/LibreOffice/dictionaries/blob/master/en/en_GB.aff -o ~/Library/Spelling/en_GB.aff
-
 fi
 
-line_block="=============="
 pyspelling_run="Running pyspelling spellchecker on docs"
 echo "$line_block $pyspelling_run $line_block"
 
