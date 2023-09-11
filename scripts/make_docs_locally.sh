42674e2585c62aba28240ef712316f779e8124ac
Tom Hepworth <thomas.hepworth@justice.gov.uk>
2023-09-11 12:34:07 +0100

    build docs table from latest splink build

--- scripts/make_docs_locally.sh
+++ scripts/make_docs_locally.sh
@@ -7,7 +7,7 @@
 if [[ "$VIRTUAL_ENV" != "$cwd/docs-venv" ]]; then
     if [ -n "$VIRTUAL_ENV" ]; then
         deactivate
-    else    
+    else
         :
     fi
     python3 -m venv docs-venv
@@ -19,8 +19,8 @@ fi
 # if you haven't run generate_dialect_comparison_docs.py, then install the splink dependencies and run
 if [[ ! -f "docs/includes/generated_files/comparison_level_library_dialect_table.md" ]]
 then
-    pip install poetry
-    poetry install
+    # add the latest Splink build to the deployment
+    source scripts/test_poetry_build.sh --current_venv
     python3 scripts/generate_dialect_comparison_docs.py
     python3 scripts/generate_dataset_docs.py
 fi
--- scripts/test_poetry_build.sh
+++ scripts/test_poetry_build.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-deactivate
-# Delete the dist folder if it exists
-rm -rf dist/
-rm -rf venv
+# If the user requests that the current virtual
+# environment be used - "--current_venv" -
+# install poetry (if not already installed) and the latest Splink build.
+# Otherwise, deactivate and create a new venv.
+if [ "$1" == "--current_venv" ]; then
+    pip3 install poetry
+else
+    deactivate
+    # Delete the dist folder if it exists
+    rm -rf dist/
+    rm -rf venv
 
-# Setup python and build the package
-python3 -m venv venv
-source venv/bin/activate
+    # Setup python and build the package
+    python3 -m venv venv
+    source venv/bin/activate
+fi
+
+# Create the wheel for the poetry build
 poetry build --format=wheel --verbose
 
 # Find the `.whl` file
