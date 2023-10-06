c18067b7e8a600876a0559f9e9934957ec9e238f
ADBond <48208438+ADBond@users.noreply.github.com>
2023-10-06 13:23:26 +0100

    exact_match_colnames tests --- +ve and -ve versions

--- tests/test_comparison_level.py
+++ tests/test_comparison_level.py
@@ -1,4 +1,4 @@
-from pytest import mark
+from pytest import mark, raises
 
 from splink.comparison_level import ComparisonLevel
 
@@ -14,20 +14,47 @@ def make_comparison_level(sql_condition, dialect):
         sql_dialect=dialect,
     )
 
+
 # SQL conditions that are of 'exact match' type
-exact_matchy_sql_conditions = [
-    "col_l = col_r",
-    "col_l = col_r AND another_col_l = another_col_r",
-    "col_l = col_r AND another_col_l = another_col_r AND third_l = third_r",
-    "(col_l = col_r AND another_col_l = another_col_r) AND third_l = third_r",
-    "col_l = col_r AND (another_col_l = another_col_r AND third_l = third_r)",
+exact_matchy_sql_conditions_and_columns = [
+    ("col_l = col_r", ["col"]),
+    ("col_l = col_r AND another_col_l = another_col_r", ["col", "another_col"]),
+    (
+        "col_l = col_r AND another_col_l = another_col_r AND third_l = third_r",
+        ["col", "another_col", "third"],
+    ),
+    (
+        "(col_l = col_r AND another_col_l = another_col_r) AND third_l = third_r",
+        ["col", "another_col", "third"],
+    ),
+    (
+        "col_l = col_r AND (another_col_l = another_col_r AND third_l = third_r)",
+        ["col", "another_col", "third"],
+    ),
 ]
 
-@mark.parametrize("sql_condition", exact_matchy_sql_conditions)
+
+@mark.parametrize(
+    "sql_condition, exact_match_cols", exact_matchy_sql_conditions_and_columns
+)
 @mark_with_dialects_excluding()
-def test_exact_match_checks_exact_matchy_levels(sql_condition, dialect):
+def test_is_exact_match_for_exact_matchy_levels(
+    sql_condition, exact_match_cols, dialect
+):
     lev = make_comparison_level(sql_condition, dialect)
     assert lev._is_exact_match
+
+
+@mark.parametrize(
+    "sql_condition, exact_match_cols", exact_matchy_sql_conditions_and_columns
+)
+@mark_with_dialects_excluding()
+def test_exact_match_colnames_for_exact_matchy_levels(
+    sql_condition, exact_match_cols, dialect
+):
+    lev = make_comparison_level(sql_condition, dialect)
+    assert lev._exact_match_colnames == exact_match_cols
+
 
 # SQL conditions that are NOT of 'exact match' type
 non_exact_matchy_sql_conditions = [
@@ -40,8 +67,19 @@ non_exact_matchy_sql_conditions = [
     "substr(col_l, 2) = substr(col_r, 2)",
 ]
 
+
 @mark.parametrize("sql_condition", non_exact_matchy_sql_conditions)
 @mark_with_dialects_excluding()
-def test_exact_match_checks_non_exact_matchy_levels(sql_condition, dialect):
+def test_is_exact_match_for_non_exact_matchy_levels(sql_condition, dialect):
     lev = make_comparison_level(sql_condition, dialect)
     assert not lev._is_exact_match
+
+
+@mark.parametrize("sql_condition", non_exact_matchy_sql_conditions)
+@mark_with_dialects_excluding()
+def test_exact_match_colnames_for_non_exact_matchy_levels(sql_condition, dialect):
+    lev = make_comparison_level(sql_condition, dialect)
+    # _exact_match_colnames should have an error if it is
+    # not actually an exact match level
+    with raises(ValueError):
+        lev._exact_match_colnames
