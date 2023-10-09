dae661427409065894d318c53197f87961fc5eb5
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-10-09 14:01:25 +0100

    add licences automatically

--- splink/files/splink_vis_utils/splink_vis_utils.js
+++ splink/files/splink_vis_utils/splink_vis_utils.js
@@ -1,3 +1,316 @@
+/**
+ * Name: lodash.clonedeep
+ * Version: 4.5.0
+ * License: MIT
+ * Private: false
+ * Description: The lodash method `_.cloneDeep` exported as a module.
+ * Repository: undefined
+ * Homepage: https://lodash.com/
+ * Author: John-David Dalton <john.david.dalton@gmail.com> (http://allyoucanleet.com/)
+ * Contributors:
+ * John-David Dalton <john.david.dalton@gmail.com> (http://allyoucanleet.com/)
+ * Blaine Bublitz <blaine.bublitz@gmail.com> (https://github.com/phated)
+ * Mathias Bynens <mathias@qiwi.be> (https://mathiasbynens.be/)
+ * License Copyright:
+ * ===
+ *
+ * Copyright jQuery Foundation and other contributors <https://jquery.org/>
+ *
+ * Based on Underscore.js, copyright Jeremy Ashkenas,
+ * DocumentCloud and Investigative Reporters & Editors <http://underscorejs.org/>
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals. For exact contribution history, see the revision history
+ * available at https://github.com/lodash/lodash
+ *
+ * The following license applies to all parts of this software except as
+ * documented below:
+ *
+ * ====
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * ====
+ *
+ * Copyright and related rights for sample code are waived via CC0. Sample
+ * code is defined as all source code displayed within the prose of the
+ * documentation.
+ *
+ * CC0: http://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * ====
+ *
+ * Files located in the node_modules and vendor directories are externally
+ * maintained libraries used by this software which have their own
+ * licenses; we recommend you read them, as their terms may differ from the
+ * terms above.
+ *
+ *
+ * ---
+ *
+ * Name: @observablehq/inspector
+ * Version: 3.2.4
+ * License: ISC
+ * Private: false
+ * Repository: https://github.com/observablehq/inspector.git
+ * Author: Observable, Inc. (https://observablehq.com)
+ * License Copyright:
+ * ===
+ *
+ * Copyright 2018-2021 Observable, Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ *
+ *
+ * ---
+ *
+ * Name: isoformat
+ * Version: 0.2.1
+ * License: ISC
+ * Private: false
+ * Description: A tidy ISO 8601 date formatter and parser
+ * Repository: https://github.com/mbostock/isoformat.git
+ * Homepage: https://github.com/mbostock/isoformat/
+ * Author: Mike Bostock (https://bost.ocks.org/mike)
+ * License Copyright:
+ * ===
+ *
+ * Copyright 2021 Mike Bostock
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ *
+ *
+ * ---
+ *
+ * Name: d3-dsv
+ * Version: 2.0.0
+ * License: BSD-3-Clause
+ * Private: false
+ * Description: A parser and formatter for delimiter-separated values, such as CSV and TSV
+ * Repository: https://github.com/d3/d3-dsv.git
+ * Homepage: https://d3js.org/d3-dsv/
+ * Author: Mike Bostock (http://bost.ocks.org/mike)
+ * License Copyright:
+ * ===
+ *
+ * Copyright 2013-2016 Mike Bostock
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the author nor the names of contributors may be used to
+ * endorse or promote products derived from this software without specific prior
+ * written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * ---
+ *
+ * Name: d3-require
+ * Version: 1.2.4
+ * License: ISC
+ * Private: false
+ * Description: A minimal, promise-based implementation to require asynchronous module definitions.
+ * Repository: https://github.com/d3/d3-require.git
+ * Author: Observable, Inc.
+ * License Copyright:
+ * ===
+ *
+ * Copyright 2017-2019 Observable, Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ *
+ *
+ * ---
+ *
+ * Name: @observablehq/stdlib
+ * Version: 3.17.0
+ * License: ISC
+ * Private: false
+ * Repository: https://github.com/observablehq/stdlib.git
+ * Author: Observable, Inc. (https://observablehq.com)
+ * License Copyright:
+ * ===
+ *
+ * Copyright 2018-2021 Observable, Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ *
+ *
+ * ---
+ *
+ * Name: @observablehq/runtime
+ * Version: 4.20.0
+ * License: ISC
+ * Private: false
+ * Repository: https://github.com/observablehq/runtime.git
+ * Author: Observable, Inc. (https://observablehq.com)
+ * License Copyright:
+ * ===
+ *
+ * Copyright 2018-2021 Observable, Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ *
+ *
+ * ---
+ *
+ * Name: htl
+ * Version: 0.3.1
+ * License: ISC
+ * Private: false
+ * Description: Hypertext Literal
+ * Homepage: https://github.com/observablehq/htl
+ * Author: Mike Bostock (https://bost.ocks.org/mike)
+ * License Copyright:
+ * ===
+ *
+ * Copyright 2019-2021 Observable, Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ *
+ *
+ * ---
+ *
+ * Name: @observablehq/inputs
+ * Version: 0.10.4
+ * License: ISC
+ * Private: false
+ * Description: User interface components for Observable notebooks
+ * Repository: https://github.com/observablehq/inputs.git
+ * Author: Observable, Inc. (https://observablehq.com)
+ * License Copyright:
+ * ===
+ *
+ * Copyright 2021 Observable, Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ *
+ *
+ * ---
+ *
+ * Name: 5496e67e6ca05c7b
+ * Version: 1784.0.0
+ * License: null
+ * Private: false
+ * Homepage: https://observablehq.com/d/5496e67e6ca05c7b
+ * Author: Robin Linacre (https://observablehq.com/@robinl)
+ *
+ * ---
+ *
+ * Name: c43e4e0aeb54d263
+ * Version: 684.0.0
+ * License: null
+ * Private: false
+ * Homepage: https://observablehq.com/d/c43e4e0aeb54d263
+ * Author: Robin Linacre (https://observablehq.com/@robinl)
+ */
+
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
