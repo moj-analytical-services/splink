86d609c03a7562753b18f995326b30be2379c4f8
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-10-10 09:24:03 +0100

    https://github.com/moj-analytical-services/splink/pull/1016

--- splink/files/splink_vis_utils/splink_vis_utils.js
+++ splink/files/splink_vis_utils/splink_vis_utils.js
@@ -9441,14 +9441,15 @@ ${splink_vis_utils.comparison_column_table(selected_edge, ss)}`;
 	) {
 	  let base_spec_2 = cloneDeep(base_spec);
 	  let sort_field;
+	  data.forEach((d) => {
+	    d.sum_matches = d.match_probability * d.count;
+	  });
 	  if (sort_bars == "sort_match_weight") {
 	    data.sort(sort_match_weight);
 	    sort_field = "match_weight";
 	  }
 	  if (sort_bars == "sort_sum_matches") {
-	    data.forEach((d) => {
-	      d.sum_matches = d.match_probability * d.count;
-	    });
+
 	    data.sort(sort_sum_matches);
 	    sort_field = "sum_matches";
 	  }
