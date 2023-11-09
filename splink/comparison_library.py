c907f17ebb47f0c4339890c860d5d033e7dbf850
ADBond <48208438+ADBond@users.noreply.github.com>
2023-11-09 10:46:57 +0000

    basic version of LevenshteinAtThresholds

--- splink/comparison_library.py
+++ splink/comparison_library.py
@@ -1,19 +1,20 @@
-from typing import List
+from typing import Iterable, List, Union
 
 from . import comparison_level_library as cll
 from .comparison_creator import ComparisonCreator
 from .comparison_level_creator import ComparisonLevelCreator
 from .dialects import SplinkDialect
+from .misc import ensure_is_iterable
 
 
 class ExactMatch(ComparisonCreator):
     """
-        Represents a comparison of the data in `col_name` with two levels:
-            - Exact match in `col_name`
-            - Anything else
+    Represents a comparison of the data in `col_name` with two levels:
+        - Exact match in `col_name`
+        - Anything else
 
-        Args:
-            col_name (str): The name of the column to compare
+    Args:
+        col_name (str): The name of the column to compare
     """
 
     def create_comparison_levels(
@@ -27,6 +28,43 @@ class ExactMatch(ComparisonCreator):
 
     def create_description(self) -> str:
         return f"Exact match '{self.col_name}' vs. anything else"
+
+    def create_output_column_name(self) -> str:
+        return self.col_name
+
+
+class LevenshteinAtThresholds(ComparisonCreator):
+    def __init__(
+        self,
+        col_name: str,
+        distance_threshold_or_thresholds: Union[Iterable[int], int] = [1, 2],
+    ):
+        thresholds_as_iterable = ensure_is_iterable(distance_threshold_or_thresholds)
+        # unpack it to a list so we can repeat iteration if needed
+        self.thresholds = [*thresholds_as_iterable]
+        super().__init__(col_name)
+
+    def create_comparison_levels(
+        self, sql_dialect: SplinkDialect
+    ) -> List[ComparisonLevelCreator]:
+        return [
+            cll.NullLevel(self.col_name),
+            cll.ExactMatchLevel(self.col_name),
+            *[
+                cll.LevenshteinLevel(self.col_name, threshold)
+                for threshold in self.thresholds
+            ],
+            cll.ElseLevel(),
+        ]
+
+    def create_description(self) -> str:
+        comma_separated_thresholds_string = ", ".join(map(str, self.thresholds))
+        return (
+            f"Exact match '{self.col_name}' vs. "
+            f"Levenshtein distance at thresholds "
+            f"{comma_separated_thresholds_string} vs. "
+            "anything else"
+        )
 
     def create_output_column_name(self) -> str:
         return self.col_name
