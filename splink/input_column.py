3cc6f69107f1399a530f5231e420c60b51d837d4
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-12-04 11:15:54 +0000

    refactor following discussion about approach

--- splink/input_column.py
+++ splink/input_column.py
@@ -58,7 +58,18 @@ class SqlglotColumnTreeBuilder:
         if self.alias is None:
             return tree
         else:
-            return column
+            return exp.alias_(tree, self.alias)
+
+    @property
+    def as_sqlglot_tree(self):
+        tree = sqlglot.column(
+            col=self.column_name, table=self.table, quoted=self.quoted
+        )
+        if self._has_key_or_index:
+            tree = self._add_key_or_index_to_tree(tree)
+        if self.alias:
+            return exp.alias_(tree, self.alias, quoted=self.quoted)
+        return tree
 
     @classmethod
     def from_raw_column_name_or_column_reference(cls, input_str, sqlglot_dialect):
