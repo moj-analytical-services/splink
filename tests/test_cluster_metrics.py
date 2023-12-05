b835e8253210870c000d57c25f2ab8ccd46e1f69
ADBond <48208438+ADBond@users.noreply.github.com>
2023-12-05 16:02:49 +0000

    translate test_cluster_metrics

--- tests/test_cluster_metrics.py
+++ tests/test_cluster_metrics.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
-from splink.duckdb.duckdb_comparison_library import (
-    exact_match,
+from splink.comparison_library import (
+    ExactMatch,
 )
 from splink.duckdb.linker import DuckDBLinker
 
@@ -26,9 +26,9 @@ def test_size_density_dedupe():
         "probability_two_random_records_match": 0.01,
         "link_type": "dedupe_only",
         "comparisons": [
-            exact_match("first_name"),
-            exact_match("surname"),
-            exact_match("dob"),
+            ExactMatch("first_name"),
+            ExactMatch("surname"),
+            ExactMatch("dob"),
         ],
     }
     linker = DuckDBLinker(df_1, settings)
@@ -54,9 +54,9 @@ def test_size_density_link():
         "probability_two_random_records_match": 0.01,
         "link_type": "link_only",
         "comparisons": [
-            exact_match("first_name"),
-            exact_match("surname"),
-            exact_match("dob"),
+            ExactMatch("first_name"),
+            ExactMatch("surname"),
+            ExactMatch("dob"),
         ],
     }
     linker = DuckDBLinker(
