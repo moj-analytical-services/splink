422d2976ef4dd7ed1d2beefde2d5ca247eb9a3dc
ADBond <48208438+ADBond@users.noreply.github.com>
2023-12-05 15:40:43 +0000

    coerce dicts to ComparisonLevelCreator in logical creators

--- splink/comparison_level_composition.py
+++ splink/comparison_level_composition.py
@@ -5,7 +5,22 @@ from typing import Iterable, Union, final
 from .blocking import BlockingRule
 from .comparison_creator import ComparisonLevelCreator
 from .comparison_level import ComparisonLevel
+from .comparison_level_library import CustomLevel
 from .dialects import SplinkDialect
+
+
+def _ensure_is_comparison_level_creator(
+    cl: Union[ComparisonLevelCreator, dict]
+) -> ComparisonLevelCreator:
+    if isinstance(cl, dict):
+        # TODO: proper dict => level method
+        return CustomLevel(**dict)
+    if isinstance(cl, ComparisonLevelCreator):
+        return cl
+    raise TypeError(
+        f"parameter 'cl' must be a `ComparisonLevelCreator` or `dict`, "
+        f"but is of type {type(cl)} [{cl}]"
+    )
 
 
 class _Merge(ComparisonLevelCreator):
@@ -14,7 +29,10 @@ class _Merge(ComparisonLevelCreator):
         num_levels = len(comparison_levels)
         if num_levels == 0:
             raise ValueError(f"Must provide at least one level to {type(self)}()")
-        self.comparison_levels = comparison_levels
+        self.comparison_levels = [
+            _ensure_is_comparison_level_creator(cl)
+            for cl in comparison_levels
+        ]
         self.is_null_level = all(cl.is_null_level for cl in comparison_levels)
 
     @final
@@ -40,7 +58,7 @@ class Or(_Merge):
 
 class Not(ComparisonLevelCreator):
     def __init__(self, comparison_level: Union[ComparisonLevelCreator, dict]):
-        self.comparison_level = comparison_level
+        self.comparison_level = _ensure_is_comparison_level_creator(comparison_level)
         # turn null levels into non-null levels, otherwise do nothing
         if comparison_level.is_null_level:
             self.is_null_level = False
