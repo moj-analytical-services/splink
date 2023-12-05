cc27688e33f310612ca722ad4cf67a36905966a5
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-12-05 12:19:39 +0000

    clarify docstring

--- splink/input_expression.py
+++ splink/input_expression.py
@@ -47,15 +47,17 @@ class InputExpression:
         that the user will specify in their linker.
         """
 
-        # If there's a () or || then assume it's a valid sql expression
+        # It's difficult (possibly impossible) to find a completely general
+        # algorithm that can distinguish between the two cases (col name, or sql
+        # expression), since lower(first_name) could technically be a column name
+        # Here I use a heuristic:
+        # If there's a () or || then assume it's a sql expression
         if re.search(r"\([^)]*\)", self.raw_sql_expression):
             return self.raw_sql_expression
-
         elif "||" in self.raw_sql_expression:
             return self.raw_sql_expression
 
-        # Otherwise, assume it's an unquoted column name or reference
-        # which needs quoting
+        # Otherwise, assume it's a column name or reference which may need quoting
         return SqlglotColumnTreeBuilder.from_raw_column_name_or_column_reference(
             self.raw_sql_expression, sqlglot_dialect
         ).sql
