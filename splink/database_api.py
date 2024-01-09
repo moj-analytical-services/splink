c3323c83a316abe913fb250688eb9ab24745fdfa
ADBond <48208438+ADBond@users.noreply.github.com>
2024-01-09 17:32:34 +0000

    fix incorrect argument order in table registration

--- splink/database_api.py
+++ splink/database_api.py
@@ -126,7 +126,7 @@ class DatabaseAPI(ABC, Generic[TablishType]):
             if not isinstance(table, str):
                 self._table_registration(table, alias)
                 table = alias
-            sdf = self.table_to_splink_dataframe(table, alias)
+            sdf = self.table_to_splink_dataframe(alias, table)
             tables_as_splink_dataframes[alias] = sdf
         return tables_as_splink_dataframes
 
