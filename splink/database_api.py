8ee18afcb1685b775b23f217662b7262537b6e9c
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-01-24 13:58:25 +0000

    (WIP) Add SQLite DB API

--- splink/database_api.py
+++ splink/database_api.py
@@ -2,6 +2,7 @@ import logging
 import math
 import os
 import re
+import sqlite3
 from abc import ABC, abstractmethod
 from itertools import compress
 from tempfile import TemporaryDirectory
@@ -16,7 +17,7 @@ from pyspark.sql.utils import AnalysisException
 
 from .cache_dict_with_logging import CacheDictWithLogging
 from .databricks.enable_splink import enable_splink
-from .dialects import DuckDBDialect, SparkDialect, SplinkDialect
+from .dialects import DuckDBDialect, SparkDialect, SplinkDialect, SqliteDialect
 from .duckdb.duckdb_helpers.duckdb_helpers import (
     create_temporary_duckdb_connection,
     duckdb_load_from_file,
@@ -29,6 +30,7 @@ from .misc import major_minor_version_greater_equal_than
 from .spark.jar_location import get_scala_udfs
 from .spark.linker import SparkDataFrame
 from .splink_dataframe import SplinkDataFrame
+from .sqlite.linker import SQLiteDataFrame
 
 logger = logging.getLogger(__name__)
 
@@ -206,6 +208,7 @@ class DatabaseAPI(ABC, Generic[TablishType]):
 
 # alias for brevity:
 ddb_con = duckdb.DuckDBPyConnection
+sql_con = sqlite3.Connection
 
 
 class DuckDBAPI(DatabaseAPI):
@@ -643,3 +646,97 @@ class SparkAPI(DatabaseAPI):
         # specified.
         elif not self.break_lineage_method:
             self.break_lineage_method = "parquet"
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+class SQLiteAPI(DatabaseAPI):
+    sql_dialect = SqliteDialect()
+
+    @staticmethod
+    def dict_factory(cursor, row):
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
+
+    def _register_udfs(self, register_udfs: bool):
+        self.con.create_function("log2", 1, math.log2)
+        self.con.create_function("pow", 2, pow)
+        self.con.create_function("power", 2, pow)
+
+        if register_udfs:
+            try:
+                from rapidfuzz.distance.DamerauLevenshtein import distance as dam_lev
+                from rapidfuzz.distance.Jaro import distance as jaro
+                from rapidfuzz.distance.JaroWinkler import distance as jaro_winkler
+                from rapidfuzz.distance.Levenshtein import distance as levenshtein
+            except ModuleNotFoundError as e:
+                raise SplinkException(
+                    "To use fuzzy string-matching udfs in SQLite you must install "
+                    "the python package 'rapidfuzz'.  "
+                    "If you do not wish to do so, and do not need to use any "
+                    "fuzzy string-matching comparisons, you can use the linker argument "
+                    "`register_udfs=False`.\n"
+                    "See https://moj-analytical-services.github.io/splink/"
+                    "topic_guides/backends.html#sqlite for more information"
+                ) from e
+
+        def wrap_func_with_str(func):
+            def wrapped_func(str_l, str_r):
+                return func(str(str_l), str(str_r))
+
+            return wrapped_func
+
+        funcs_to_register = {
+            "levenshtein": levenshtein,
+            "damerau_levenshtein": dam_lev,
+            "jaro_winkler": jaro_winkler,
+            "jaro": jaro,
+        }
+
+        for sql_name, func in funcs_to_register.items():
+            self.con.create_function(sql_name, 2, wrap_func_with_str(func))
+
+    def __init__(
+        self, connection: Union[str, sql_con] = ":memory:", register_udfs=True
+    ):
+        if isinstance(connection, str):
+            connection = sqlite3.connect(connection)
+        self.con = connection
+        self.con.row_factory = self.dict_factory
+        self._register_udfs(register_udfs)
+
+    def _table_registration(self, input, table_name):
+        if isinstance(input, dict):
+            input = pd.DataFrame(input)
+        elif isinstance(input, list):
+            input = pd.DataFrame.from_records(input)
+
+        # Will error if an invalid data type is passed
+        input.to_sql(
+            table_name,
+            self.con,
+            index=False,
+            if_exists="replace",
+        )
+
+    def _table_to_splink_dataframe(self, templated_name, physical_name):
+        return SQLiteDataFrame(templated_name, physical_name, self)
+
+    def _table_exists_in_database(self, table_name):
+        sql = f"PRAGMA table_info('{table_name}');"
+
+        rec = self.con.execute(sql).fetchone()
+        if not rec:
+            return False
+        else:
+            return True
+
+    def _run_sql_execution(self, final_sql: str) -> sqlite3.Cursor:
+        return self.con.sql(final_sql)
