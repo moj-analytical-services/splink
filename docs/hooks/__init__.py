d80f2eb48ee3666fd59e043120c5576bd50f55f7
ADBond <48208438+ADBond@users.noreply.github.com>
2024-01-30 18:29:32 +0000

    preprocessing markdown fixes internal links

--- docs/hooks/__init__.py
+++ docs/hooks/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import logging
-import os
 import re
 from pathlib import Path
 
@@ -21,6 +19,16 @@ INCLUDE_MARKDOWN_REGEX = (
 )
 
 def include_markdown(markdown: str) -> str | None:
+    """
+    Takes markdown string content and replaces blocks such as:
+
+    {% include-markdown "./includes/some_file.md" %}
+
+    with the _contents_ of "docs/includes/some_file.md", or fail
+    with an error if the file is not located
+
+    If there is no such block, returns None.
+    """
     if not re.search(INCLUDE_MARKDOWN_REGEX, markdown):
         return
     # if we have an include tag, replace text with file contents
@@ -37,26 +45,49 @@ def include_markdown(markdown: str) -> str | None:
                 markdown = new_text
         # if we can't find include file then warn but carry on
         except FileNotFoundError as e:
-            logging.warning(f"Couldn't find specified include file: {e}")
+            raise FileNotFoundError(
+                f"Couldn't find specified include file: {e}"
+            ) from None
     return markdown
 
 
-# def re_route_links(markdown: str) -> str | None
-# nasty hack:
-# also adjust CONTRIBUTING.md links - mismatch between links as on github and
-# where it fits in folder hierarchy of docs
-with open(Path(".") / "CONTRIBUTING.md", "r") as f:
-    contributing_text = f.read()
-# in docs CONTRIBUTING.md 'thinks' it's already in docs/, so remove that level from
-# relative links
-new_text = re.sub("docs/", "", contributing_text)
-with open(Path(".") / "CONTRIBUTING.md", "w") as f:
-    f.write(new_text)
+def re_route_links(markdown: str, page_title: str) -> str | None:
+    """
+    If any links are to files 'docs/foo/bar.md' (which work directly in-repo)
+    reroute instead to 'foo/bar.md' (which work in structure of docs)
+
+    To avoid false positives this is opt-in - i.e. it only works for files
+    with titles as specified in tuple
+
+    If not one of these files, or no such links, we return None
+    """
+    # the 'proper' way to do this would be to check if the file lives outside
+    # the docs/ folder, and only adjust if so, rather relying on title
+    # (which could be changed), and must be opted-into
+    relevant_file_titles = (
+        "Contributor Guide",
+    )
+    if page_title not in relevant_file_titles:
+        return
+
+    docs_folder_regex = "docs/"
+    if not re.search(docs_folder_regex, markdown):
+        return
+    return re.sub(docs_folder_regex, "", markdown)
 
 
 def on_page_markdown_hook(
     markdown: str, page: Page, config: MkDocsConfig, files: Files
 ) -> str | None:
+    """
+    mkdocs hook to transform the raw markdown before it is sent to the renderer.
+
+    See https://www.mkdocs.org/dev-guide/plugins/#on_page_markdown for details.
+    """
     if (replaced_markdown := include_markdown(markdown)) is not None:
+        return replaced_markdown
+    # this only works if we don't have files that need links rewritten + includes
+    # this is currently the case, so no need to worry
+    if (replaced_markdown := re_route_links(markdown, page.title)) is not None:
         return replaced_markdown
     return
