1da3c0919102defa94684e3d03a9c83456d5e06b
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-01-30 15:30:31 +0000

    trigger tests

--- splink/database_api.py
+++ splink/database_api.py
@@ -915,7 +915,7 @@ class PostgresAPI(DatabaseAPI):
 
     def _create_splink_schema(self, other_schemas_to_search):
         other_schemas_to_search = ensure_is_list(other_schemas_to_search)
-        # always search _db_schema first, and public last
+        # always search _db_schema first and public last
         schemas_to_search = [self._db_schema] + other_schemas_to_search + ["public"]
         search_path = ",".join(schemas_to_search)
         sql = f"""
