b8f41f73a4b5cb3cb338ddd853c31133abca9568
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-02-06 12:21:22 +0000

    test DatediffAtThresholds

--- tests/test_datediff_level.py
+++ tests/test_datediff_level.py
@@ -1,8 +1,10 @@
 import splink.comparison_level_library as cll
+import splink.comparison_library as cl
 from splink.column_expression import ColumnExpression
 from tests.decorator import mark_with_dialects_excluding
 from tests.literal_utils import (
     ComparisonLevelTestSpec,
+    ComparisonTestSpec,
     LiteralTestValues,
     run_tests_with_args,
 )
@@ -33,4 +35,24 @@ def test_datediff_levels(test_helpers, dialect):
             ),
         ],
     )
+    run_tests_with_args(test_spec, db_api)
+
+
+@mark_with_dialects_excluding("sqlite", "postgres", "spark")
+def test_datediff_at_thresholds(test_helpers, dialect):
+    helper = test_helpers[dialect]
+    db_api = helper.extra_linker_args()["database_api"]
+
+    test_spec = ComparisonTestSpec(
+        cl.DatediffAtThresholds(
+            "dob", date_thresholds=[1], date_metrics=["day"], cast_strings_to_dates=True
+        ),
+        tests=[
+            LiteralTestValues(
+                values={"dob_l": "2000-01-01", "dob_r": "2000-01-28"},
+                expected_gamma_val=0,
+            )
+        ],
+    )
+
     run_tests_with_args(test_spec, db_api)
