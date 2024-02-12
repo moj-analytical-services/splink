f8b2ced42a25710aebe8d78af1f4513ad27716f2
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-02-12 19:22:44 +0000

    fix duckdb full example

--- tests/test_full_example_duckdb.py
+++ tests/test_full_example_duckdb.py
@@ -127,7 +127,7 @@ def test_full_example_duckdb(tmp_path):
     db_api = DuckDBAPI()
     linker_2 = Linker(df, settings=simple_settings, database_api=db_api)
 
-    Linker(df, database_api=db_api, settings=path)
+    linker_2 = Linker(df, database_api=db_api, settings=path)
 
     # Test that writing to files works as expected
     _test_write_functionality(linker_2, pd.read_csv)
