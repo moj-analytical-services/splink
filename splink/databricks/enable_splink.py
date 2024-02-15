942479c4d94d75372ab72fb08cc67741ba570b4f
Bobby W <boobay@users.noreply.github.com>
2024-02-15 13:03:30 -0600

    Adding support for DBR 13.x and 14.x+
    Adding support for DBR 13.x and 14.x

--- splink/databricks/enable_splink.py
+++ splink/databricks/enable_splink.py
@@ -14,21 +14,55 @@ def enable_splink(spark):
     """
     sc = spark.sparkContext
     _jar_path = similarity_jar_location()
-    JavaURI = sc._jvm.java.net.URI
-    JavaJarId = sc._jvm.com.databricks.libraries.JavaJarId
-    ManagedLibraryId = sc._jvm.com.databricks.libraries.ManagedLibraryId
-    ManagedLibraryVersions = sc._jvm.com.databricks.libraries.ManagedLibraryVersions
+
+    JavaURI = getattr(sc._jvm.java.net, "URI")
+    JavaJarId = getattr(sc._jvm.com.databricks.libraries, "JavaJarId")
+    ManagedLibraryId = getattr(
+        sc._jvm.com.databricks.libraries, "ManagedLibraryId"
+    )
+    ManagedLibraryVersions = getattr(
+        sc._jvm.com.databricks.libraries, "ManagedLibraryVersions"
+    )
     NoVersion = getattr(ManagedLibraryVersions, "NoVersion$")
     NoVersionModule = getattr(NoVersion, "MODULE$")
-    DatabricksILoop = sc._jvm.com.databricks.backend.daemon.driver.DatabricksILoop
+    DatabricksILoop = getattr(
+        sc._jvm.com.databricks.backend.daemon.driver, "DatabricksILoop"
+    )
     converters = sc._jvm.scala.collection.JavaConverters
 
     JarURI = JavaURI.create("file:" + _jar_path)
-    lib = JavaJarId(
-        JarURI,
-        ManagedLibraryId.defaultOrganization(),
-        NoVersionModule.simpleString(),
-    )
+    optionClass = getattr(sc._jvm.scala, "Option$")
+    optionModule = getattr(optionClass, "MODULE$")
+
+    # Note(bobby): So dirty
+    try:
+        # This will fix the exception when running on Databricks Runtime 14.x+
+        lib = JavaJarId(
+            JarURI,
+            ManagedLibraryId.defaultOrganization(),
+            NoVersionModule.simpleString(),
+            optionModule.apply(None),
+            optionModule.apply(None),
+            optionModule.apply(None)
+        )
+    except:
+        try:
+            # This will fix the exception when running on Databricks Runtime 13.x
+            lib = JavaJarId(
+                JarURI,
+                ManagedLibraryId.defaultOrganization(),
+                NoVersionModule.simpleString(),
+                optionModule.apply(None),
+                optionModule.apply(None)
+            )
+        except:
+            # This will work for < 13.x
+            lib = JavaJarId(
+                JarURI,
+                ManagedLibraryId.defaultOrganization(),
+                NoVersionModule.simpleString()
+            )
+
     libSeq = converters.asScalaBufferConverter((lib,)).asScala().toSeq()
 
     context = DatabricksILoop.getSharedDriverContextIfExists().get()
