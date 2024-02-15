0960012d5c730cb12b459e135047b0e9feb3712a
Bobby W <boobay@users.noreply.github.com>
2024-02-15 13:15:22 -0600

    Updating changes to match Splink lint standards

--- splink/databricks/enable_splink.py
+++ splink/databricks/enable_splink.py
@@ -15,19 +15,13 @@ def enable_splink(spark):
     sc = spark.sparkContext
     _jar_path = similarity_jar_location()
 
-    JavaURI = getattr(sc._jvm.java.net, "URI")
-    JavaJarId = getattr(sc._jvm.com.databricks.libraries, "JavaJarId")
-    ManagedLibraryId = getattr(
-        sc._jvm.com.databricks.libraries, "ManagedLibraryId"
-    )
-    ManagedLibraryVersions = getattr(
-        sc._jvm.com.databricks.libraries, "ManagedLibraryVersions"
-    )
+    JavaURI = sc._jvm.java.net.URI
+    JavaJarId = sc._jvm.com.databricks.libraries.JavaJarId
+    ManagedLibraryId = sc._jvm.com.databricks.libraries.ManagedLibraryId
+    ManagedLibraryVersions = sc._jvm.com.databricks.libraries.ManagedLibraryVersions
     NoVersion = getattr(ManagedLibraryVersions, "NoVersion$")
     NoVersionModule = getattr(NoVersion, "MODULE$")
-    DatabricksILoop = getattr(
-        sc._jvm.com.databricks.backend.daemon.driver, "DatabricksILoop"
-    )
+    DatabricksILoop = sc._jvm.com.databricks.backend.daemon.driver.DatabricksILoop
     converters = sc._jvm.scala.collection.JavaConverters
 
     JarURI = JavaURI.create("file:" + _jar_path)
