9168a148477a65939769a7e7aafa1a8f9a8a0caf
ADBond <48208438+ADBond@users.noreply.github.com>
2024-03-08 11:42:06 +0000

    get rid of unneeded argument

--- tests/test_profile_data.py
+++ tests/test_profile_data.py
@@ -29,7 +29,7 @@ def generate_raw_profile_dataset(table, columns_to_profile, db_api):
 
     pipeline.enqueue_sql(sql, "__splink__df_all_column_value_frequencies")
 
-    return db_api.execute_sql_pipeline(pipeline, []).as_pandas_dataframe()
+    return db_api.execute_sql_pipeline(pipeline).as_pandas_dataframe()
 
 
 @mark_with_dialects_including("duckdb")
