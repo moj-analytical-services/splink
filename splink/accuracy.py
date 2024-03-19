bbbd2f5aa751e87c2538f508692836a8fb526b22
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-03-19 14:36:18 +0000

    prediction_errors_from_labels_table

--- splink/accuracy.py
+++ splink/accuracy.py
@@ -268,13 +268,12 @@ def prediction_errors_from_labels_table(
     include_false_negatives=True,
     threshold=0.5,
 ):
-    # Read from the cache or generate
-    nodes_with_tf = linker._initialise_df_concat_with_tf()
+    pipeline = CTEPipeline(reusable=False)
+    pipeline = linker._enqueue_df_concat_with_tf(pipeline)
 
     sqls = predictions_from_sample_of_pairwise_labels_sql(linker, labels_tablename)
 
-    for sql in sqls:
-        linker._enqueue_sql(sql["sql"], sql["output_table_name"])
+    pipeline.enqueue_list_of_sqls(sqls)
 
     false_positives = f"""
     (clerical_match_score < {threshold} and
@@ -307,9 +306,9 @@ def prediction_errors_from_labels_table(
     {where_condition}
     """
 
-    linker._enqueue_sql(sql, "__splink__labels_with_fp_fn_status")
+    pipeline.enqueue_sql(sql, "__splink__labels_with_fp_fn_status")
 
-    return linker._execute_sql_pipeline([nodes_with_tf])
+    return linker.db_api.sql_pipeline_to_splink_dataframe(pipeline)
 
 
 def _predict_from_label_column_sql(linker, label_colname):
