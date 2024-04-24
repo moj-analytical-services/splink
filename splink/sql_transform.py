490ae3afba10abf76392c5b195c7b6d1efa910c2
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-04-24 15:40:50 +0100

    allow sql dialect

--- splink/sql_transform.py
+++ splink/sql_transform.py
@@ -35,8 +35,8 @@ def _remove_table_prefix(node):
     return node
 
 
-def move_l_r_table_prefix_to_column_suffix(blocking_rule) -> str:
-    expression_tree = sqlglot.parse_one(blocking_rule, read=None)
+def move_l_r_table_prefix_to_column_suffix(blocking_rule, dialect=None) -> str:
+    expression_tree = sqlglot.parse_one(blocking_rule, read=dialect)
     transformed_tree = expression_tree.transform(_add_l_or_r_to_identifier)
     transformed_tree = transformed_tree.transform(_remove_table_prefix)
     return transformed_tree.sql()
