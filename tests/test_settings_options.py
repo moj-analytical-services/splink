3b0056fba9a175d2c7a726863bb8af848852e000
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-05-15 06:20:53 +0100

    duckdb tests pass

--- tests/test_settings_options.py
+++ tests/test_settings_options.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 
 import splink.comparison_library as cl
+from splink import block_on
 
 from .decorator import mark_with_dialects_excluding
 
@@ -32,8 +33,11 @@ def test_model_heavily_customised_settings(test_helpers, dialect, tmp_path):
     settings = {
         "link_type": "link_and_dedupe",
         "blocking_rules_to_generate_predictions": [
-            "l.city = r.city AND l.dob = r.dob",
-            "l.first_name = r.first_name and l.surname = r.surname",
+            block_on("city", "dob"),
+            """l.first_name = r.first_name
+            and l.surname = r.surname
+            and substr(l.first_name, 1, 1) = 'j'
+            """,
         ],
         "comparisons": [
             cl.ExactMatch("first_name"),
@@ -54,7 +58,7 @@ def test_model_heavily_customised_settings(test_helpers, dialect, tmp_path):
     }
     linker = helper.Linker([df_l, df_r], settings, **helper.extra_linker_args())
     # run through a few common operations to check functioning
-    linker.estimate_probability_two_random_records_match("l.dob = r.dob", 0.5)
+    linker.estimate_probability_two_random_records_match(["l.dob = r.dob"], 0.5)
     linker.estimate_u_using_random_sampling(2e4)
     linker.estimate_parameters_using_expectation_maximisation("l.dob = r.dob")
     df_predict = linker.predict(0.1)
