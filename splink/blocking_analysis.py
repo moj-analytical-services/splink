afe1d9c58c906717f220bc398ab51bfad320b03f
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-05-15 16:23:45 +0100

    aliases for public api

---
+++ splink/blocking_analysis.py
@@ -0,0 +1,11 @@
+from .internals.blocking_analysis import (
+    count_comparisons_from_blocking_rule,
+    cumulative_comparisons_to_be_scored_from_blocking_rules_chart,
+    cumulative_comparisons_to_be_scored_from_blocking_rules_data,
+)
+
+__all__ = [
+    "count_comparisons_from_blocking_rule",
+    "cumulative_comparisons_to_be_scored_from_blocking_rules_chart",
+    "cumulative_comparisons_to_be_scored_from_blocking_rules_data",
+]
