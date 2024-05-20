2927ea6bc4a06a0463801311b06ce9654ce0c25b
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-05-20 21:13:33 +0100

    start fixing notebooks

---
+++ splink/comparison_library.py
@@ -0,0 +1,29 @@
+from splink.internals.comparison_library import (
+    AbsoluteDateDifferenceAtThresholds,
+    AbsoluteTimeDifferenceAtThresholds,
+    ArrayIntersectAtSizes,
+    CustomComparison,
+    DamerauLevenshteinAtThresholds,
+    DistanceFunctionAtThresholds,
+    DistanceInKMAtThresholds,
+    ExactMatch,
+    JaccardAtThresholds,
+    JaroAtThresholds,
+    JaroWinklerAtThresholds,
+    LevenshteinAtThresholds,
+)
+
+__all__ = [
+    "CustomComparison",
+    "ExactMatch",
+    "LevenshteinAtThresholds",
+    "DamerauLevenshteinAtThresholds",
+    "JaccardAtThresholds",
+    "JaroAtThresholds",
+    "JaroWinklerAtThresholds",
+    "DistanceFunctionAtThresholds",
+    "AbsoluteTimeDifferenceAtThresholds",
+    "AbsoluteDateDifferenceAtThresholds",
+    "ArrayIntersectAtSizes",
+    "DistanceInKMAtThresholds",
+]
---
+++ splink/comparison_template_library.py
@@ -0,0 +1,15 @@
+from splink.internals.comparison_template_library import (
+    DateComparison,
+    EmailComparison,
+    ForenameSurnameComparison,
+    NameComparison,
+    PostcodeComparison,
+)
+
+__all__ = [
+    "DateComparison",
+    "EmailComparison",
+    "ForenameSurnameComparison",
+    "NameComparison",
+    "PostcodeComparison",
+]
