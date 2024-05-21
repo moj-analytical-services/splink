5f59ebc6f567ed24957cc983956d69887e5b5470
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-05-21 09:27:22 +0100

    bump version

--- splink/__init__.py
+++ splink/__init__.py
@@ -44,7 +44,7 @@ def __getattr__(name):
     raise AttributeError(f"module 'splink' has no attribute '{name}'") from None
 
 
-__version__ = "4.0.0.dev4"
+__version__ = "4.0.0.dev5"
 
 
 __all__ = [
