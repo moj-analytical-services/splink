2c95968f8bb6ab7c9e9ae2a0af711fd62bb7b801
ADBond <48208438+ADBond@users.noreply.github.com>
2024-10-22 12:31:40 +0100

    don't use parametrize - wrap tests together

--- tests/test_debug_mode.py
+++ tests/test_debug_mode.py
@@ -1,10 +1,13 @@
 import pandas as pd
-from pytest import mark, param
 
 import splink.comparison_library as cl
 from splink import Linker, SettingsCreator, block_on
 
 from .decorator import mark_with_dialects_excluding
+
+
+class DebugModeError(Exception): ...
+
 
 df = pd.read_csv("./tests/datasets/fake_1000_from_splink_demos.csv")
 settings = SettingsCreator(
@@ -22,10 +25,29 @@ settings = SettingsCreator(
 )
 
 
-@mark.parametrize(
-    "debug_mode", [param(False, id="debug_mode off"), param(True, id="debug_mode on")]
-)
+def debug_mode_test_wrapper(test_function):
+    def wrapper(test_helpers, dialect):
+        errors = {}
+        for debug_mode in (False, True):
+            try:
+                test_function(test_helpers, dialect, debug_mode=debug_mode)
+            except Exception as e:
+                errors[debug_mode] = e
+        if errors:
+            # in this case we get an error in normal test execution
+            # raise, but this is not a debug_mode issue
+            if errors.get(False, False):
+                raise errors[False]
+            # this is a debug mode error only!
+            raise DebugModeError(
+                f"Error occurs in debug mode only: {errors[True]}"
+            ) from errors[True]
+
+    return wrapper
+
+
 @mark_with_dialects_excluding()
+@debug_mode_test_wrapper
 def test_debug_mode_u_training(test_helpers, dialect, debug_mode):
     helper = test_helpers[dialect]
 
@@ -36,10 +58,9 @@ def test_debug_mode_u_training(test_helpers, dialect, debug_mode):
 
     linker.training.estimate_u_using_random_sampling(max_pairs=6e5)
 
-@mark.parametrize(
-    "debug_mode", [param(False, id="debug_mode off"), param(True, id="debug_mode on")]
-)
+
 @mark_with_dialects_excluding()
+@debug_mode_test_wrapper
 def test_debug_mode_ptrrm_train(test_helpers, dialect, debug_mode):
     helper = test_helpers[dialect]
 
@@ -54,10 +75,9 @@ def test_debug_mode_ptrrm_train(test_helpers, dialect, debug_mode):
         recall=0.7,
     )
 
-@mark.parametrize(
-    "debug_mode", [param(False, id="debug_mode off"), param(True, id="debug_mode on")]
-)
+
 @mark_with_dialects_excluding()
+@debug_mode_test_wrapper
 def test_debug_mode_combined_training(test_helpers, dialect, debug_mode):
     helper = test_helpers[dialect]
 
