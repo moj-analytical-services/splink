c43a0e7014e60cb57a1b03ae0c322b7bd6c97920
Ross Kennedy <ross.kennedy@digital.justice.gov.uk>
2025-02-12 15:54:12 +0000

    switch with statement to sql pipeline

--- splink/internals/graph_metrics.py
+++ splink/internals/graph_metrics.py
@@ -78,7 +78,6 @@ def _node_degree_centralisation_sql(
     # join clusters table to capture edge-less nodes
     # want all clusters included so left join
     sql = f"""
-        WITH all_nodes AS (
         SELECT
             c.{composite_uid_clusters} AS composite_unique_id,
             c.cluster_id AS cluster_id,
@@ -90,17 +89,24 @@ def _node_degree_centralisation_sql(
             {all_nodes_table_name} n
         ON
             c.{composite_uid_clusters} = n.node
-        GROUP BY composite_unique_id, cluster_id)
+        GROUP BY composite_unique_id, cluster_id
+    """
+    node_degree_table_name = "__splink__graph_metrics_node_degree"
+    sql_info = {"sql": sql, "output_table_name": node_degree_table_name}
+    sqls.append(sql_info)
 
+    # calculate node centrality
+    sql = f"""
         SELECT
             composite_unique_id,
             cluster_id,
             node_degree,
-            node_degree / (cluster_size - 1) AS node_centralisation
-        FROM all_nodes
+            node_degree / (cluster_size - 1) AS node_centrality
+        FROM {node_degree_table_name}
     """
     sql_info = {"sql": sql, "output_table_name": "__splink__graph_metrics_nodes"}
     sqls.append(sql_info)
+
     return sqls
 
 
