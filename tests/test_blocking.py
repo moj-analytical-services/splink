ffc616ea43d7e4883008258bddc2f05a29e51888
aymonwuolanne <a.wuolanne@gmail.com>
2025-08-13 12:00:42 +1000

    updating tests and removing preceding rules test

--- tests/test_blocking.py
+++ tests/test_blocking.py
@@ -1,6 +1,5 @@
 from splink.internals.blocking import BlockingRule, blocking_rule_to_obj
 from splink.internals.blocking_rule_library import block_on
-from splink.internals.input_column import _get_dialect_quotes
 from splink.internals.linker import Linker
 
 from .basic_settings import get_settings_dict
@@ -8,23 +7,7 @@ from .decorator import mark_with_dialects_excluding
 
 
 @mark_with_dialects_excluding()
-def test_preceding_blocking_rules(dialect):
-    br_surname = block_on("surname", salting_partitions=4).get_blocking_rule(dialect)
-
-    q, _ = _get_dialect_quotes(dialect)
-    em_rule = f"l.{q}surname{q} = r.{q}surname{q}"
-
-    assert br_surname.blocking_rule_sql == em_rule
-    assert br_surname.salting_partitions == 4
-    assert br_surname.preceding_rules == []
-
-    preceding_rules = [
-        block_on("first_name").get_blocking_rule(dialect),
-        block_on("dob").get_blocking_rule(dialect),
-    ]
-    br_surname.add_preceding_rules(preceding_rules)
-    assert br_surname.preceding_rules == preceding_rules
-
+def test_match_keys(dialect):
     blocking_rules = [
         BlockingRule("l.help = r.help", dialect),
         blocking_rule_to_obj(
@@ -39,23 +22,11 @@ def test_preceding_blocking_rules(dialect):
         ),
         block_on("help4").get_blocking_rule(dialect),
     ]
-    blocking_rules = BlockingRule._add_preceding_rules_to_each_blocking_rule(
-        blocking_rules
-    )
-    blocking_rule_sqls = [
-        blocking_rule_to_obj(br).blocking_rule_sql for br in blocking_rules
-    ]
 
-    assert blocking_rules[0].preceding_rules == []
+    BlockingRule._set_match_keys_for_each_blocking_rule(blocking_rules)
 
-    def assess_preceding_rules(settings_brs_index):
-        br_prec = blocking_rules[settings_brs_index].preceding_rules
-        br_prec_txt = [br.blocking_rule_sql for br in br_prec]
-        assert br_prec_txt == blocking_rule_sqls[:settings_brs_index]
-
-    assess_preceding_rules(1)
-    assess_preceding_rules(2)
-    assess_preceding_rules(3)
+    for n, br in enumerate(blocking_rules):
+        assert br.match_key == n
 
 
 @mark_with_dialects_excluding()
