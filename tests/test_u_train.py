3effa79e9c9afebd7e786bcce7e5d575e37024be
Robin Linacre <robinlinacre@hotmail.com>
2025-10-01 20:18:52 +0100

    no longer need drop code

--- tests/test_u_train.py
+++ tests/test_u_train.py
@@ -101,8 +101,6 @@ def test_u_train_link_only(test_helpers, dialect):
     self_table_count = linker._db_api.sql_pipeline_to_splink_dataframe(pipeline)
 
     result = self_table_count.as_record_dict()
-    db_api = helper.DatabaseAPI(**helper.db_api_args())
-    db_api._drop_temp_view_if_exists("__splink__df_blocked_same_table_count")
 
     self_table_count.drop_table_from_database_and_remove_from_cache()
     assert result[0]["count"] == 0
@@ -298,8 +296,7 @@ def test_u_train_multilink(test_helpers, dialect):
     self_table_count = linker._db_api.sql_pipeline_to_splink_dataframe(pipeline)
 
     result = self_table_count.as_record_dict()
-    db_api = helper.DatabaseAPI(**helper.db_api_args())
-    db_api._drop_temp_view_if_exists("__splink__df_blocked_same_table_count")
+
     self_table_count.drop_table_from_database_and_remove_from_cache()
     assert result[0]["count"] == 0
 
@@ -339,8 +336,6 @@ def test_u_train_multilink(test_helpers, dialect):
 
     result = self_table_count.as_record_dict()
 
-    db_api = helper.DatabaseAPI(**helper.db_api_args())
-    db_api._drop_temp_view_if_exists("__splink__df_blocked_same_table_count")
     self_table_count.drop_table_from_database_and_remove_from_cache()
     assert result[0]["count"] == (2 * 1 / 2 + 3 * 2 / 2 + 4 * 3 / 2 + 7 * 6 / 2)
 
