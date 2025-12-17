c1ace529a5cef19054699369bb20fe08c6903b05
Robin Linacre <robinlinacre@hotmail.com>
2025-12-17 14:26:09 +0000

    harden

--- splink/internals/splinkdataframe_utils.py
+++ splink/internals/splinkdataframe_utils.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 
-from splink.internals.misc import ensure_is_iterable
+from splink.internals.splink_dataframe import SplinkDataFrame
 
 if TYPE_CHECKING:
     from splink.internals.database_api import DatabaseAPI
@@ -10,14 +10,22 @@ if TYPE_CHECKING:
 
 
 def get_db_api_from_inputs(
-    table_or_tables: SplinkDataFrame | Any,
+    table_or_tables: SplinkDataFrame | list[SplinkDataFrame],
 ) -> DatabaseAPI[Any]:
-    first = next(iter(ensure_is_iterable(table_or_tables)))
+    if isinstance(table_or_tables, SplinkDataFrame):
+        tables = [table_or_tables]
+    else:
+        tables = table_or_tables
+    first = next(iter(tables))
     return first.db_api
 
 
 def splink_dataframes_to_dict(
-    table_or_tables: SplinkDataFrame | Any,
+    table_or_tables: SplinkDataFrame | list[SplinkDataFrame],
 ) -> dict[str, SplinkDataFrame]:
-    tables = ensure_is_iterable(table_or_tables)
+    if isinstance(table_or_tables, SplinkDataFrame):
+        tables = [table_or_tables]
+    else:
+        tables = table_or_tables
+
     return {sdf.templated_name: sdf for sdf in tables}
