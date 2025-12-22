4ff4d6334f8d4dceb8650547bf8f1a1d525a601e
ADBond <48208438+ADBond@users.noreply.github.com>
2025-12-22 10:08:47 +0000

    remove unneeded pandas fallback

--- splink/internals/expectation_maximisation.py
+++ splink/internals/expectation_maximisation.py
@@ -4,6 +4,7 @@ import logging
 import time
 from typing import Any, List, cast
 
+import duckdb
 import pandas as pd
 
 from splink.internals.comparison import Comparison
@@ -117,48 +118,11 @@ def compute_proportions_for_new_parameters_sql(table_name):
     return sql
 
 
-def compute_proportions_for_new_parameters_pandas(
-    m_u_df: pd.DataFrame,
-) -> List[dict[str, Any]]:
-    data = m_u_df.copy()
-    m_prob = "m_probability"
-    u_prob = "u_probability"
-    data.rename(columns={"m_count": m_prob, "u_count": u_prob}, inplace=True)
-
-    random_records = data[
-        data.output_column_name == "_probability_two_random_records_match"
-    ]
-    data = data[data.output_column_name != "_probability_two_random_records_match"]
-
-    data = data[data.comparison_vector_value != -1]
-    index = data.index.tolist()
-
-    m_probs = data.loc[index, m_prob] / data.groupby("output_column_name")[
-        m_prob
-    ].transform("sum")
-    u_probs = data.loc[index, u_prob] / data.groupby("output_column_name")[
-        u_prob
-    ].transform("sum")
-
-    data.loc[index, m_prob] = m_probs
-    data.loc[index, u_prob] = u_probs
-
-    data = pd.concat([random_records, data])
-
-    return data.to_dict("records")
-
-
 def compute_proportions_for_new_parameters(
     m_u_df: pd.DataFrame,
 ) -> List[dict[str, Any]]:
-    # Execute with duckdb if installed, otherwise default to pandas
-    try:
-        import duckdb
-
-        sql = compute_proportions_for_new_parameters_sql("m_u_df")
-        return duckdb.query(sql).to_df().to_dict("records")
-    except (ImportError, ModuleNotFoundError):
-        return compute_proportions_for_new_parameters_pandas(m_u_df)
+    sql = compute_proportions_for_new_parameters_sql("m_u_df")
+    return duckdb.query(sql).to_df().to_dict("records")
 
 
 def populate_m_u_from_lookup(
