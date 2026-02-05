66eccdaca4d8ab0e75009f9f0daf03e5dbfe96c1
ADBond <48208438+ADBond@users.noreply.github.com>
2026-02-05 08:48:42 +0000

    test ingest from arrow + pandas

--- tests/test_dataframe_in_out_formats.py
+++ tests/test_dataframe_in_out_formats.py
@@ -1,3 +1,5 @@
+from pytest import mark
+
 from tests.decorator import mark_with_dialects_excluding
 
 input_data_list = [
@@ -25,6 +27,7 @@ def test_splink_dataframe_from_list(dialect, test_helpers, unique_per_test_table
 
     db_api = helper.db_api()
     db_api.register(input_data_list, unique_per_test_table_name)
+    db_api.delete_table_from_database(unique_per_test_table_name)
 
 
 @mark_with_dialects_excluding()
@@ -33,6 +36,7 @@ def test_splink_dataframe_from_tuple(dialect, test_helpers, unique_per_test_tabl
 
     db_api = helper.db_api()
     db_api.register(tuple(input_data_list), unique_per_test_table_name)
+    db_api.delete_table_from_database(unique_per_test_table_name)
 
 
 @mark_with_dialects_excluding()
@@ -41,9 +45,38 @@ def test_splink_dataframe_from_dict(dialect, test_helpers, unique_per_test_table
 
     db_api = helper.db_api()
     db_api.register(input_data_dict, unique_per_test_table_name)
+    db_api.delete_table_from_database(unique_per_test_table_name)
 
 
 @mark_with_dialects_excluding()
+@mark.needs_pandas
+def test_splink_dataframe_from_pandas(
+    dialect, test_helpers, unique_per_test_table_name
+):
+    import pandas as pd
+
+    helper = test_helpers[dialect]
+
+    db_api = helper.db_api()
+    db_api.register(pd.DataFrame(input_data_dict), unique_per_test_table_name)
+    db_api.delete_table_from_database(unique_per_test_table_name)
+
+
+@mark_with_dialects_excluding()
+def test_splink_dataframe_from_pyarrow(
+    dialect, test_helpers, unique_per_test_table_name
+):
+    import pyarrow as pa
+
+    helper = test_helpers[dialect]
+
+    db_api = helper.db_api()
+    db_api.register(pa.Table.from_pydict(input_data_dict), unique_per_test_table_name)
+    db_api.delete_table_from_database(unique_per_test_table_name)
+
+
+@mark_with_dialects_excluding()
+@mark.needs_pandas
 def test_splink_dataframe_to_pandas(dialect, test_helpers, unique_per_test_table_name):
     helper = test_helpers[dialect]
 
@@ -53,6 +86,7 @@ def test_splink_dataframe_to_pandas(dialect, test_helpers, unique_per_test_table
         table_sql, "test_table", unique_per_test_table_name
     )
     sdf.as_pandas_dataframe()
+    db_api.delete_table_from_database(unique_per_test_table_name)
 
 
 @mark_with_dialects_excluding()
@@ -65,6 +99,7 @@ def test_splink_dataframe_to_duckdb(dialect, test_helpers, unique_per_test_table
         table_sql, "test_table", unique_per_test_table_name
     )
     sdf.as_duckdbpyrelation()
+    db_api.delete_table_from_database(unique_per_test_table_name)
 
 
 @mark_with_dialects_excluding()
@@ -77,3 +112,4 @@ def test_splink_dataframe_to_pyarrow(dialect, test_helpers, unique_per_test_tabl
         table_sql, "test_table", unique_per_test_table_name
     )
     sdf.as_pyarrow_table()
+    db_api.delete_table_from_database(unique_per_test_table_name)
