f1d53c6f9d25f6c8e0314b99f1e6e31e7929595f
ADBond <48208438+ADBond@users.noreply.github.com>
2026-03-05 17:30:24 +0000

    move tf chart data logic from pandas to duckdb

--- splink/internals/term_frequencies.py
+++ splink/internals/term_frequencies.py
@@ -4,12 +4,17 @@ from __future__ import annotations
 # https://github.com/moj-analytical-services/splink/pull/107
 import logging
 import warnings
+from functools import reduce
+from math import ceil, floor
 from typing import TYPE_CHECKING, Any, Optional
 
 from splink.internals.charts import (
     ChartReturnType,
     altair_or_json,
     load_chart_definition,
+)
+from splink.internals.duckdb.duckdb_helpers.duckdb_helpers import (
+    record_dicts_from_relation,
 )
 from splink.internals.input_column import InputColumn
 from splink.internals.pipeline import CTEPipeline
@@ -186,44 +191,48 @@ def compute_all_term_frequencies_sqls(
     return sqls
 
 
-def comparison_level_to_tf_chart_data(cl: dict[str, Any]) -> dict[str, Any]:
-    from numpy import log2
-
-    df = cl["df_tf"]
-    df.columns = ["value", "tf"]
-    df = df[df.value.notnull()]
-
-    del cl["df_tf"]
-    df = df.assign(**cl)
-
-    # TF match weight scaled by tf_adjustment_weight
-    df.loc[:, "log2_bf_tf"] = (
-        log2(df.loc[:, "u_probability"] / df.loc[:, "tf"])
-        * df.loc[:, "tf_adjustment_weight"]
+def comparison_level_to_tf_chart_data(
+    cl: dict[str, Any], con, column_info_settings, sqlglot_dialect_str
+) -> dict[str, Any]:
+    # TODO: don't want to have to pass this around everywhere
+    input_col = InputColumn(
+        cl["tf_adjustment_column"],
+        column_info_settings=column_info_settings,
+        sqlglot_dialect_str=sqlglot_dialect_str,
     )
 
-    # Tidy up columns
-    # df = df.drop(columns=["tf", "u_probability", "tf_adjustment_weight"])
-    df.rename(
-        columns={
-            "comparison_vector_value": "gamma",
-            "tf_adjustment_column": "tf_col",
-            "log2_bayes_factor": "log2_bf",
-        },
-        inplace=True,
-    )
-    df["log2_bf_final"] = df["log2_bf_tf"] + df["log2_bf"]
+    sdf = cl["df_tf"]
+    # ensure our duckdb connexion has access to this table
+    sdf.as_duckdbpyrelation()
 
-    # Add ranks for sorting/selecting
-    df = df.sort_values("log2_bf_tf")
-    df["most_freq_rank"] = df.groupby("gamma", observed=False)["log2_bf_tf"].cumcount()
-    df["least_freq_rank"] = df.groupby("gamma", observed=False)["log2_bf_tf"].cumcount(
-        ascending=False
-    )
+    sql = f"""
+    SELECT
+        {input_col.name} AS value,
+        {input_col.tf_name} AS tf,
+        {cl["u_probability"]}::DOUBLE AS u_probability,
+        {cl["tf_adjustment_weight"]}::DOUBLE AS tf_adjustment_weight,
+        -- TF match weight scaled by tf_adjustment_weight
+        log2(u_probability/tf) * tf_adjustment_weight AS log2_bf_tf,
+        -- Tidy up columns
+        {cl["comparison_vector_value"]} AS gamma,
+        '{input_col.unquote().name}' AS tf_col,
+        {cl["log2_bayes_factor"]}::DOUBLE AS log2_bf,
+        log2_bf_tf + log2_bf AS log2_bf_final,
+        row_number() OVER (
+                PARTITION BY gamma
+                ORDER BY log2_bf_tf
+            ) AS most_freq_rank,
+        row_number() OVER (
+                PARTITION BY gamma
+                ORDER BY log2_bf_tf DESC
+            ) AS least_freq_rank,
+    FROM
+        {sdf.physical_name}
+    WHERE
+        value IS NOT NULL
+    """
 
-    cl["df_out"] = df
-
-    return cl
+    return con.sql(sql)
 
 
 def tf_adjustment_chart(
@@ -234,30 +243,26 @@ def tf_adjustment_chart(
     vals_to_include: list[str],
     as_dict: bool,
 ) -> ChartReturnType:
-    from numpy import arange, ceil, floor
-    from pandas import concat, cut
-
     # Data for chart
     comparison = linker._settings_obj._get_comparison_by_output_column_name(col)
-    comparison_records = list(
-        map(lambda rec: rec.as_dict(), comparison._as_detailed_records)
-    )
+    tf_comparison_records = [
+        detailed_rec
+        for detailed_rec in comparison._as_detailed_records
+        if detailed_rec.has_tf_adjustments
+    ]
 
     keys_to_retain = [
         "comparison_vector_value",
         "label_for_charts",
         "tf_adjustment_column",
         "tf_adjustment_weight",
-        "tf_minimum_u_value",
         "u_probability",
         "log2_bayes_factor",
     ]
 
     # Select levels with TF adjustments
     comparison_records = [
-        {k: cl[k] for k in cl.keys() if k in keys_to_retain}
-        for cl in comparison_records
-        if cl["has_tf_adjustments"]
+        {k: getattr(cl, k) for k in keys_to_retain} for cl in tf_comparison_records
     ]
 
     # Add data ("df_tf") to each level
@@ -267,23 +272,32 @@ def tf_adjustment_chart(
             **{
                 "df_tf": linker.table_management.compute_tf_table(
                     cl["tf_adjustment_column"]
-                ).as_pandas_dataframe()
+                )
             },
         )
         for cl in comparison_records
     ]
+    con = linker._db_api.duckdb_con
 
-    c = [comparison_level_to_tf_chart_data(cl) for cl in comparison_records]
-    df = concat([cl["df_out"] for cl in c])
-    # Filter values
-    selected = False if not vals_to_include else df["value"].isin(vals_to_include)
-    least_freq = True if not n_least_freq else df["least_freq_rank"] < n_least_freq
-    most_freq = True if not n_most_freq else df["most_freq_rank"] < n_most_freq
-    mask = selected | least_freq | most_freq
+    column_info_settings = linker._settings_obj.column_info_settings
+    sqlglot_dialect_str = linker._settings_obj._sql_dialect_str
 
-    vals_not_included = [
-        val for val in vals_to_include if val not in df["value"].values
+    levels_chart_data = [
+        comparison_level_to_tf_chart_data(
+            cl, con, column_info_settings, sqlglot_dialect_str
+        )
+        for cl in comparison_records
     ]
+    df = reduce(
+        lambda relation_left, relation_right: relation_left.union(relation_right),
+        levels_chart_data,
+    )
+
+    vals_not_included = (
+        set(vals_to_include) - set(x[0] for x in df.select("value").fetchall())
+        if vals_to_include
+        else {}
+    )
     if vals_not_included:
         warnings.warn(
             f"Values {vals_not_included} from `vals_to_include` were not found in "
@@ -294,42 +308,78 @@ def tf_adjustment_chart(
     # Histogram data
     bin_width = 0.5  # Specify the desired bin width
 
-    min_value = floor(df["log2_bf_final"].min())
-    max_value = ceil(df["log2_bf_final"].max())
-    bin_edges = arange(min_value, max_value + bin_width, bin_width)
-
-    df["bin"] = cut(df["log2_bf_final"], bins=bin_edges)
-    binned_df = (
-        df.groupby(["gamma", "bin", "log2_bf"], observed=False)
-        .agg({"log2_bf_final": "count", "log2_bf_tf": "mean"})
-        .reset_index()
-        .rename(columns={"log2_bf_final": "count"})
+    min_value = floor(min(x[0] for x in df["log2_bf_final"].fetchall()))
+    max_value = ceil(max(x[0] for x in df["log2_bf_final"].fetchall()))
+    bin_edges = list(
+        map(
+            lambda x: x / 2, range(min_value * 2, max_value * 2 + 1, int(2 * bin_width))
+        )
     )
-    binned_df["bin_start"] = binned_df["bin"].apply(lambda x: x.left).astype("float")
-    binned_df["bin_end"] = binned_df["bin"].apply(lambda x: x.right).astype("float")
-    binned_df["log2_bf_final"] = (binned_df["bin_start"] + binned_df["bin_end"]) / 2
-    binned_df["log2_bf_desc"] = (
-        binned_df["bin_start"].astype("str") + "-" + binned_df["bin_end"].astype("str")
-    )
-    binned_df = binned_df.drop(columns="bin")
-    binned_df = binned_df[binned_df["count"] > 0]
 
-    df = df.drop(columns="bin")
-    df = df[mask]
+    # TODO: this is circumventing the 'standard' approach when we use duckdb.
+    # Maybe another way?
+    df_table_name = "__splink__df_tmp_tf_table_data"
+    con.register(df_table_name, df)
+
+    reln = con.sql(
+        f"""
+        WITH binned AS (
+            SELECT
+                gamma,
+                log2_bf,
+                unnest(map_entries(histogram(log2_bf_final, {bin_edges}))) AS hist_data,
+                hist_data['key'] AS bin_upper,
+                hist_data['value'] AS count,
+                bin_upper - {bin_width/2} AS log2_bf_final,
+                (bin_upper - {bin_width})::VARCHAR || '-' || (bin_upper)::VARCHAR
+                    AS log2_bf_desc,
+                -- have to repeat ourselves as duckdb can't disambiguate log2_bf_final
+                -- in this relation as opposed to the base table
+                bin_upper - {bin_width/2} - log2_bf AS log2_bf_tf
+            FROM
+                {df_table_name}
+            GROUP BY
+                gamma, log2_bf
+        )
+        -- only fields we need for the chart
+        SELECT
+            gamma,
+            count,
+            log2_bf_desc,
+            log2_bf_tf,
+            log2_bf_final,
+        FROM
+            binned
+        WHERE
+            count > 0
+        """
+    )
+
+    # Filter values
+    df = df.filter(
+        f"least_freq_rank < {n_least_freq} OR most_freq_rank < {n_most_freq}"
+    )
+    if vals_to_include:
+        df = df.filter(f"value IN ('{"', '".join(vals_to_include)}')")
 
     chart_path = "tf_adjustment_chart.json"
     chart = load_chart_definition(chart_path)
 
     # Complete chart schema
-    tf_levels = [cl["comparison_vector_value"] for cl in c]
+    tf_levels = [cl.comparison_vector_value for cl in tf_comparison_records]
     labels = [
-        f'{cl["label_for_charts"]} (TF col: {cl["tf_adjustment_column"]})' for cl in c
+        f"{cl.label_for_charts} (TF col: {cl.tf_adjustment_column})"
+        for cl in tf_comparison_records
     ]
 
-    df = df[df["gamma"].isin(tf_levels)].sort_values("least_freq_rank")
+    # trim down to only the data we need for the chart
+    main_chart_data = record_dicts_from_relation(
+        df.select("gamma", "value", "log2_bf_final", "log2_bf_tf", "log2_bf")
+    )
+    hist_data = record_dicts_from_relation(reln.filter(f"gamma IN {tf_levels}"))
 
-    chart["datasets"]["data"] = df.to_dict("records")
-    chart["datasets"]["hist"] = binned_df.to_dict("records")
+    chart["datasets"]["data"] = main_chart_data
+    chart["datasets"]["hist"] = hist_data
     chart["config"]["params"][0]["value"] = max(tf_levels)
     chart["config"]["params"][0]["bind"]["options"] = tf_levels
     chart["config"]["params"][0]["bind"]["labels"] = labels
