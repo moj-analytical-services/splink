58693ed38db5c2996b41644a1dd0d07910e3fd27
Lawrence Adams <lawrencetadams@googlemail.com>
2026-03-13 19:40:27 +0000

    chore: add more impl
    reading around implementation in other classes

--- splink/internals/snowflake/database_api.py
+++ splink/internals/snowflake/database_api.py
@@ -1,6 +1,7 @@
 import logging
 from typing import Any
 
+from snowflake.connector.connection import SnowflakeConnection
 from snowflake.connector.cursor import SnowflakeCursor
 
 from splink.internals.database_api import DatabaseAPI
@@ -13,3 +14,12 @@ logger = logging.getLogger(__name__)
 
 class SnowflakeAPI(DatabaseAPI[SnowflakeCursor[Any]]):
     sql_dialect = SnowflakeDialect()
+    _con: SnowflakeConnection
+
+    def __init__(self, connection: SnowflakeConnection, register_udfs: bool = True):
+        super().__init__()
+
+        self._con = connection
+
+    def _execute_sql_against_backend(self, final_sql: str) -> SnowflakeCursor | None:
+        return self._con.cursor().execute(final_sql)
--- splink/internals/snowflake/dataframe.py
+++ splink/internals/snowflake/dataframe.py
@@ -1,2 +1,10 @@
-class SnowflakeDataframe:
-    pass
+from typing import TYPE_CHECKING
+
+from splink.internals.splink_dataframe import SplinkDataFrame
+
+if TYPE_CHECKING:
+    from .database_api import SnowflakeAPI
+
+
+class SnowflakeDataframe(SplinkDataFrame):
+    db_api: SnowflakeAPI
