f3b831ea88ab57bc5d7ca5344b81aaee16505186
Lawrence Adams <lawrencetadams@googlemail.com>
2026-03-14 12:52:18 +0000

    chore: implement core connectivity and dataframe registration tests

--- tests/test_snowflake_io.py
+++ tests/test_snowflake_io.py
@@ -15,7 +15,7 @@ def test_snowflake_create_dp_api(snowflake_connection: SnowflakeConnection):
     _ = SnowflakeAPI(connection=snowflake_connection, register_udfs=False)
 
 
-def test_snowflake__table_registration_pd_dataframe(
+def test_snowflake_private_table_registration_pd_dataframe(
     snowflake_connection: SnowflakeConnection,
 ):
     conn = snowflake_connection
@@ -40,3 +40,38 @@ def test_snowflake__table_registration_pd_dataframe(
 
     # Clean up
     _ = conn.cursor().execute("DROP TABLE FAKE_1000;")
+
+
+def test_snowflake_public_table_register_multiple_tables_pd_dataframe(
+    snowflake_connection: SnowflakeConnection,
+):
+    conn = snowflake_connection
+    cli = SnowflakeAPI(snowflake_connection, False)
+
+    # Typing incomplete - ignore
+    ds: pd.DataFrame = splink_datasets.fake_1000  # pyright: ignore[reportAssignmentType]
+
+    cli.register_table(ds, "FAKE_1000", overwrite=False)
+
+    res = (
+        conn.cursor()
+        .execute(
+            "SELECT ROW_COUNT "
+            "FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'FAKE_1000'"
+        )
+        .fetchall()  # type: ignore -- May be None, we don't care
+    )
+
+    assert len(res) == 1, "should only have one row"
+    assert res[0][0] == 1000, "should have 1000 rows"
+
+    # Do not clean up - let the next test do that
+
+
+def test_snowflake_public_delete_table_from_database(
+    snowflake_connection: SnowflakeConnection,
+):
+    conn = snowflake_connection
+    cli = SnowflakeAPI(snowflake_connection, False)
+
+    cli.delete_table_from_database("FAKE_1000")
