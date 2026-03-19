a1716751cef10cb315572e1fdf3ad5540cc6c5a0
ADBond <48208438+ADBond@users.noreply.github.com>
2026-03-19 10:49:54 +0000

    restore functional column

--- tests/test_settings_options.py
+++ tests/test_settings_options.py
@@ -49,8 +49,7 @@ def test_model_heavily_customised_settings(test_helpers, dialect, tmp_path):
         "retain_intermediate_calculation_columns": True,
         "retain_matching_columns": True,
         "unique_id_column_name": uid,
-        # not currently fully functional:
-        # "source_dataset_column_name": source_ds_col,
+        "source_dataset_column_name": source_ds_col,
         "additional_columns_to_retain": ["cluster"],
         "match_weight_column_prefix": "match_w__",
         "term_frequency_adjustment_column_prefix": "term_freq__",
