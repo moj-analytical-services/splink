3d030b69617d915700611d3d1e3308e5375be67e
ADBond <48208438+ADBond@users.noreply.github.com>
2026-03-23 11:42:38 +0000

    test helper: convert_frame to noöp

--- tests/helpers.py
+++ tests/helpers.py
@@ -22,10 +22,6 @@ class TestHelper(ABC):
     def db_api_args(self):
         return {}
 
-    # def extra_linker_args(self):
-    #     # create fresh api each time
-    #     return {"db_api": self.DatabaseAPI(**self.db_api_args())}
-
     @property
     def date_format(self):
         return "yyyy-mm-dd"
@@ -33,9 +29,8 @@ class TestHelper(ABC):
     def db_api(self):
         return self.DatabaseAPI(**self.db_api_args())
 
-    @abstractmethod
     def convert_frame(self, df):
-        pass
+        return df
 
     def load_frame_from_csv(self, path):
         import pyarrow.csv as pv
@@ -82,9 +77,6 @@ class DuckDBTestHelper(TestHelper):
     def db_api_args(self):
         return {"connection": self.con}
 
-    def convert_frame(self, df):
-        return df
-
     def load_frame_from_csv(self, path):
         return self.con.read_csv(path)
 
@@ -112,11 +104,6 @@ class SparkTestHelper(TestHelper):
             "num_partitions_on_repartition": 2,
             "break_lineage_method": "parquet",
         }
-
-    def convert_frame(self, df):
-        spark_frame = self.spark.createDataFrame(df)
-        spark_frame.persist()
-        return spark_frame
 
     def load_frame_from_csv(self, path):
         df = self.spark.read.csv(path, header=True)
@@ -153,11 +140,6 @@ class SQLiteTestHelper(TestHelper):
         cls._frame_counter += 1
         return name
 
-    def convert_frame(self, df):
-        name = self._get_input_name()
-        df.to_sql(name, self.con, if_exists="replace")
-        return name
-
     def load_frame_from_csv(self, path):
         return self.convert_frame(super().load_frame_from_csv(path))
 
@@ -186,26 +168,6 @@ class PostgresTestHelper(TestHelper):
     def _get_input_name(cls):
         name = f"input_alias_{cls._frame_counter}"
         cls._frame_counter += 1
-        return name
-
-    def convert_frame(self, df):
-        from sqlalchemy.dialects import postgresql
-        from sqlalchemy.types import INTEGER, TEXT
-
-        name = self._get_input_name()
-        # workaround to handle array column conversion
-        # manually mark any list columns so type is handled correctly
-        dtypes = {}
-        for colname, values in df.items():
-            # TODO: will fail if first value is null
-            if isinstance(values[0], list):
-                # TODO: will fail if first array is empty
-                initial_array_val = values[0][0]
-                if isinstance(initial_array_val, int):
-                    dtypes[colname] = postgresql.ARRAY(INTEGER)
-                elif isinstance(initial_array_val, str):
-                    dtypes[colname] = postgresql.ARRAY(TEXT)
-        df.to_sql(name, con=self.engine, if_exists="replace", dtype=dtypes)
         return name
 
     def load_frame_from_csv(self, path):
