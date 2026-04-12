fbfe328a1e03200c2c20d23c9055c11a852a1cd7
Lawrence Adams <lawrencetadams@googlemail.com>
2026-04-12 13:08:06 +0100

    fix: missing space with transient; add query tag setting

--- splink/internals/snowflake/database_api.py
+++ splink/internals/snowflake/database_api.py
@@ -5,6 +5,7 @@ import pandas as pd
 import snowflake.connector.pandas_tools as sf_pd_tools
 from snowflake.connector.connection import SnowflakeConnection
 from snowflake.connector.cursor import SnowflakeCursor
+from datetime import datetime
 
 from splink.internals.database_api import DatabaseAPI
 from splink.internals.dialects import SnowflakeDialect
@@ -24,21 +25,33 @@ class SnowflakeAPI(DatabaseAPI[SnowflakeCursor]):
         connection: SnowflakeConnection,
         register_udfs: bool = True,
         use_transient_tables: bool = True,
+        ignore_quoted_identifiers: bool = True,
+        query_tag: str | None = f"splink_run__{datetime.now()}__",
     ):
         super().__init__()
 
         self._con = connection
-        self.__set_snowflake_quoted_identifiers_ignore()
         self._use_transient_tables = use_transient_tables
+        self._ignore_quoted_identifiers = ignore_quoted_identifiers
+
+        if ignore_quoted_identifiers:
+            self.__set_snowflake_quoted_identifiers_ignore()
 
         if register_udfs:
             self._register_udfs()
 
+        self._set_query_tag(query_tag)
+
+    def _set_query_tag(self, query_tag: str | None):
+        if query_tag:
+            sql = f"ALTER SESSION SET QUERY_TAG = '{query_tag}'"
+            logger.warning(sql)
+            self._con.cursor().execute(sql)
+
     def _register_udfs(self):
-        # if people have issues with permissions we can allow these to be optional
-        # need for predict_from_comparison_vectors_sql (could adjust)
         self._create_log2_function()
 
+    # === CUSTOM UDFs ===
     def _create_log2_function(self):
         sql = """
             CREATE TEMPORARY FUNCTION IF NOT EXISTS LOG2(FLOAT_IN FLOAT)
@@ -50,8 +63,10 @@ class SnowflakeAPI(DatabaseAPI[SnowflakeCursor]):
             """
         self._con.cursor().execute(sql)
 
+    # === END CUSTOM UDFs ===
+
     def __set_snowflake_quoted_identifiers_ignore(self) -> None:
-        logger.warning(
+        logger.info(
             "Setting snowflake session to ignore quoted identifiers for greater"
             " compatibility"
         )
@@ -75,10 +90,24 @@ class SnowflakeAPI(DatabaseAPI[SnowflakeCursor]):
             input = pd.DataFrame.from_records(input)
 
         # HACK: Force table names to be upper to allow more flexible use cases
+        # HACK: warn users if mixed case tables are being used; somewhat of a snowflake
+        #       anti-pattern
+        if (
+            not (table_name.islower() or table_name.isupper())
+            and self._ignore_quoted_identifiers
+        ):
+            logger.warning(f"Mixed case detected for table `{table_name}`")
+            logger.warning(
+                "This may cause failures downstream as `ignore_quoted_identifiers`"
+                "is not enabled"
+            )
+            logger.warning(
+                "https://docs.snowflake.com/en/sql-reference/parameters#label-quoted-identifiers-ignore-case"
+            )
+
         table_name = table_name.upper()
 
         # Use snowflake helper library rather than faff around
-        # TODO: Maybe just import explicit libraries
         sf_pd_tools.write_pandas(self._con, input, table_name, auto_create_table=True)
 
     def table_to_splink_dataframe(
@@ -108,7 +137,7 @@ class SnowflakeAPI(DatabaseAPI[SnowflakeCursor]):
         """
         self.delete_table_from_database(physical_name)
         sql = (
-            f"CREATE {'TRANSIENT' if self._use_transient_tables else ''}"
+            f"CREATE {'TRANSIENT ' if self._use_transient_tables else ''}"
             f"TABLE {physical_name} AS {sql}"
         )
         return sql
