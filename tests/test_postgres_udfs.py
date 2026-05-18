b3d4d80c3aadf84424e3cb79004826d5b250c33f
ADBond <48208438+ADBond@users.noreply.github.com>
2026-05-18 14:42:51 +0100

    depandas (& simplify!) postgres udfs test

--- tests/test_postgres_udfs.py
+++ tests/test_postgres_udfs.py
@@ -3,10 +3,6 @@ import pytest
 pytest.importorskip("sqlalchemy")
 # ruff: noqa: E402 (module level import not at top of file)
 
-import pandas as pd
-from sqlalchemy.dialects import postgresql
-from sqlalchemy.types import INTEGER
-
 from splink.internals.postgres.database_api import PostgresAPI
 
 from .decorator import mark_with_dialects_including
@@ -15,42 +11,51 @@ from .decorator import mark_with_dialects_including
 @mark_with_dialects_including("postgres")
 def test_log2(pg_engine):
     db_api = PostgresAPI(engine=pg_engine)
-    df = pd.DataFrame({"x": [2, 8, 0.5, 1]})
-    expected_log2_vals = [1, 3, -1, 0]
-    db_api._create_backend_table(df, "log_values")
-    sql = """SELECT log2("x") AS logs FROM log_values"""
-    frame = db_api._execute_sql(sql, "test_log_table").as_pandas_dataframe()
 
-    for log_result, expected in zip(frame["logs"], expected_log2_vals):
-        assert log_result == expected
+    data = [
+        {"x": 8, "expected_log2_value": 3},
+        {"x": 2, "expected_log2_value": 1},
+        {"x": 1, "expected_log2_value": 0},
+        {"x": 0.5, "expected_log2_value": -1},
+    ]
+    log_values = db_api.register(data)
+
+    sql = "SELECT log2(x) AS actual_log2_value, expected_log2_value FROM {this}"
+    result = log_values.query_sql(sql)
+
+    for result_row in result.as_record_dict():
+        assert result_row["actual_log2_value"] == result_row["expected_log2_value"]
 
 
 @mark_with_dialects_including("postgres")
 def test_array_intersect(pg_engine):
     db_api = PostgresAPI(engine=pg_engine)
-    df = pd.DataFrame(
-        [
-            {"arr_l": [1, 2, 3], "arr_r": [1, 5, 6], "expected": [1]},
-            {"arr_l": [1, 2, 3], "arr_r": [10, 1, -2], "expected": [1]},
-            {"arr_l": [1, 2, 3], "arr_r": [4, 5, 6], "expected": []},
-            {"arr_l": [1, 2, 3], "arr_r": [2, 1, 7, 10], "expected": [1, 2]},
-            {"arr_l": [1, 2, 3], "arr_r": [1, 1, 1], "expected": [1]},
-            {"arr_l": [1, 1, 1], "arr_r": [1, 2, 3], "expected": [1]},
-            {"arr_l": [3, 5, 7], "arr_r": [3, 5, 7], "expected": [3, 5, 7]},
-            {"arr_l": [1, 2, 3, 4, 5], "arr_r": [3, 5, 7], "expected": [3, 5]},
-        ]
-    )
-    expected_intersect_vals = df["expected"]
-    df.to_sql(
-        "intersect_vals",
-        pg_engine,
-        dtype={"arr_l": postgresql.ARRAY(INTEGER), "arr_r": postgresql.ARRAY(INTEGER)},
-    )
-    sql = "SELECT array_intersect(arr_l, arr_r) AS intersects FROM intersect_vals"
-    frame = db_api._execute_sql(sql, "test_intersect_table").as_pandas_dataframe()
+    data = [
+        {"arr_l": [1, 2, 3], "arr_r": [1, 5, 6], "expected": [1]},
+        {"arr_l": [1, 2, 3], "arr_r": [10, 1, -2], "expected": [1]},
+        {"arr_l": [1, 2, 3], "arr_r": [4, 5, 6], "expected": []},
+        {"arr_l": [1, 2, 3], "arr_r": [2, 1, 7, 10], "expected": [1, 2]},
+        {"arr_l": [1, 2, 3], "arr_r": [1, 1, 1], "expected": [1]},
+        {"arr_l": [1, 1, 1], "arr_r": [1, 2, 3], "expected": [1]},
+        {"arr_l": [3, 5, 7], "arr_r": [3, 5, 7], "expected": [3, 5, 7]},
+        {"arr_l": [1, 2, 3, 4, 5], "arr_r": [3, 5, 7], "expected": [3, 5]},
+    ]
 
-    for int_result, expected in zip(frame["intersects"], expected_intersect_vals):
+    data_sdf = db_api.register(data)
+    sql = """
+        SELECT
+            array_intersect(arr_l, arr_r) AS actual_intersection,
+            expected AS expected_intersection
+        FROM {this}
+    """
+    result = data_sdf.query_sql(sql)
+
+    for result_row in result.as_record_dict():
         # don't care about order
-        assert set(int_result) == set(expected)
+        assert set(result_row["actual_intersection"]) == set(
+            result_row["expected_intersection"]
+        )
         # should check we don't have duplicates
-        assert len(int_result) == len(expected)
+        assert len(result_row["actual_intersection"]) == len(
+            result_row["expected_intersection"]
+        )
