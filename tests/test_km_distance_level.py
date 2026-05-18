ac6a781a23041faee3c2a9193586be1924c7555a
ADBond <48208438+ADBond@users.noreply.github.com>
2026-05-18 13:47:01 +0100

    depandas km distance tests

--- tests/test_km_distance_level.py
+++ tests/test_km_distance_level.py
@@ -1,11 +1,12 @@
-import pandas as pd
-
 import splink.internals.comparison_level_library as cll
 import splink.internals.comparison_library as cl
 from splink.internals.duckdb.database_api import DuckDBAPI
 from splink.internals.linker import Linker
-
-from .decorator import mark_with_dialects_excluding
+from tests.decorator import mark_with_dialects_excluding
+from tests.utils import (
+    assert_id_pair_has_gamma_value,
+    assert_number_of_rows_with_gamma_value,
+)
 
 
 @mark_with_dialects_excluding()
@@ -13,16 +14,6 @@ def test_simple_run(dialect):
     cl.DistanceInKMAtThresholds(
         lat_col="lat", long_col="long", km_thresholds=[1, 5, 10]
     ).create_comparison_dict(dialect)
-    # print(
-    #    cl.DistanceInKMAtThresholds(
-    #        lat_col="latlong[0]", long_col="latlong[1]", km_thresholds=[1, 5, 10]
-    #    ).as_dict()
-    # )
-    # print(
-    #    cl.DistanceInKMAtThresholds(
-    #        lat_col="ll['lat']", long_col="ll['long']", km_thresholds=[1, 5, 10]
-    #    ).as_dict()
-    # )
 
 
 @mark_with_dialects_excluding()
@@ -32,58 +23,50 @@ def test_simple_run_cll(dialect):
             lat_col="lat", long_col="long", km_threshold=1
         ).create_level_dict(dialect)
     )
-    # cll.DistanceInKMLevel(
-    #     lat_col="latlong[0]", long_col="latlong[1]", km_threshold=0.1
-    # ).as_dict()
-    # cll.DistanceInKMLevel(
-    #     lat_col="ll['lat']", long_col="ll['long']", km_threshold=10
-    # ).as_dict()
 
 
 @mark_with_dialects_excluding()
 def test_km_distance_levels(dialect, test_helpers):
     helper = test_helpers[dialect]
 
-    df = pd.DataFrame(
-        [
-            {
-                "unique_id": 1,
-                "name": "102 Petty France",
-                "lat": 51.500516,
-                "long": -0.133192,
-            },
-            {
-                "unique_id": 2,
-                "name": "10 South Colonnade",
-                "lat": 51.504444,
-                "long": -0.021389,
-            },
-            {
-                "unique_id": 3,
-                "name": "Houses of Parliament",
-                "lat": 51.499479,
-                "long": -0.124809,
-            },
-            {
-                "unique_id": 4,
-                "name": "5 Wellington Place",
-                "lat": 53.796105,
-                "long": -1.549725,
-            },
-            {
-                "unique_id": 5,
-                "name": "102 Petty France Duplicate",
-                "lat": 51.500516,
-                "long": -0.133192,
-            },
-            {
-                "unique_id": 6,
-                "name": "Splink",
-                "lat": 53.3338,
-                "long": -6.24488,
-            },
-        ]
-    )
+    data = [
+        {
+            "unique_id": 1,
+            "name": "102 Petty France",
+            "lat": 51.500516,
+            "long": -0.133192,
+        },
+        {
+            "unique_id": 2,
+            "name": "10 South Colonnade",
+            "lat": 51.504444,
+            "long": -0.021389,
+        },
+        {
+            "unique_id": 3,
+            "name": "Houses of Parliament",
+            "lat": 51.499479,
+            "long": -0.124809,
+        },
+        {
+            "unique_id": 4,
+            "name": "5 Wellington Place",
+            "lat": 53.796105,
+            "long": -1.549725,
+        },
+        {
+            "unique_id": 5,
+            "name": "102 Petty France Duplicate",
+            "lat": 51.500516,
+            "long": -0.133192,
+        },
+        {
+            "unique_id": 6,
+            "name": "Splink",
+            "lat": 53.3338,
+            "long": -6.24488,
+        },
+    ]
 
     settings_cl = {
         "link_type": "dedupe_only",
@@ -122,32 +105,34 @@ def test_km_distance_levels(dialect, test_helpers):
 
     settings_cll = {"link_type": "dedupe_only", "comparisons": [km_diff]}
 
-    linker = helper.linker_with_registration(df, settings_cl)
-    cl_df_e = linker.inference.predict().as_pandas_dataframe()
-    linker = helper.linker_with_registration(df, settings_cll)
-    cll_df_e = linker.inference.predict().as_pandas_dataframe()
+    linker = helper.linker_with_registration([data], settings_cl)
+    cl_df_e = linker.inference.predict()
+    linker = helper.linker_with_registration([data], settings_cll)
+    cll_df_e = linker.inference.predict()
 
     linker_outputs = {
-        "cl": cl_df_e,
-        "cll": cll_df_e,
+        "cl": {
+            "df_pred": cl_df_e,
+            "gamma_colname": "gamma_lat_long",
+        },
+        "cll": {
+            "df_pred": cll_df_e,
+            "gamma_colname": "gamma_custom_lat_long",
+        },
     }
 
     # # Dict key: {size: gamma_level value}
     size_gamma_lookup = {0: 5, 1: 4, 2: 3, 3: 2, 4: 1}
 
     # Check gamma sizes are as expected
-    for gamma, gamma_lookup in size_gamma_lookup.items():
+    for gamma_level, gamma_lookup in size_gamma_lookup.items():
         for linker_pred in linker_outputs.values():
-            gamma_column_name_options = [
-                # cl version
-                "gamma_lat_long",
-                # our custom cll version
-                "gamma_custom_lat_long",
-            ]
-            gamma_column_name = linker_pred.columns[
-                linker_pred.columns.str.contains("|".join(gamma_column_name_options))
-            ][0]
-            assert sum(linker_pred[gamma_column_name] == gamma) == gamma_lookup
+            assert_number_of_rows_with_gamma_value(
+                linker_pred["df_pred"],
+                linker_pred["gamma_colname"],
+                gamma_level,
+                gamma_lookup,
+            )
 
     # Check individual IDs are assigned to the correct gamma values
     # Dict key: {gamma_value: tuple of ID pairs}
@@ -158,40 +143,30 @@ def test_km_distance_levels(dialect, test_helpers):
         1: [(3, 4)],
     }
 
-    for gamma, id_pairs in gamma_lookup.items():
-        for left, right in id_pairs:
-            gamma_column_name_options = [
-                "gamma_custom_long_lat",
-                "gamma_custom_lat_long",
-            ]  # lat and long switch unpredictably
-            gamma_column_name = linker_pred.columns[
-                linker_pred.columns.str.contains("|".join(gamma_column_name_options))
-            ][0]
-            assert (
-                linker_pred.loc[
-                    (linker_pred.unique_id_l == left)
-                    & (linker_pred.unique_id_r == right)
-                ][gamma_column_name].values[0]
-                == gamma
-            )
+    for gamma_level, id_pairs in gamma_lookup.items():
+        for id_pair in id_pairs:
+            for linker_pred in linker_outputs.values():
+                assert_id_pair_has_gamma_value(
+                    linker_pred["df_pred"],
+                    linker_pred["gamma_colname"],
+                    gamma_level,
+                    id_pair,
+                )
 
 
 def test_haversine_level():
     data = [
-        {"id": 1, "lat": 22.730590, "lon": 9.388589},
-        {"id": 2, "lat": 22.836322, "lon": 9.276112},
-        {"id": 3, "lat": 37.770850, "lon": 95.689880},
-        {"id": 4, "lat": -31.336319, "lon": 145.183685},
+        {"unique_id": 1, "lat": 22.730590, "lon": 9.388589},
+        {"unique_id": 2, "lat": 22.836322, "lon": 9.276112},
+        {"unique_id": 3, "lat": 37.770850, "lon": 95.689880},
+        {"unique_id": 4, "lat": -31.336319, "lon": 145.183685},
     ]
     # Add another the array version of the lat_long column
     for d in data:
         d["lat_long"] = {"lat": d["lat"], "long": d["lon"]}
         d["lat_long_arr"] = [d["lat"], d["lon"]]
 
-    df = pd.DataFrame(data)
-
     settings = {
-        "unique_id_column_name": "id",
         "link_type": "dedupe_only",
         "blocking_rules_to_generate_predictions": [],
         "comparisons": [
@@ -224,20 +199,17 @@ def test_haversine_level():
 
     db_api = DuckDBAPI()
 
-    df_sdf = db_api.register(df, source_dataset_name="test")
+    df_sdf = db_api.register(data, source_dataset_name="test")
     linker = Linker(df_sdf, settings)
-    df_e = linker.inference.predict().as_pandas_dataframe()
+    df_e = linker.inference.predict()
 
-    row = dict(df_e.query("id_l == 1 and id_r == 2").iloc[0])
-    assert row["gamma_lat_long"] == 3
+    assert_id_pair_has_gamma_value(df_e, "gamma_lat_long", 3, (1, 2))
 
     # id comparisons w/ dist < 10000km
     id_comb = {(1, 3), (2, 3), (3, 4)}
     for id_pair in id_comb:
-        row = dict(df_e.query("id_l == {} and id_r == {}".format(*id_pair)).iloc[0])
-        assert row["gamma_lat_long"] == 2
+        assert_id_pair_has_gamma_value(df_e, "gamma_lat_long", 2, id_pair)
 
     id_comb = {(1, 4), (2, 4)}
     for id_pair in id_comb:
-        row = dict(df_e.query("id_l == {} and id_r == {}".format(*id_pair)).iloc[0])
-        assert row["gamma_lat_long"] == 1
+        assert_id_pair_has_gamma_value(df_e, "gamma_lat_long", 1, id_pair)
