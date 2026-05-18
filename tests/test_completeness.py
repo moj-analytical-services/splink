f4caac06d36669180fba971d20bf42d01fce2d88
ADBond <48208438+ADBond@users.noreply.github.com>
2026-05-18 11:24:48 +0100

    completeness test pandas compatible

--- tests/test_completeness.py
+++ tests/test_completeness.py
@@ -1,9 +1,10 @@
-import pandas as pd
+import pyarrow as pa
 from pytest import raises
 
 from splink.exploratory import completeness_chart
 from splink.internals.exceptions import SplinkException
 from tests.decorator import mark_with_dialects_excluding
+from tests.helpers import TestHelper
 
 
 # The UNION ALL used for this chart gives a
@@ -35,21 +36,20 @@ def test_completeness_chart_mismatched_columns(dialect, test_helpers, fake_1000)
 def test_completeness_chart_complex_columns(dialect, test_helpers):
     helper = test_helpers[dialect]
     db_api = helper.db_api()
-    df = pd.DataFrame(
-        {
-            "id": [1, 2, 3, 4, 5],
-            "first_name": ["Arnie", None, "Carla", "Debbie", None],
-            "surname": [None, None, None, None, "Everett"],
-            "city_arr": [
-                ["London", "Leeds"],
-                ["Birmingham"],
-                None,
-                ["Leeds", "Manchester"],
-                None,
-            ],
-        }
-    )
-    df_sdf = db_api.register(df)
+    data = {
+        "id": [1, 2, 3, 4, 5],
+        "first_name": ["Arnie", None, "Carla", "Debbie", None],
+        "surname": [None, None, None, None, "Everett"],
+        "city_arr": [
+            ["London", "Leeds"],
+            ["Birmingham"],
+            None,
+            ["Leeds", "Manchester"],
+            None,
+        ],
+    }
+
+    df_sdf = db_api.register(data)
     first = helper.arrays_from
     # check completeness when we have more complicated column constructs, such as
     # indexing into array columns
@@ -57,10 +57,13 @@ def test_completeness_chart_complex_columns(dialect, test_helpers):
 
 
 @mark_with_dialects_excluding("sqlite")
-def test_completeness_chart_source_dataset(dialect, test_helpers):
+def test_completeness_chart_source_dataset(
+    dialect: str, test_helpers: dict[str, TestHelper], fake_1000: pa.Table
+):
     helper = test_helpers[dialect]
     db_api = helper.db_api()
-    df_pd = pd.read_csv("./tests/datasets/fake_1000_from_splink_demos.csv")
-    df_pd["source_dataset"] = "fake_1000"
-    df_sdf = db_api.register(df_pd)
+
+    pa_df = fake_1000.append_column("source_dataset", pa.array(1000 * ["fake_1000"]))
+    assert pa_df["source_dataset"]
+    df_sdf = db_api.register(pa_df)
     completeness_chart(df_sdf)
