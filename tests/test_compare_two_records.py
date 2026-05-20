615195952c9357de89547cda679d8e7578d44df5
ADBond <48208438+ADBond@users.noreply.github.com>
2026-05-20 15:21:24 +0100

    more tests without pandas

--- tests/test_compare_two_records.py
+++ tests/test_compare_two_records.py
@@ -1,7 +1,5 @@
 import datetime
 
-import numpy as np
-import pandas as pd
 import pytest
 
 import splink.internals.comparison_library as cl
@@ -12,15 +10,11 @@ from .decorator import mark_with_dialects_excluding
 
 
 @mark_with_dialects_excluding("sqlite")
-def test_compare_two_records_1(test_helpers, dialect):
+def test_compare_two_records_1(test_helpers, dialect, fake_1000):
     # This one tests the following cases
     # - User provides a city tf tble
     # - But first_name tf table derived from input data
     helper = test_helpers[dialect]
-
-    df = helper.load_frame_from_parquet(
-        "./tests/datasets/fake_1000_from_splink_demos_strip_datetypes.parquet"
-    )
 
     settings = SettingsCreator(
         link_type="dedupe_only",
@@ -40,19 +34,20 @@ def test_compare_two_records_1(test_helpers, dialect):
         retain_matching_columns=True,
     )
 
-    linker = helper.linker_with_registration(df, settings)
+    linker = helper.linker_with_registration(fake_1000, settings)
+    # linker._debug_mode = True
 
-    city_tf = pd.DataFrame(
-        [
-            {"city": "London", "tf_city": 0.2},
-            {"city": "Liverpool", "tf_city": 0.8},
-        ]
-    )
+    city_tf = [
+        {"city": "London", "tf_city": 0.2},
+        {"city": "Liverpool", "tf_city": 0.8},
+    ]
+
     linker.table_management.register_term_frequency_lookup(city_tf, "city")
 
     # Test with dictionary inputs
+    # need to include spaces in first name as that is how they appear in csv file
     r1 = {
-        "first_name": "Julia",
+        "first_name": "Julia ",
         "surname": "Taylor",
         "dob": datetime.date(2015, 10, 29),
         "city": "London",
@@ -60,7 +55,7 @@ def test_compare_two_records_1(test_helpers, dialect):
     }
 
     r2 = {
-        "first_name": "Julia",
+        "first_name": "Julia ",
         "surname": "Taylor",
         "dob": datetime.date(2015, 10, 29),
         "city": "London",
@@ -68,31 +63,24 @@ def test_compare_two_records_1(test_helpers, dialect):
     }
 
     res = linker.inference.compare_two_records(r1, r2)
-    res_pd = res.as_pandas_dataframe()
+    res_dict = res.as_dict()
 
     # Verify term frequencies match in the comparison result
-    assert res_pd["tf_city_l"].iloc[0] == 0.2
-    assert res_pd["tf_city_r"].iloc[0] == 0.2
+    assert res_dict["tf_city_l"][0] == 0.2
+    assert res_dict["tf_city_r"][0] == 0.2
     # This is the tf value as derived from the input data
-    assert pytest.approx(res_pd["tf_first_name_l"].iloc[0]) == np.float64(
-        0.00444444444444
-    )
-    assert pytest.approx(res_pd["tf_first_name_r"].iloc[0]) == np.float64(
-        0.00444444444444
-    )
+    assert pytest.approx(res_dict["tf_first_name_l"][0]) == 0.00444444444444
+
+    assert pytest.approx(res_dict["tf_first_name_r"][0]) == 0.00444444444444
 
 
 @mark_with_dialects_excluding("sqlite")
-def test_compare_two_records_2(test_helpers, dialect):
+def test_compare_two_records_2(test_helpers, dialect, fake_1000):
     # This one tests the following cases
     # - User provides a city and first_name tf tables
     # - But specific values provided in input data, which take precedence
 
     helper = test_helpers[dialect]
-
-    df = helper.load_frame_from_parquet(
-        "./tests/datasets/fake_1000_from_splink_demos_strip_datetypes.parquet"
-    )
 
     settings = SettingsCreator(
         link_type="dedupe_only",
@@ -112,22 +100,18 @@ def test_compare_two_records_2(test_helpers, dialect):
         retain_matching_columns=True,
     )
 
-    linker = helper.linker_with_registration(df, settings)
+    linker = helper.linker_with_registration(fake_1000, settings)
 
-    city_tf = pd.DataFrame(
-        [
-            {"city": "London", "tf_city": 0.2},
-            {"city": "Liverpool", "tf_city": 0.8},
-        ]
-    )
+    city_tf = [
+        {"city": "London", "tf_city": 0.2},
+        {"city": "Liverpool", "tf_city": 0.8},
+    ]
     linker.table_management.register_term_frequency_lookup(city_tf, "city")
 
-    first_name_tf = pd.DataFrame(
-        [
-            {"first_name": "Julia", "tf_first_name": 0.3},
-            {"first_name": "Robert", "tf_first_name": 0.8},
-        ]
-    )
+    first_name_tf = [
+        {"first_name": "Julia", "tf_first_name": 0.3},
+        {"first_name": "Robert", "tf_first_name": 0.8},
+    ]
     linker.table_management.register_term_frequency_lookup(first_name_tf, "first_name")
 
     # Test with dictionary inputs
@@ -150,10 +134,10 @@ def test_compare_two_records_2(test_helpers, dialect):
     }
 
     res = linker.inference.compare_two_records(r1, r2)
-    res_pd = res.as_pandas_dataframe()
+    res_dict = res.as_dict()
 
     # Verify term frequencies match in the comparison result
-    assert res_pd["tf_city_l"].iloc[0] == 0.5
-    assert res_pd["tf_city_r"].iloc[0] == 0.2
-    assert res_pd["tf_first_name_l"].iloc[0] == 0.3
-    assert res_pd["tf_first_name_r"].iloc[0] == 0.4
+    assert res_dict["tf_city_l"][0] == 0.5
+    assert res_dict["tf_city_r"][0] == 0.2
+    assert res_dict["tf_first_name_l"][0] == 0.3
+    assert res_dict["tf_first_name_r"][0] == 0.4
