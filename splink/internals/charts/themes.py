ed22c06703a4a25d90bbfdb7267a119de2ead660
ADBond <48208438+ADBond@users.noreply.github.com>
2026-07-22 10:44:49 +0100

    nest theme colours as specific theme layer

--- splink/internals/charts/themes.py
+++ splink/internals/charts/themes.py
@@ -1,56 +1,85 @@
+from copy import deepcopy
 from typing import Any
 
 # TODO: we can use TypedDict to type this, with NotRequired when we drop python 3.10
-default_colours = {
-    "positive": "#008000",  # green
-    "neutral": "#bbbbbb",
-    "negative": "#FF0000",  # red
-    "reference_strong": "#000000",  # black
-    "text": "#000000",  # black
-    "text_soft": "#808080",  # grey
-    "text_inverse": "#FFFFFF",  # white
+default_theme_values = {
+    "colour": {
+        "positive": "#008000",  # green
+        "neutral": "#bbbbbb",
+        "negative": "#FF0000",  # red
+        "reference_strong": "#000000",  # black
+        "text": "#000000",  # black
+        "text_soft": "#808080",  # grey
+        "text_inverse": "#FFFFFF",  # white
+    },
 }
-colour_keys = set(default_colours.keys())
+theme_keys = set(default_theme_values.keys())
+theme_sub_keys = {k: set(v.keys()) for k, v in default_theme_values.items()}
 
 
 class SplinkColourTheme:
     def __init__(self, theme_dict: dict[str, Any] | None = None):
         if theme_dict is None:
-            theme_dict = default_colours
-        self._validate_colour_dict(theme_dict)
-        # make a copy so we don't need to worry about mutation
-        self.theme_dict = {**theme_dict}
+            theme_dict = default_theme_values
+        self._validate_theme_dict(theme_dict)
+        self.theme_dict = deepcopy(theme_dict)
 
     @staticmethod
-    def _validate_colour_dict(theme_dict: dict[str, Any]) -> None:
-        provided_colours = set(theme_dict.keys())
-        if not provided_colours <= colour_keys:
+    def _validate_theme_dict(theme_dict: dict[str, Any]) -> None:
+        provided_theme_elements = set(theme_dict.keys())
+        if not provided_theme_elements <= theme_keys:
             raise ValueError(
-                f"Can only provide values for colours: {colour_keys}.  "
-                f"Found extra colours: {provided_colours - colour_keys}"
+                f"Can only provide values for theme elements: {theme_keys}.  "
+                f"Found extra theme elements: {provided_theme_elements - theme_keys}"
             )
 
+        for theme_element, values in theme_dict.items():
+            provided_sub_keys = set(values.keys())
+            allowed_sub_keys = theme_sub_keys[theme_element]
+            if not provided_sub_keys <= allowed_sub_keys:
+                raise ValueError(
+                    f"Can only provide values for "
+                    f"'{theme_element}': {allowed_sub_keys}.  "
+                    f"Found extra values: {provided_sub_keys - allowed_sub_keys}"
+                )
+
     def extend_with(self, theme_dict: dict[str, Any]) -> "SplinkColourTheme":
-        self._validate_colour_dict(theme_dict)
-        return SplinkColourTheme({**self.theme_dict, **theme_dict})
+        self._validate_theme_dict(theme_dict)
+
+        merged_theme = deepcopy(self.theme_dict)
+        for item_type, values in theme_dict.items():
+            merged_theme[item_type] = {**merged_theme[item_type], **values}
+
+        return SplinkColourTheme(merged_theme)
 
     def inject_colours_into_spec(self, chart_spec: dict[str, Any]) -> dict[str, Any]:
-        for key, value in self.theme_dict.items():
-            chart_spec = self._replace_colour_placeholder(chart_spec, key, value)
+        for theme_element, value_dict in self.theme_dict.items():
+            for name, value in value_dict.items():
+                chart_spec = self._replace_colour_placeholder(
+                    chart_spec, theme_element, name, value
+                )
         return chart_spec
 
     def _replace_colour_placeholder(
-        self, chart_spec: dict[str, Any], colour_key: str, colour_value: str
+        self,
+        chart_spec: dict[str, Any],
+        theme_element: str,
+        colour_key: str,
+        colour_value: str,
     ) -> dict[str, Any]:
-        placeholder = f"__splink_colour_{colour_key}__"
+        placeholder = f"__splink_{theme_element}_{colour_key}__"
         if isinstance(chart_spec, dict):
             return {
-                k: self._replace_colour_placeholder(v, colour_key, colour_value)
+                k: self._replace_colour_placeholder(
+                    v, theme_element, colour_key, colour_value
+                )
                 for k, v in chart_spec.items()
             }
         elif isinstance(chart_spec, list):
             return [
-                self._replace_colour_placeholder(item, colour_key, colour_value)
+                self._replace_colour_placeholder(
+                    item, theme_element, colour_key, colour_value
+                )
                 for item in chart_spec
             ]
         elif isinstance(chart_spec, str) and chart_spec == placeholder:
@@ -61,9 +90,11 @@ class SplinkColourTheme:
 default_theme = SplinkColourTheme()
 
 alt = {
-    "positive": "#0571b0",
-    "neutral": "#f7f7f7",
-    "negative": "#ca0020",
+    "colour": {
+        "positive": "#0571b0",
+        "neutral": "#f7f7f7",
+        "negative": "#ca0020",
+    },
 }
 alt_theme = default_theme.extend_with(alt)
 
